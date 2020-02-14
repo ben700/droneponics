79536d45baf3c4bd700c6ff41bc71ed0a6d3ab24
ben700 <ben700@hotmail.com>
2020-02-14 18:30:25 +0000

    Add files via upload

--- relay.py
+++ relay.py
@@ -1,22 +1,19 @@
-import blynklib
+import BlynkLib
 
 BLYNK_AUTH = 'GP_sDPLJqyEN7jky9_zcQVSkgiyx-AeW' #motherLights
 
 # Initialize Blynk
-blynk = blynklib.Blynk(BLYNK_AUTH)
+blynk = BlynkLib.Blynk(BLYNK_AUTH)
 
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(26, GPIO.OUT)
 
-@blynk.handle_event("connect")
+@blynk.on("connected")
 def connect_handler():
-   blynk.virtual_sync(V38, 38)
-   blynk.read_response(timeout=0.5)
+   blynk.virtual_sync(38)
         
-        
-        
-@blynk.handle_event('write V38')
+@blynk.on("V38")
 def v38_write_handler(pin, value):
     GPIO.output(26,value[0])
     
