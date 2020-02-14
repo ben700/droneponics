ac82c4131c6d979b34063cbbe0d93111f38a971d
ben700 <ben700@hotmail.com>
2020-02-14 17:59:04 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+sudo apt-get update && sudo apt-get upgrade
+
+sudo apt-get install python-pip
+
+sudo pip3 install blynklib
+
+sudo pip3 install python-tsl2591
+sudo pip3 install board
+sudo pip3 install adafruit-circuitpython-bme280
+sudo pip3 install mh-z19
