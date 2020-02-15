52e3fdf44f51d06b133f4f9f204d95c0462e850c
ben700 <ben700@hotmail.com>
2020-02-15 02:03:47 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -8,3 +8,8 @@ sudo pip3 install python-tsl2591
 sudo pip3 install board
 sudo pip3 install adafruit-circuitpython-bme280
 sudo pip3 install mh-z19
+
+sudo apt-get install xterm -y
+
+mkdir /home/pi/.config/autostart
+cp lights.desktop /home/pi/.config/autostart/
