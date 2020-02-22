2cc7664b9175bccebf2be2cc465ff8d1c5f60fe2
ben700 <ben700@hotmail.com>
2020-02-22 04:05:07 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,24 +1,31 @@
-sudo apt-get update && sudo apt-get upgrade
-
+ 
+sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get install python-pip
+sudo apt-get install xterm -y
+sudo apt full-upgrade -y
+sudo apt update
+sudo apt upgrade -y
+sudo apt dist-upgrade -y
+sudo apt autoremove -y
+sudo apt autoclean
 
-sudo pip3 install blynklib
+pip3 install blynklib
+pip3 install blynklib
 
-sudo pip3 install python-tsl2591
-sudo pip3 install board
-sudo pip3 install adafruit-circuitpython-bme280
-sudo pip3 install adafruit-circuitpython-lis3dh
-sudo pip3 install mh-z19
+pip3 install python-tsl2591
+pip3 install board
+pip3 install adafruit-circuitpython-bme280
+pip3 install adafruit-circuitpython-lis3dh
+pip3 install mh-z19
 sudo apt-get install lxde
 
-sudo pip3 install adafruit-circuitpython-seesaw
+pip3 install adafruit-circuitpython-seesaw
 git submodule init
 git submodule update
 git submodule foreach git fetch --all
 git submodule foreach "tag=\$(git rev-list --exclude='*-*' --tags --max-count=1); git checkout -q \$tag"
 
 
-sudo apt-get install xterm -y
-
 mkdir /home/pi/.config/autostart
-cp soil.desktop /home/pi/.config/autostart/
+cp env.desktop /home/pi/.config/autostart/
