b7de5e76201c9285dcce90b311fffc1aafc955c5
ben700 <ben700@hotmail.com>
2020-02-26 20:12:14 +0000

    Add files via upload

--- feedPump.py
+++ feedPump.py
@@ -1,3 +1,6 @@
+from board import SCL, SDA
+import busio
+from adafruit_seesaw.seesaw import Seesaw
 import time
 from datetime import datetime
 import RPi.GPIO as GPIO
@@ -63,6 +66,14 @@ GPIO.output(Pump2Backup, GPIO.LOW)
 GPIO.output(Pump3Backup, GPIO.LOW)
 GPIO.output(Pump4Backup, GPIO.LOW)
 
+
+i2c_bus = busio.I2C(SCL, SDA)
+
+ss1 = Seesaw(i2c_bus, addr=0x36)
+ss2 = Seesaw(i2c_bus, addr=0x37)
+ss3 = Seesaw(i2c_bus, addr=0x38)
+ss4 = Seesaw(i2c_bus, addr=0x38)
+
 # Initialize Blynk
 blynk = BlynkLib.Blynk(BLYNK_AUTH)
 
@@ -77,13 +88,23 @@ def blynk_data():
 
    
     print("Time updated : " + now.strftime("%d/%m/%Y %H:%M:%S"))
-    
+    print("Moisture plat 1 = " + str(ss1.moisture_read()))
     
     blynk.virtual_write(21, GPIO.input(sensorFlowmeterPinPump1))
     blynk.virtual_write(22, GPIO.input(sensorFlowmeterPinPump2))
     blynk.virtual_write(23, GPIO.input(sensorFlowmeterPinPump3))
     blynk.virtual_write(24, GPIO.input(sensorFlowmeterPinPump4))
     blynk.virtual_write(25, GPIO.input(waterLevel))
+    
+    blynk.virtual_write(11, str(ss1.moisture_read()))
+    blynk.virtual_write(12, str(ss1.get_temp()))
+    blynk.virtual_write(13, str(ss2.moisture_read()))
+    blynk.virtual_write(14, str(ss2.get_temp()))
+    blynk.virtual_write(15, str(ss3.moisture_read()))          
+    blynk.virtual_write(16, str(ss3.get_temp()))
+    blynk.virtual_write(17, str(ss4.moisture_read()))
+    blynk.virtual_write(18, str(ss4.get_temp()))
+    
     
 
 
