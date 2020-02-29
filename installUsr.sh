fc9493be49d9de9d13e869f50b5a713ec56821e4
ben700 <ben700@hotmail.com>
2020-02-29 17:28:04 +0000

    Update installUsr.sh

--- installUsr.sh
+++ installUsr.sh
@@ -6,6 +6,7 @@ pip3 install busio
 
 pip3 install adafruit-circuitpython-bme280
 pip3 install adafruit-circuitpython-lis3dh
+pip3 install Adafruit-ADS1x15
 pip3 install mh-z19
 sudo apt-get install lxde
 
