e5cbe9121f6dc5df0a513e61ec1896a6a0b40ca7
ben700 <ben700@hotmail.com>
2020-02-29 17:25:53 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
- 
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install python-pip
@@ -9,23 +8,4 @@ sudo apt upgrade -y
 sudo apt dist-upgrade -y
 sudo apt autoremove -y
 sudo apt autoclean
-
-pip3 install blynklib
-pip3 install blynklib
-
-pip3 install python-tsl2591
-pip3 install board
-pip3 install adafruit-circuitpython-bme280
-pip3 install adafruit-circuitpython-lis3dh
-pip3 install mh-z19
 sudo apt-get install lxde
-
-pip3 install adafruit-circuitpython-seesaw
-git submodule init
-git submodule update
-git submodule foreach git fetch --all
-git submodule foreach "tag=\$(git rev-list --exclude='*-*' --tags --max-count=1); git checkout -q \$tag"
-
-
-mkdir /home/pi/.config/autostart
-cp env.desktop /home/pi/.config/autostart/
---
+++ installUsr.sh
@@ -0,0 +1,16 @@
+pip3 install blynklib
+
+pip3 install python-tsl2591
+pip3 install board
+pip3 install busio
+
+pip3 install adafruit-circuitpython-bme280
+pip3 install adafruit-circuitpython-lis3dh
+pip3 install mh-z19
+sudo apt-get install lxde
+
+pip3 install adafruit-circuitpython-seesaw
+git submodule init
+git submodule update
+git submodule foreach git fetch --all
+git submodule foreach "tag=\$(git rev-list --exclude='*-*' --tags --max-count=1); git checkout -q \$tag"
