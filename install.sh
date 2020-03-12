25362457b5445e091ce95c381d0d97d57b0f20cc
ben700 <ben700@hotmail.com>
2020-03-12 23:21:20 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,7 @@
 cp updateDronepnics.sh ../
-sudo apt-get update
-sudo apt-get upgrade
+/home/pi/droneponics/updatePi.sh
 sudo apt-get install python-pip
 sudo apt-get install xterm -y
-sudo apt full-upgrade -y
-sudo apt update
-sudo apt upgrade -y
-sudo apt dist-upgrade -y
-sudo apt autoremove -y
-sudo apt autoclean
 sudo apt-get install lxde
 /home/pi/droneponics/installUsr.sh
 sudo reboot
