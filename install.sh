8592344317364c66c8909d674fea52ef50f87caf
ben700 <ben700@hotmail.com>
2020-03-13 01:43:19 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 cp /home/pi/droneponics/updateDroneponics.sh /home/pi/updateDroneponics.sh
+sh /home/pi/droneponics/updatePi.sh.sh
 sudo apt-get install python-pip
 sudo apt-get install xterm -y
 sudo apt-get install lxde
-/home/pi/droneponics/installUsr.sh
+sh /home/pi/droneponics/installUsr.sh
 echo "Going to Reboot now"
 sudo reboot
