63669e775b101f6e27117790bd24c2e311df54b5
ben700 <ben700@hotmail.com>
2020-03-13 00:40:07 +0000

    Update installUsr.sh

--- installUsr.sh
+++ installUsr.sh
@@ -1,12 +1,13 @@
 pip3 install blynklib
 
 sudo pip3 install adafruit-circuitpython-tsl2591
+sudo pip3 install python-tsl2591
 pip3 install adafruit-blinka
 
 pip3 install adafruit-circuitpython-bme280
 pip3 install adafruit-circuitpython-lis3dh
 pip3 install adafruit-circuitpython-ads1x15
-pip3 install mh-z19
+sudo pip3 install mh-z19
 sudo apt-get install lxde
 
 pip3 install adafruit-circuitpython-seesaw
