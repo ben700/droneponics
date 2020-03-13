e153826c85207a5daf80268afc7fc5820bfc7ead
ben700 <ben700@hotmail.com>
2020-03-13 01:33:46 +0000

    Add files via upload

---
+++ dfRobotMonitor.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python
+
+import logging
+import blynklib
+import blynktimer
+from datetime import datetime
+import io
+import sys
+import fcntl
+import time
+import copy
+import string
+from AtlasI2C import (
+	 AtlasI2C
+)
+import RPi.GPIO as GPIO
+import board
+import busio
+from adafruit_seesaw.seesaw import Seesaw
+import adafruit_ads1x15.ads1015 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+import os
+import logging
+
+
+bootup = True 
+
+# tune console logging
+_log = logging.getLogger('BlynkLog')
+logFormatter = logging.Formatter("%(asctime)s [%(levelname)s]  %(message)s")
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(logFormatter)
+_log.addHandler(consoleHandler)
+_log.setLevel(logging.DEBUG)
+
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+BLYNK_GREEN     ="#23C48E"
+BLYNK_BLUE      ="#04C0F8"
+BLYNK_YELLOW    ="#ED9D00"
+BLYNK_RED       ="#D3435C"
+BLYNK_DARK_BLUE ="#5F7CD8"
+colors = {'1': '#23C48E', '0': '#D3435C', 'OFFLINE': '#FF0000'}
+
+
+buttFullSensor =  17
+buttEmptySensor = 4
+
+#setup sensor 2
+GPIO.setup(buttFullSensor, GPIO.IN, GPIO.PUD_UP)
+GPIO.setup(buttEmptySensor, GPIO.IN, GPIO.PUD_DOWN)
+
+
+
+# The ID and range of a sample spreadsheet.
+BLYNK_AUTH = '00vIt07mIauITIq4q_quTOakFvcvpgGb' #robot Mon
+
+# Initialize Blynk
+blynk = blynklib.Blynk(BLYNK_AUTH)
+timer = blynktimer.Timer()
+
+APP_CONNECT_PRINT_MSG = '[APP_CONNECT_EVENT]'
+APP_DISCONNECT_PRINT_MSG = '[APP_DISCONNECT_EVENT]'
+CONNECT_PRINT_MSG = '[CONNECT_EVENT]'
+DISCONNECT_PRINT_MSG = '[DISCONNECT_EVENT]'
+WRITE_EVENT_PRINT_MSG = "[WRITE_VIRTUAL_PIN_EVENT] Pin: V{} Value: '{}'"
+READ_PRINT_MSG = "[READ_VIRTUAL_PIN_EVENT] Pin: V{}"
+ALLOWED_COMMANDS_LIST = ['ls', 'lsusb', 'ip a', 'ip abc']
+TWEET_MSG = "New value='{}' on VPIN({})"
+
+
+
+
+@blynk.handle_event("connect")
+def connect_handler():
+    _log.info('SCRIPT_START')
+    for pin in range(5):
+        _log.info('Syncing virtual pin {}'.format(pin))
+        blynk.virtual_sync(pin)
+
+        # within connect handler after each server send operation forced socket reading is required cause:
+        #  - we are not in script listening state yet
+        #  - without forced reading some portion of blynk server messages can be not delivered to HW
+        blynk.read_response(timeout=0.5)
+    
+
+    # Will Print Every 10 Seconds
+@timer.register(interval=10, run_once=False)
+def blynk_data():
+    _log.info("Start of blynk_data")
+    now = datetime.now()
+    blynk.virtual_write(0, now.strftime("%d/%m/%Y %H:%M:%S"))
+    
+    
+while True:
+    try:
+       blynk.run()
+       if bootup :
+          bootup = False
+          now = datetime.now()
+          blynk.virtual_write(99, now.strftime("%d/%m/%Y %H:%M:%S"))
+          _log.info('Just Booted')
+          
+       timer.run()
+    except:
+       _log.info('Unexpected error')
+       os.system('sh /home/pi/updateDroneponics.sh')
+    
