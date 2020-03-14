bfc792a77309e7bfc7b185cfdc9f72470993ecc0
ben700 <ben700@hotmail.com>
2020-03-14 00:21:13 +0000

    Add files via upload

--- testMHZ.py
+++ testMHZ.py
@@ -10,7 +10,7 @@ def mh_z19():
                       stopbits=serial.STOPBITS_ONE,
                       timeout=1.0)
   while 1:
-    result=ser.write("\xff\x01\x86\x00\x00\x00\x00\x00\x79")
+    result=ser.write(b'\xff\x01\x86\x00\x00\x00\x00\x00\x79')
     s=ser.read(9)
     if s[0] == "\xff" and s[1] == "\x86":
       return {'co2': ord(s[2])*256 + ord(s[3])}
