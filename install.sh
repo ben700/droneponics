78b929daca113beb8235edaad2ad2563c9aa4042
ben700 <ben700@hotmail.com>
2020-03-15 03:53:31 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 cp /home/pi/droneponics/updateDroneponics.sh /home/pi/updateDroneponics.sh
 sh /home/pi/droneponics/updatePi.sh
+sudo apt-get install wiringpi
 sudo apt-get install python-pip
 sudo apt-get install xterm -y
 sudo apt-get install lxde
