be8a356799ce983b4ef5fe4348141eca9d80e73c
ben700 <ben700@hotmail.com>
2020-03-21 21:36:02 +0000

    Add files via upload

--- i2cLogger.py
+++ i2cLogger.py
@@ -1,89 +1,101 @@
-##!/usr/bin/env python3 
+    ##!/usr/bin/env python3 
 
+try:
+    from python_tsl2591 import tsl2591
+    import datetime
+    import time
+    import shlex, requests
+    import board
+    import busio
+    import smbus 
+    from ctypes import c_short
+    from ctypes import c_byte
+    from ctypes import c_ubyte
+    import mh_z19
+    import blynklib
+    import blynktimer
+    import logging
+    from datetime import datetime
+    import adafruit_tsl2591
+    import sys
+    import os
 
-from python_tsl2591 import tsl2591
-import datetime
-import time
-import shlex, requests
-import board
-import busio
-import smbus 
-from ctypes import c_short
-from ctypes import c_byte
-from ctypes import c_ubyte
-import mh_z19
-import blynklib
-import blynktimer
-import logging
-from datetime import datetime
-import adafruit_tsl2591
-import sys
-import os
+    class Counter:
+        cycle = 0
 
-class Counter:
-    cycle = 0
+    bootup = True
     
-bootup = True
+    buttFullSensor =  8
+    buttEmptySensor = 10
+    GPIO.setup(buttFullSensor, GPIO.IN)
+    GPIO.setup(buttEmptySensor, GPIO.IN)
+    _log.info(GPIO.input(buttEmptySensor))
+    _log.info(GPIO.input(buttFullSensor))
 
 
-# tune console logging
-_log = logging.getLogger('BlynkLog')
-logFormatter = logging.Formatter("%(asctime)s [%(levelname)s]  %(message)s")
-consoleHandler = logging.StreamHandler()
-consoleHandler.setFormatter(logFormatter)
-_log.addHandler(consoleHandler)
-_log.setLevel(logging.DEBUG)
 
-BLYNK_AUTH = '4IfX_hzDREonPi_PIDQrETikxc0-XpqI' #i2cLogger
+    # tune console logging
+    _log = logging.getLogger('BlynkLog')
+    logFormatter = logging.Formatter("%(asctime)s [%(levelname)s]  %(message)s")
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(logFormatter)
+    _log.addHandler(consoleHandler)
+    _log.setLevel(logging.DEBUG)
 
-# Initialize Blynk
-blynk = blynklib.Blynk(BLYNK_AUTH)
-timer = blynktimer.Timer()
-#blynk.run()
-APP_CONNECT_PRINT_MSG = '[APP_CONNECT_EVENT]'
-APP_DISCONNECT_PRINT_MSG = '[APP_DISCONNECT_EVENT]'
-CONNECT_PRINT_MSG = '[CONNECT_EVENT]'
-DISCONNECT_PRINT_MSG = '[DISCONNECT_EVENT]'
-WRITE_EVENT_PRINT_MSG = "[WRITE_VIRTUAL_PIN_EVENT] Pin: V{} Value: '{}'"
-READ_PRINT_MSG = "[READ_VIRTUAL_PIN_EVENT] Pin: V{}"
-ALLOWED_COMMANDS_LIST = ['ls', 'lsusb', 'ip a', 'ip abc']
-TWEET_MSG = "New value='{}' on VPIN({})"
+    BLYNK_AUTH = '4IfX_hzDREonPi_PIDQrETikxc0-XpqI' #i2cLogger
 
-    
-@blynk.handle_event('write V255')
-def rebooter(pin, value):
-    _log.info("Update Software")
-    blynk.virtual_write(98, "User Reboot " + '\n')
-    os.system('sh /home/pi/updateDroneponics.sh')
-    blynk.virtual_write(98, "System updated and restarting " + '\n')
-    os.system('sudo reboot')
-    
-  
-@timer.register(interval=10, run_once=False)
-def blynk_data():
-    _log.info("Update Timer Run")
-    Counter.cycle += 1
-    now = datetime.now()
-    blynk.virtual_write(0, now.strftime("%d/%m/%Y %H:%M:%S"))    
-    blynk.virtual_write(98, "Completed Timer Function" + '\n') 
-      
-    
+    # Initialize Blynk
+    blynk = blynklib.Blynk(BLYNK_AUTH)
+    timer = blynktimer.Timer()
+    #blynk.run()
+    APP_CONNECT_PRINT_MSG = '[APP_CONNECT_EVENT]'
+    APP_DISCONNECT_PRINT_MSG = '[APP_DISCONNECT_EVENT]'
+    CONNECT_PRINT_MSG = '[CONNECT_EVENT]'
+    DISCONNECT_PRINT_MSG = '[DISCONNECT_EVENT]'
+    WRITE_EVENT_PRINT_MSG = "[WRITE_VIRTUAL_PIN_EVENT] Pin: V{} Value: '{}'"
+    READ_PRINT_MSG = "[READ_VIRTUAL_PIN_EVENT] Pin: V{}"
+    ALLOWED_COMMANDS_LIST = ['ls', 'lsusb', 'ip a', 'ip abc']
+    TWEET_MSG = "New value='{}' on VPIN({})"
 
-while True:
-    try:
-       blynk.run()
-       if bootup :
-          bootup = False
-          now = datetime.now()
-          blynk.virtual_write(99, now.strftime("%d/%m/%Y %H:%M:%S"))
-          blynk.virtual_write(98, "clr")
-          blynk.virtual_write(98, "System now updated and restarted " + '\n')
-          blynk.virtual_write(255, 0)
-          _log.info('Just Booted')
-          
-       timer.run()
-    except:
-       _log.info('Unexpected error')
-       os.system('sh /home/pi/updateDroneponics.sh')
-       os.system('sudo reboot')
-    
+
+    @blynk.handle_event('write V255')
+    def rebooter(pin, value):
+        blynk.virtual_write(98, "User Reboot " + '\n')
+        os.system('sh /home/pi/updateDroneponics.sh')
+        blynk.virtual_write(98, "System updated and restarting " + '\n')
+        os.system('sudo reboot')
+
+
+    @timer.register(interval=10, run_once=False)
+    def blynk_data():
+        _log.info("Update Timer Run")
+        Counter.cycle += 1
+        now = datetime.now()
+        blynk.virtual_write(0, now.strftime("%d/%m/%Y %H:%M:%S"))    
+        blynk.virtual_write(98, "Completed Timer Function" + '\n') 
+
+
+
+    while True:
+        try:
+           blynk.run()
+           if bootup :
+              bootup = False
+              now = datetime.now()
+              blynk.virtual_write(99, now.strftime("%d/%m/%Y %H:%M:%S"))
+              blynk.virtual_write(98, "clr")
+              blynk.virtual_write(98, "System now updated and restarted " + '\n')
+              blynk.virtual_write(255, 0)
+              _log.info('Just Booted')
+
+           timer.run()
+        except:
+           _log.info('Unexpected error')
+           os.system('sh /home/pi/updateDroneponics.sh')
+           os.system('sudo reboot') 
+except:
+   _log.info('Unexpected error')
+   blynkErr = blynklib.Blynk(BLYNK_AUTH)
+   blynk.virtual_write(98, "System has error" + '\n')
+   os.system('sh /home/pi/updateDroneponics.sh')
+   os.system('sudo reboot')
