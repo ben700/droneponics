aa46a0b8b4f7b0289e1b4fbc98211e03879c2305
ben700 <ben700@hotmail.com>
2020-03-25 17:59:23 +0000

    Update fRelay.py

--- fRelay.py
+++ fRelay.py
@@ -116,10 +116,10 @@ try:
     @blynk.handle_event('write V1')
     def buttonV1Pressed(pin, value):
         blynk.virtual_write(98, "User button 1 " + '\n')
-        blynk.virtual_write(98, int(value[0]) + '\n')
+        blynk.virtual_write(98, value[0] + '\n')
         blynk.virtual_write(98, str(colours) + '\n')
         blynk.virtual_write(1, str(value[0]))
-      #  blynk.set_property(5, 'color', colours[1])
+        blynk.set_property(5, 'color', colours[0])
         if(value[0] == '1'):
             blynk.virtual_write(98,"Waste turned off" + '\n')
             GPIO.output(Relay1,GPIO.HIGH)
