482ff0472f7c3647b9140acf54aff3439973b57d
ben700 <ben700@hotmail.com>
2020-03-29 21:37:12 +0100

    Update nutrientSchedule.py

--- nutrientSchedule.py
+++ nutrientSchedule.py
@@ -8,10 +8,10 @@ class Dose:
        self.volumePin = volumePin	
        self.volume = 0	
             
-def buildNutrientMix(nutrientMix):        
-        nutrientMix.append( Dose(111, 6.00, LED[1], "Hydro Bloom A", VolumePin[1])) 
-        nutrientMix.append( Dose(112, 6.00, LED[2], "Hydro Bloom B", VolumePin[2])) 
-        nutrientMix.append( Dose(113, 10, LED[3], "Ignition", VolumePin[3]))
-        nutrientMix.append( Dose(114, 4, LED[4], "Enzyme", VolumePin[4]))
-        nutrientMix.append( Dose(115, 1, LED[5], "Magne-Cal", VolumePin[5])) 
-    return nutrientMix
+   def buildNutrientMix(nutrientMix):        
+            nutrientMix.append( Dose(111, 6.00, LED[1], "Hydro Bloom A", VolumePin[1])) 
+            nutrientMix.append( Dose(112, 6.00, LED[2], "Hydro Bloom B", VolumePin[2])) 
+            nutrientMix.append( Dose(113, 10, LED[3], "Ignition", VolumePin[3]))
+            nutrientMix.append( Dose(114, 4, LED[4], "Enzyme", VolumePin[4]))
+            nutrientMix.append( Dose(115, 1, LED[5], "Magne-Cal", VolumePin[5])) 
+        return nutrientMix
