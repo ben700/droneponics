e35aa850a493c647c75bb9b8df890fc518a20605
ben700 <ben700@hotmail.com>
2020-03-30 00:31:37 +0100

    Update doser.py

--- doser.py
+++ doser.py
@@ -112,8 +112,13 @@ try:
            if(dosage.pump is not None):
                    blynk.set_property(dosage.LED, 'color', colours[0])
                    dosage.pump.query("D,"+str(dosage.dose))
-                 #  while (str(dosage.pump.query("R").split(":")[1].strip().rstrip('\x00')) != str(dosage.dose)):
-                 #      _log.info( "Pump id " + str(dosage.pumpId) + " has dosed = " + str(dosage.pump.query("R").split(",")[1]) + '\n')
+                   while (True):
+                        dosed = dosage.pump.query("R").split(":")[1].strip().rstrip('\x00')
+                        _log.info( "Pump id " + str(dosage.pumpId) + " has dosed = " + str(dosed) + "ml of 10ml")
+                        _log.info(str(dosed))
+                        _log.info(str(dosage.dose))
+                        if (str(dosed) == str(dosage.dose)):
+                            break	
                    dosage.volume = dosage.pump.query("TV,?").split("TV,")[1]
                    blynk.virtual_write(dosage.volumePin, dosage.volume )
                    _log.info( "Pump id " + str(dosage.pumpId) + " has dosed = " + str(dosage.volume) + '\n')
