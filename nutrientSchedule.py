e332cb58b2e211af61acc255c7d15a4c9f648937
ben700 <ben700@hotmail.com>
2020-03-29 21:34:59 +0100

    Update nutrientSchedule.py

--- nutrientSchedule.py
+++ nutrientSchedule.py
@@ -4,7 +4,8 @@ def buildNutrientMix(nutrientMix):
         nutrientMix.append( Dose(113, 10, LED[3], "Ignition", VolumePin[3]))
         nutrientMix.append( Dose(114, 4, LED[4], "Enzyme", VolumePin[4]))
         nutrientMix.append( Dose(115, 1, LED[5], "Magne-Cal", VolumePin[5])) 
-    return nutrientMix     
+    return nutrientMix
+
 
 class Dose:
    def __init__(self, PumpId, Dose, Led, name, volumePin):
