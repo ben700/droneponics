acb11255114a9fa9cf3280b11731f961f029d8a6
ben700 <ben700@hotmail.com>
2020-03-29 22:39:20 +0100

    Update nutrientSchedule.py

--- drone/nutrientSchedule.py
+++ drone/nutrientSchedule.py
@@ -7,7 +7,7 @@ VolumePin = [0,21,22,23,24,25]
 def buildNutrientMix(nutrientMix, _log):
     _log.info("now building buildNutrientMix")        
     #nutrientMix.append( Dose(111, 6.00, LED[1], "Hydro Bloom A", VolumePin[1]))
-    nutrientMix.append( 111)
+    nutrientMix.append( (111, 6.00, LED[1], "Hydro Bloom A", VolumePin[1]))
     _log.info("Added first object")                
     #nutrientMix.append( Dose(112, 6.00, LED[2], "Hydro Bloom B", VolumePin[2])) 
     #nutrientMix.append( Dose(113, 10, LED[3], "Ignition", VolumePin[3]))
