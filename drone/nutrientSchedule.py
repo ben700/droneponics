ff87d79bf1829fc47d9751444b93d3222b50f34f
ben700 <ben700@hotmail.com>
2020-03-30 00:32:45 +0100

    Update nutrientSchedule.py

--- drone/nutrientSchedule.py
+++ drone/nutrientSchedule.py
@@ -8,7 +8,7 @@ def buildNutrientMix(nutrientMix, _log):
     _log.info("now building buildNutrientMix")        
     nutrientMix.append( Dose(111, 6.00, LED[1], "Hydro Bloom A", VolumePin[1]))
     nutrientMix.append( Dose(112, 6.00, LED[2], "Hydro Bloom B", VolumePin[2])) 
-    nutrientMix.append( Dose(113, 10, LED[3], "Ignition", VolumePin[3]))
-    nutrientMix.append( Dose(114, 4, LED[4], "Enzyme", VolumePin[4]))
-    nutrientMix.append( Dose(115, 1, LED[5], "Magne-Cal", VolumePin[5])) 
+    nutrientMix.append( Dose(113, 10.00, LED[3], "Ignition", VolumePin[3]))
+    nutrientMix.append( Dose(114, 4.00, LED[4], "Enzyme", VolumePin[4]))
+    nutrientMix.append( Dose(115, 1.00, LED[5], "Magne-Cal", VolumePin[5])) 
     return nutrientMix
