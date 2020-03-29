baa3423c6c15acbaba49244e6ac717014488a655
ben700 <ben700@hotmail.com>
2020-03-29 19:42:21 +0100

    Update doserCal.py

--- doserCal.py
+++ doserCal.py
@@ -102,7 +102,7 @@ try:
                    #_log.info( "Pump id " + str(dosage.pumpId) + " has dosed = " + str(dosage.pump.query("R").split(",")[1]) + '\n')
                         dosed = dosage.pump.query("R").split(":")[1].strip()
                         _log.info("["+ str(dosed) +"]")
-                        if (str(dosed) == "10.00"):
+                        if (str(dosed).strip() is str("10.00").strip()):
                             _log.info("break")
                             break
                   		
