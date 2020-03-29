99404bf8a231ee7e75e901308d96f6aa41e75791
ben700 <ben700@hotmail.com>
2020-03-29 18:44:41 +0100

    Update doserCal.py

--- doserCal.py
+++ doserCal.py
@@ -1,17 +1,58 @@
-from datetime import datetime
-import io
-import sys
-import fcntl
-import time
-import copy
-import string
-from AtlasI2C import (
-	 AtlasI2C
-)
+##!/usr/bin/env python3 
+BLYNK_AUTH = 'e06jzpI2zuRD4KB5eHyHdCQTGFT7einR' #i2cLogger
+BLYNK_AUTH_DATA = 'XVbhfI6ZYxkqFp7d4RsCIN6Is9YnKp9q' #i2cLogger
+
+
+try:
+    from python_tsl2591 import tsl2591
+    import datetime
+    import time
+    import shlex, requests
+    import board
+    import busio
+    import smbus 
+    from ctypes import c_short
+    from ctypes import c_byte
+    from ctypes import c_ubyte
+    import mh_z19
+    import blynklib
+    import blynktimer
+    import logging
+    from datetime import datetime
+    import adafruit_tsl2591
+    import sys
+    import os
+    import RPi.GPIO as GPIO
+
+    from AtlasI2C import (
+	    AtlasI2C
+    )
+    
+    import subprocess
+    import re
+
+    class Counter:
+        cycle = 0
+
+    bootup = True
+    colours = {1: '#FF0000', 0: '#00FF00', 'OFFLINE': '#0000FF'}
+
+
+    # tune console logging
+    _log = logging.getLogger('BlynkLog')
+    logFormatter = logging.Formatter("%(asctime)s [%(levelname)s]  %(message)s")
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(logFormatter)
+    _log.addHandler(consoleHandler)
+    _log.setLevel(logging.DEBUG)
 
 
 
-answer = input("Are you sure you want to calibrate (y/n)")
-print(answer)
-if answer is None or answer != 'y':
-    quit()
+    answer = input("Are you sure you want to calibrate (y/n)")
+    print(answer)
+    if answer is None or answer != 'y':
+        quit()
+  
+  
+except:
+   _log.info('Unexpected error')
