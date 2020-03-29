b54329c312a2048aa2aa2b3badaef4eec86730b0
ben700 <ben700@hotmail.com>
2020-03-29 22:46:36 +0100

    Update doser.py

--- doser.py
+++ doser.py
@@ -30,6 +30,7 @@ try:
     
     import subprocess
     import re
+    import drone
 
     class Counter:
         cycle = 0
@@ -46,28 +47,8 @@ try:
     _log.addHandler(consoleHandler)
     _log.setLevel(logging.DEBUG)
 
-
-    class Dose:
-        def __init__(self, PumpId, Dose, Led, name, volumePin):
-            self.pump = None
-            self.pumpId = PumpId
-            self.dose = Dose
-            self.LED = Led
-            self.name = name
-            self.volumePin = volumePin	
-            self.volume = 0	
-    
-
-    LED = [10,11,12,13,14,15]
-    VolumePin = [0,21,22,23,24,25]
-
     nutrientMix = []
-    nutrientMix.append( Dose(111, 6.00, LED[1], "Hydro Bloom A", VolumePin[1])) 
-    nutrientMix.append( Dose(112, 6.00, LED[2], "Hydro Bloom B", VolumePin[2])) 
-    nutrientMix.append( Dose(113, 10, LED[3], "Ignition", VolumePin[3]))
-    nutrientMix.append( Dose(114, 4, LED[4], "Enzyme", VolumePin[4]))
-    nutrientMix.append( Dose(115, 1, LED[5], "Magne-Cal", VolumePin[5])) 
-
+    nutrientMix = drone.buildNutrientMix(nutrientMix, _log)
 	
     
     # Initialize Blynk
