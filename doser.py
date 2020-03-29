e5695056d4b9a0c9b1bd4d5e5fdd318805a37c92
ben700 <ben700@hotmail.com>
2020-03-30 00:04:54 +0100

    Update doser.py

--- doser.py
+++ doser.py
@@ -136,7 +136,7 @@ try:
         
         nutrientMix[0].volume = nutrientMix[0].pump.query("TV,?").split("TV,")[1]
         blynk.virtual_write(nutrientMix[0].volumePin, nutrientMix[0].volume )
-        dosed = dosage.pump.query("R").split(":")[1].strip().rstrip('\x00')
+        dosed = nutrientMix[0].pump.query("R").split(":")[1].strip().rstrip('\x00')
         _log.info( "Pump id " + str(dosage.pumpId) + " has dosed = " + str(dosed) + "ml of 10ml")
                         
     @timer.register(interval=10, run_once=False)
