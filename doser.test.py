0a5364def2b773dc2de201b5a45bbf0ade4b81e7
ben700 <ben700@hotmail.com>
2020-03-29 21:39:00 +0100

    Update doser.test.py

--- doser.test.py
+++ doser.test.py
@@ -51,7 +51,7 @@ try:
     VolumePin = [0,21,22,23,24,25]
 
     nutrientMix = []
-    nutrientMix = Dose.buildNutrientMix(nutrientMix)
+    nutrientMix = nutrientSchedule.buildNutrientMix(nutrientMix)
 
 
     answer = input("Are you sure you want to test (y/n)")
