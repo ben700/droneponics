e051863b00ad94de606b6c7e3a8b265d6c3ee6cf
ben700 <ben700@hotmail.com>
2020-03-29 18:40:50 +0100

    Create doserCal.py

---
+++ doserCal.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+import io
+import sys
+import fcntl
+import time
+import copy
+import string
+from AtlasI2C import (
+	 AtlasI2C
+)
+
+
+
+answer = input("Are you sure you want to calibrate (y/n)")
+if answer is None or answer != 'y':
+    quit()
