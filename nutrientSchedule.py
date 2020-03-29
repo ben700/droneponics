05114b0e60b7afe0f9f94a96f998b7120e45b2e2
ben700 <ben700@hotmail.com>
2020-03-29 22:01:56 +0100

    Update nutrientSchedule.py

--- nutrientSchedule.py
+++ nutrientSchedule.py
@@ -1,13 +1,4 @@
-class Dose:
-   def __init__(self, PumpId, Dose, Led, name, volumePin):
-       print("Building object")
-       self.pump = None
-       self.pumpId = PumpId
-       self.dose = Dose
-       self.LED = Led
-       self.name = name
-       self.volumePin = volumePin	
-       self.volume = 0	
+import Dose from droneObjects
             
             
  
