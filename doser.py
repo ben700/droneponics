6f2d28c87d0b098f86676c2e17a85ebfb926645a
ben700 <ben700@hotmail.com>
2020-03-30 04:35:54 +0100

    Update doser.py

--- doser.py
+++ doser.py
@@ -184,14 +184,14 @@ try:
         blynk.set_property(nutrientMix[x].LED, 'color', colours[value[0]])
         blynk.set_property(LED[0], 'color', colours[value[0]])
         if(value[0] == '1'):
-            _log.info("Pump for " +nutrientMix[x].name +" = " + nutrientMix[x].pump.query("X") + '\n')
+            _log.info("stop Pump for " +nutrientMix[x].name +" = " + nutrientMix[x].pump.query("X") + '\n')
             dosed = nutrientMix[x].pump.query("R").split(":")[1].strip().rstrip('\x00')
             nutrientMix[x].volume = nutrientMix[x].pump.query("TV,?").split("TV,")[1]
             blynk.virtual_write(nutrientMix[x].volumePin, nutrientMix[x].volume )
             blynk.virtual_write(98, now.strftime("%d/%m/%Y %H:%M:%S") + " :- Had used " + lVolume + " ml| Now Dosed :"+ str(nutrientMix[x].volume) + "ml" + '\n') 
             blynk.virtual_write(98, now.strftime("%d/%m/%Y %H:%M:%S") + " :- Pump for " + nutrientMix[x].name + ":- STOPPED"  + " Dosed :"+ str(dosed) + "ml" + '\n') 
         else:
-            _log.info("Pump for " +nutrientMix[x].name +" = " + nutrientMix[x].pump.query("D,*") + '\n') 
+            _log.info("start Pump for " +nutrientMix[x].name +" = " + nutrientMix[x].pump.query("D,*") + '\n') 
             blynk.virtual_write(98, now.strftime("%d/%m/%Y %H:%M:%S") + " :- Pump for " + nutrientMix[x].name + ":- STARTED" + '\n') 
 
     @blynk.handle_event('write V43')
