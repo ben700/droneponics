b65c1db60b6a09ce68bf7932bc17a14edaf30783
ben700 <ben700@hotmail.com>
2020-03-30 19:07:51 +0100

    Create atlasTempExportCal.py

---
+++ atlas/calibration/atlasTempExportCal.py
@@ -0,0 +1,54 @@
+##!/usr/bin/env python3 
+BLYNK_AUTH = 'SHraFqInf27JKowTcFZapu0rHH2QGtuO' #atlasMonitor
+
+if True:
+    import datetime
+    import time
+    import shlex, requests
+    import logging
+    from datetime import datetime
+    import sys
+    import os
+    sys.path.append('/home/pi/droneponics')
+    from AtlasI2C import (
+	    AtlasI2C
+    )
+    import blynklib
+    import blynktimer
+    
+    import subprocess
+    import re
+    import drone
+
+    class Counter:
+        cycle = 0
+
+    bootup = True
+    colours = {1: '#FF0000', 0: '#00FF00', 'OFFLINE': '#0000FF'}
+
+
+    # tune console logging
+    _log = logging.getLogger('BlynkLog')
+    logFormatter = logging.Formatter("%(asctime)s [%(levelname)s]  %(message)s")
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(logFormatter)
+    _log.addHandler(consoleHandler)
+    _log.setLevel(logging.DEBUG)
+  
+    # Initialize the sensor.
+    try:
+        temp = AtlasI2C(102)
+
+        _log.info("sensor created")
+    except:
+        _log.info("Unexpected error: Atlas")
+    else:
+        try:	
+             print(temp.query("Export,?"))
+             for i in range(11):
+                 print(temp.query("Export,?"))
+		   
+        except:
+            _log.info("Expected error: Use Atlas Error")
+            
+ 
