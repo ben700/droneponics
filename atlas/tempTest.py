fd92629ce89db5a2682d84d37d30ce3f262c3fb8
ben700 <ben700@hotmail.com>
2020-03-30 13:28:44 +0100

    Update tempTest.py

--- atlas/tempTest.py
+++ atlas/tempTest.py
@@ -16,16 +16,6 @@ try:
 	    AtlasI2C
     )
     
-    import subprocess
-    import re
-    import drone
-
-    class Counter:
-        cycle = 0
-
-    bootup = True
-    colours = {1: '#FF0000', 0: '#00FF00', 'OFFLINE': '#0000FF'}
-
 
     # tune console logging
     _log = logging.getLogger('BlynkLog')
@@ -34,9 +24,6 @@ try:
     consoleHandler.setFormatter(logFormatter)
     _log.addHandler(consoleHandler)
     _log.setLevel(logging.DEBUG)
-
-    nutrientMix = []
-    nutrientMix = drone.buildNutrientMix(nutrientMix, _log)
 	  
     # Initialize the sensor.
     try:
@@ -50,7 +37,7 @@ try:
              cTemp = temp.query("R,").split(":")[1]
              while ( cTemp != oTemp):
                  oTemp = cTemp
-                 sleep(1)
+                 time.sleep(1)
                  cTemp = temp.query("R,").split(":")[1]
                  _log.info("Waiting for temp to be stable. It's now :" + str(cTemp) + '\n')
                            
