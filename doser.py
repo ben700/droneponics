aa69391871fa28bbdbe000b055dafa41dde719b4
ben700 <ben700@hotmail.com>
2020-03-30 03:20:33 +0100

    Update doser.py

--- doser.py
+++ doser.py
@@ -154,8 +154,9 @@ try:
             blynk.virtual_write(98, now.strftime("%d/%m/%Y %H:%M:%S") + " :- Pump for " + nutrientMix[x].name + ":- STARTED" + '\n') 
 
     @blynk.handle_event('write V200')
-    def clearCounters(pin, value):        
-	blynk.virtual_write(98, now.strftime("%d/%m/%Y %H:%M:%S") + " :- Reset Pump Counters " + '\n') 
+    def clearCounters(pin, value):    
+        now = datetime.now()
+        blynk.virtual_write(98, now.strftime("%d/%m/%Y %H:%M:%S") + " :- Reset Pump Counters " + '\n') 
 
         
                         
