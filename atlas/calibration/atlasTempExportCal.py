d7d3f51d6250f42450cf91066337dbe1b362de99
ben700 <ben700@hotmail.com>
2020-03-30 19:39:12 +0100

    Update atlasTempExportCal.py

--- atlas/calibration/atlasTempExportCal.py
+++ atlas/calibration/atlasTempExportCal.py
@@ -47,7 +47,7 @@ if True:
              x =x+1
              for i in range(x):
                 y = temp.query("Export").split(":")[1].strip().rstrip('^@')	
-		y = y.encode('ascii', errors='ignore').decode("utf-8")
+                y = y.encode('ascii', errors='ignore').decode("utf-8")
                 f.write(y + '\n')
                 print(y)
              f.close()
