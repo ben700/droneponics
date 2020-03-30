be792a004361731e1a291970965c45b1b57be229
ben700 <ben700@hotmail.com>
2020-03-30 05:29:18 +0100

    Update doser.py

--- doser.py
+++ doser.py
@@ -105,7 +105,7 @@ try:
         
     @blynk.handle_event('write V255')
     def rebooter(pin, value):
-	_log.info( "User reboot")	
+        _log.info( "User reboot")	
         blynk.virtual_write(98, "User Reboot " + '\n')
         for l in LED:
             blynk.set_property(l, 'color', colours['OFFLINE'])
