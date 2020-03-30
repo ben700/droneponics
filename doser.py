2a0f1f0a7dc8026770cb409a4efb60c49152e044
ben700 <ben700@hotmail.com>
2020-03-30 03:04:17 +0100

    Update doser.py

--- doser.py
+++ doser.py
@@ -145,9 +145,9 @@ try:
         if(value[0] == '1'):
             _log.info("Pump for " +nutrientMix[x].name +" = " + nutrientMix[x].pump.query("X") + '\n')
             dosed = nutrientMix[x].pump.query("R").split(":")[1].strip().rstrip('\x00')
-            dosage.volume = dosage.pump.query("TV,?").split("TV,")[1]
+            nutrientMix[x].volume = dosage.pump.query("TV,?").split("TV,")[1]
             blynk.virtual_write(dosage.volumePin, dosage.volume )
-            blynk.virtual_write(98, now.strftime("%d/%m/%Y %H:%M:%S") + " :- Had used " + lVolume + " ml| Now Dosed :"+ str(dosage.volume) + "ml" + '\n') 
+            blynk.virtual_write(98, now.strftime("%d/%m/%Y %H:%M:%S") + " :- Had used " + lVolume + " ml| Now Dosed :"+ str(nutrientMix[x].volume) + "ml" + '\n') 
             blynk.virtual_write(98, now.strftime("%d/%m/%Y %H:%M:%S") + " :- Pump for " + nutrientMix[x].name + ":- STOPPED"  + " Dosed :"+ str(dosed) + "ml" + '\n') 
         else:
             _log.info("Pump for " +nutrientMix[x].name +" = " + nutrientMix[x].pump.query("D,*") + '\n') 
