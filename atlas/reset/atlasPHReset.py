fc203799e6429c1efe9716fb2f7715ca26a88525
ben700 <ben700@hotmail.com>
2020-03-30 14:13:51 +0100

    Create atlasPHReset.py

---
+++ atlas/reset/atlasPHReset.py
@@ -0,0 +1,47 @@
+##!/usr/bin/env python3 
+BLYNK_AUTH = 'SHraFqInf27JKowTcFZapu0rHH2QGtuO' #atlasMonitor
+
+if True:
+    import datetime
+    import time
+    import shlex, requests
+    import logging
+    from datetime import datetime
+    import sys
+    import os
+    sys.path.append('/home/pi/droneponics')
+    from AtlasI2C import (
+	    AtlasI2C
+    )
+    
+    import subprocess
+    import re
+
+    # tune console logging
+    _log = logging.getLogger('BlynkLog')
+    logFormatter = logging.Formatter("%(asctime)s [%(levelname)s]  %(message)s")
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(logFormatter)
+    _log.addHandler(consoleHandler)
+    _log.setLevel(logging.DEBUG)
+
+    # Initialize the sensor.
+    try:
+        temp = AtlasI2C(102)
+        ec = AtlasI2C(100)
+        ph = AtlasI2C(99)
+
+        _log.info("sensor created")
+    except:
+        _log.info("Unexpected error: Atlas")
+    else:
+        try:	
+             answer = input("Are you sure you want to reset PH (y/n)")
+             if answer == 'y':
+                      ph.query("Factory")
+                      _log.info("PH Reset")
+		   
+        except:
+            _log.info("Expected error: Use Atlas Error")
+            
+ 
