99228ec851349ad56c302aacb126bd38c53e3e49
ben700 <ben700@hotmail.com>
2020-04-03 16:37:44 +0100

    Add files via upload

--- doser.py
+++ doser.py
@@ -307,7 +307,7 @@ try:
     def blynk_data():
         _log.info("Update Timer Run")
         blynk.virtual_write(98, "Starting Timer Function" + '\n') 
-        Counter.cycle += 1
+        #Counter.cycle += 1
         now = datetime.now()
         blynk.virtual_write(0, now.strftime("%d/%m/%Y %H:%M:%S"))    
        # drone.readSensors(sensors, _log)
