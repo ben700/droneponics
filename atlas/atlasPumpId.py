a0463b652cf128559a92e75424fd6b96aaf6e0a6
ben700 <ben700@hotmail.com>
2020-04-03 13:09:45 +0100

    Update atlasPumpId.py

--- atlas/atlasPumpId.py
+++ atlas/atlasPumpId.py
@@ -34,7 +34,7 @@ if True:
     try:
        # Create the I2C bus
        pump = AtlasI2C(103)
-       _log.info(pump.query("I2C,112")
+       _log.info(pump.query("I2C,112"))
        _log.info("pump created")
     except:
         _log.info("Unexpected error: Atlas")
