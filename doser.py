b2a630baf136f521b7a18bc874a7f59116fbc2e1
ben700 <ben700@hotmail.com>
2020-04-03 16:49:19 +0100

    Add files via upload

--- doser.py
+++ doser.py
@@ -309,11 +309,12 @@ try:
         blynk.virtual_write(98, "Starting Timer Function" + '\n') 
         #Counter.cycle += 1
         now = datetime.now()
-        blynk.virtual_write(0, now.strftime("%d/%m/%Y %H:%M:%S"))   
+        blynk.virtual_write(0, now.strftime("%d/%m/%Y %H:%M:%S"))
+        
         blynk.virtual_write(98, "Read Sensors" + '\n') 
-        sensors[0].value = sensor[0].query("R").split(":")[1] #Temp
-        sensors[1].value = sensor[1].query("RT,"+sensors[0].value).split(":")[1] #EC
-        sensors[2].value = sensor[2].query("RT,"+sensors[0].value).split(":")[1] #pH
+        sensors[0].value = sensors[0].sensor.query("R").split(":")[1] #Temp
+        sensors[1].value = sensors[1].sensor.query("RT,"+sensors[0].value).split(":")[1] #EC
+        sensors[2].value = sensors[2].sensor.query("RT,"+sensors[0].value).split(":")[1] #pH
         blynk.virtual_write(98, "Temp = " + str(sensors[0].value) + '\n') 
         blynk.virtual_write(98, "EC = "  + str(sensors[1].value)+ '\n') 
         blynk.virtual_write(98, "PH = "  + str(sensors[2].value)+ '\n') 
