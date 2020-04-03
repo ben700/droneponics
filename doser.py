5ff01ee1e3a4ca0d6d554d8c2891db963ef076f1
ben700 <ben700@hotmail.com>
2020-04-03 17:19:49 +0100

    Add files via upload

--- doser.py
+++ doser.py
@@ -326,11 +326,12 @@ try:
         
         for sensor in sensors:
              blynk.virtual_write(sensor.displayPin, sensor.value)   
-                
-        #if (sensors[1].target < sensors[1].value):
-        #     doSingleDose()
-        #elif (sensors[0].target > sensors[0].value):
-        #     doSinglePHDose()
+        blynk.virtual_write(98, "EC target = [" +str(sensors[1].target) +"] current EC reading =[" + str(sensors[1].value) + "]" + '\n')
+        blynk.virtual_write(98, "PH target = [" +str(sensors[2].target) +"] current PH reading =[" + str(sensors[2].value) + "]" + '\n')         
+        if (sensors[1].target < sensors[1].value):
+             doSingleDose()
+        elif (sensors[2].target > sensors[2].value):
+             doSinglePHDose()
        
         blynk.virtual_write(98, "Completed Timer Function" + '\n') 
 
