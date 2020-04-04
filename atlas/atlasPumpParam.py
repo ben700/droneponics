848712ac353df11b4e09c5da43fbcf655f174a1c
ben700 <ben700@hotmail.com>
2020-04-04 16:25:48 +0100

    Create atlasPumpParam.py

---
+++ atlas/atlasPumpParam.py
@@ -0,0 +1,40 @@
+##!/usr/bin/env python3 
+
+if True:
+    import datetime
+    import time
+    import shlex, requests
+    import logging
+    from datetime import datetime
+    import sys
+    import os
+    import RPi.GPIO as GPIO
+    sys.path.append('/home/pi/droneponics')
+    from AtlasI2C import (
+	    AtlasI2C
+    )
+    import blynklib
+    import blynktimer
+    
+    import subprocess
+    import re
+    import drone
+
+
+
+    # tune console logging
+    _log = logging.getLogger('BlynkLog')
+    logFormatter = logging.Formatter("%(asctime)s [%(levelname)s]  %(message)s")
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(logFormatter)
+    _log.addHandler(consoleHandler)
+    _log.setLevel(logging.DEBUG)
+    
+    # Initialize the sensor.
+    try:
+       # Create the I2C bus
+       pump = AtlasI2C(111)
+       _log.info(pump.query("O,?"))
+    except:
+        _log.info("Unexpected error: Atlas")
+    
