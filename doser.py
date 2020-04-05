473ad9ff32cc5beddab9e0152eeb06730be9cda1
ben700 <ben700@hotmail.com>
2020-04-05 01:49:29 +0100

    Update doser.py

--- doser.py
+++ doser.py
@@ -24,7 +24,7 @@ import re
 import drone
 
 bootup = True
-colours = {0: '#FF0000', 1: '#00FF00', '0': '#FF0000', '1': '#00FF00', 'OFFLINE': '#0000FF', 'ONLINE': '#00FF00'}
+colours = {0: '#FF0000', 1: '#00FF00', '0': '#FF0000', '1': '#00FF00', 'OFFLINE': '#0000FF', 'ONLINE': '#00FFFF'}
 systemLED=101
 
 
@@ -81,14 +81,14 @@ try:
                    blynk.set_property(dosage.volumePin, 'label', dosage.name + "-TVP")
                    dosage.volume = dosage.pump.query("ATV,?").split("TV,")[1].strip().rstrip('\x00')
                    blynk.virtual_write(dosage.volumePin, dosage.volume )
-                   blynk.virtual_write(98, dosage.pump.query("O,V,1").strip().rstrip('\x00') + '\n')
-                   blynk.virtual_write(98, dosage.pump.query("O,TV,1").strip().rstrip('\x00') + '\n')
-                   blynk.virtual_write(98, dosage.pump.query("O,ATV,1").strip().rstrip('\x00') + '\n')
-                   blynk.virtual_write(98, dosage.pump.query("O,?").strip().rstrip('\x00') + '\n')
-                   blynk.virtual_write(98, "TV =" + dosage.pump.query("TV,?").strip().rstrip('\x00') + '\n')
-                   blynk.virtual_write(98, "ATV =" + dosage.pump.query("ATV,?").strip().rstrip('\x00') + '\n')
-                   blynk.virtual_write(98, "R =" + dosage.pump.query("R").strip().rstrip('\x00') + '\n')
-                   blynk.virtual_write(98, "Pump id " + str(dosage.pumpId) + " has dosed = " + str(dosage.volume) + '\n')
+                   blynk.virtual_write(98, dosage.name + " " + dosage.pump.query("O,V,1").strip().rstrip('\x00') + '\n')
+                   blynk.virtual_write(98, dosage.name + " " + dosage.pump.query("O,TV,1").strip().rstrip('\x00') + '\n')
+                   blynk.virtual_write(98, dosage.name + " " + dosage.pump.query("O,ATV,1").strip().rstrip('\x00') + '\n')
+                   blynk.virtual_write(98, dosage.name + " " + dosage.pump.query("O,?").strip().rstrip('\x00') + '\n')
+                   blynk.virtual_write(98, dosage.name + " TV =" + dosage.pump.query("TV,?").strip().rstrip('\x00') + '\n')
+                   blynk.virtual_write(98, dosage.name + " ATV =" + dosage.pump.query("ATV,?").strip().rstrip('\x00') + '\n')
+                   blynk.virtual_write(98, dosage.name + " R =" + dosage.pump.query("R").strip().rstrip('\x00') + '\n')
+                   blynk.virtual_write(98, dosage.name + " Pump id " + str(dosage.pumpId) + " has dosed = " + str(dosage.volume) + '\n')
             _log.info("Pumps all read")          
         except:
             _log.info("Expected error: Use Atlas Error")
@@ -111,8 +111,8 @@ try:
         blynk.virtual_write(98, now.strftime("%d/%m/%Y %H:%M:%S") + " Going to Dose" + '\n')
         for dosage in nutrientMix:           
 		
-           blynk.virtual_write(98, "TV =" + dosage.pump.query("TV,?").strip().rstrip('\x00') + '\n')
-           blynk.virtual_write(98, "ATV =" + dosage.pump.query("ATV,?").strip().rstrip('\x00') + '\n')
+           blynk.virtual_write(98, dosage.name + " TV =" + dosage.pump.query("TV,?").strip().rstrip('\x00') + '\n')
+           blynk.virtual_write(98, dosage.name + " ATV =" + dosage.pump.query("ATV,?").strip().rstrip('\x00') + '\n')
            blynk.virtual_write(98, "Read "+dosage.name+" =" + dosage.pump.query("R").strip().rstrip('\x00') + '\n')
 			
            if(dosage.pump is not None and dosage.name != "pH"):
