62a523d3684c24771fcfa39fd70cf09a7d08c83c
ben700 <ben700@hotmail.com>
2020-04-07 01:42:24 +0100

    Update relay.py

--- relay.py
+++ relay.py
@@ -1,42 +1,103 @@
-import BlynkLib
+##!/usr/bin/env python3 
+BLYNK_AUTH = 'iipK7r0pSz68i8ZDo4sVdtkhbCzXM_ns' #relay
+
+import blynklib
+import blynktimer
+
 from datetime import datetime
+import time
 
-
-BLYNK_AUTH = 'jX8rstxGafpXQU9ChKVCliPHRlmgjHHp' #motherLights
-
-# Initialize Blynk
-blynk = BlynkLib.Blynk(BLYNK_AUTH)
-
+import logging
+import sys
+import os
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
-GPIO.setup(37, GPIO.OUT)
 
-@blynk.on("connected")
-def connect_handler():
-   blynk.sync_virtual(38,39)
-        
-@blynk.on("V38")
-def V38Pressed(value):
-    now = datetime.now()
-    blynk.virtual_write(36, now.strftime("%d/%m/%Y %H:%M:%S"))
-    if(value[0] == '1'):
-        print("Lights turned on")
-        GPIO.output(37,GPIO.HIGH)
-        blynk.virtual_write(39, 255)
-        blynk.set_property(39, 'color', '#00ff00')
-    else:
-        print("Lights turned off")
-        GPIO.output(37,GPIO.LOW)
-        blynk.virtual_write(39, 0)
-        blynk.set_property(39,'color','#D3435C')
+from AtlasI2C import (
+   AtlasI2C
+)
+import math  
+import subprocess
+import re
+import drone
+
+bootup = True
+colours = {0: '#FF0000', 1: '#00FF00', '0': '#FF0000', '1': '#00FF00', 'OFFLINE': '#0000FF', 'ONLINE': '#00FF00'}
+systemLED=101
+
+
+if True:
+
+    # tune console logging
+    _log = logging.getLogger('BlynkLog')
+    logFormatter = logging.Formatter("%(asctime)s [%(levelname)s]  %(message)s")
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(logFormatter)
+    _log.addHandler(consoleHandler)
+    _log.setLevel(logging.DEBUG)
+
+
+    relays = []
+    relays = drone.buildRelay(relays, _log)
     
-###########################################################
-# infinite loop that waits for event
-###########################################################
-try:
+    # Initialize Blynk
+    blynk = blynklib.Blynk(BLYNK_AUTH)        
+    timer = blynktimer.Timer()
+    blynk.run()
+    #blynk.virtual_write(98, "clr")
+    blynk.set_property(systemLED, 'color', colours['ONLINE'])
+   
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    for relay in relays:
+         GPIO.setup(relay.pinId, GPIO.OUT)
+    
+            
+    APP_CONNECT_PRINT_MSG = '[APP_CONNECT_EVENT]'
+    APP_DISCONNECT_PRINT_MSG = '[APP_DISCONNECT_EVENT]'
+    CONNECT_PRINT_MSG = '[CONNECT_EVENT]'
+    DISCONNECT_PRINT_MSG = '[DISCONNECT_EVENT]'
+    WRITE_EVENT_PRINT_MSG = "[WRITE_VIRTUAL_PIN_EVENT] Pin: V{} Value: '{}'"
+    READ_PRINT_MSG = "[READ_VIRTUAL_PIN_EVENT] Pin: V{}"
+    ALLOWED_COMMANDS_LIST = ['ls', 'lsusb', 'ip a', 'ip abc']
+    TWEET_MSG = "New value='{}' on VPIN({})"
+	 
+   			
+  
+    @blynk.handle_event('write V255')
+    def rebooter(pin, value):
+        _log.info( "User reboot")	
+        blynk.virtual_write(98, "User Reboot " + '\n')
+        for l in LED:
+            blynk.set_property(l, 'color', colours['OFFLINE'])
+        blynk.set_property(systemLED, 'color', colours['OFFLINE'])	
+        os.system('sh /home/pi/updateDroneponics.sh')
+        blynk.virtual_write(98, "System updated and restarting " + '\n')
+        os.system('sudo reboot')
+
+	
+    @timer.register(interval=30, run_once=False)
+    def blynk_data():
+        _log.info("Update Timer Run")
+        now = datetime.now()
+        blynk.virtual_write(0, now.strftime("%d/%m/%Y %H:%M:%S"))
+        _log.info("Completed Timer Function") 
+
     while True:
-        blynk.run()
-except KeyboardInterrupt:
-    blynk.disconnect()
-    print('SCRIPT WAS INTERRUPTED')
+        if True:
+           blynk.run()
+           if bootup :
+              p = subprocess.Popen(['i2cdetect', '-y','1'],stdout=subprocess.PIPE,)
+              #cmdout = str(p.communicate())
+              for i in range(0,9):
+                   blynk.virtual_write(98, str(p.stdout.readline()) + '\n')
+              bootup = False
+              now = datetime.now()
+              blynk.virtual_write(99, now.strftime("%d/%m/%Y %H:%M:%S"))
+              for l in LED:
+                  blynk.virtual_write(l, 255)
+              blynk.virtual_write(systemLED, 255)
+              #blynk.virtual_write(98, "clr")
+              blynk.virtual_write(98, "System now updated and restarted " + '\n')
+              blynk.virtual_write(255, 0)
+              _log.info('Just Booted')
+           timer.run()
