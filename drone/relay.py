fec43148c78aee7202e37531cd210c1d832bd2cd
ben700 <ben700@hotmail.com>
2020-04-07 01:27:10 +0100

    Update relay.py

--- drone/relay.py
+++ drone/relay.py
@@ -6,3 +6,9 @@ class Relay:
        self.LED = Led
        self.name = name
        
+def buildRelay(relays, _log): 
+    relays.append( Relay(26, LED[0], "Air"))
+    relays.append( Relay(13, LED[1], "Mixer"))
+    relays.append( Relay(21, LED[3], "Heater"))
+    relays.append( Relay(27, LED[4], "Other"))
+    return relays
