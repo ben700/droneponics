7ede1e70c161ec2e80c144fe8ad4764927a38f36
ben700 <ben700@hotmail.com>
2020-04-07 02:38:42 +0100

    Update relay.py

--- relay.py
+++ relay.py
@@ -145,8 +145,8 @@ if True:
               bootup = False
               now = datetime.now()
               blynk.virtual_write(99, now.strftime("%d/%m/%Y %H:%M:%S"))
-              for l in LED:
-                  blynk.virtual_write(l, 255)
+              for relay in relays:
+                  blynk.virtual_write(relay.pinId, 255)
               blynk.virtual_write(systemLED, 255)
               #blynk.virtual_write(98, "clr")
               blynk.virtual_write(98, "System now updated and restarted " + '\n')
