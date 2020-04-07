f3cddf43c1cdd9a57985761ce4a3673f8bbc20d4
ben700 <ben700@hotmail.com>
2020-04-07 03:34:03 +0100

    Update relay.py

--- drone/relay.py
+++ drone/relay.py
@@ -1,14 +1,15 @@
 from drone import * 
 class Relay:
-   def __init__(self, PinId, Led, Name):
+   def __init__(self, PinId, Led, Name, Button):
        print("Building object")
        self.pinId = PinId
        self.LED = Led
        self.name = Name
+       self.button = Button
        
 def buildRelay(relays, _log): 
-    relays.append( Relay(26, LED[1], "Air"))
-    relays.append( Relay(13, LED[2], "Mixer"))
-    relays.append( Relay(21, LED[3], "Heater"))
-    relays.append( Relay(27, LED[4], "Other"))
+    relays.append( Relay(26, LED[1], "Air", 1))
+    relays.append( Relay(13, LED[2], "Mixer", 2))
+    relays.append( Relay(21, LED[3], "Heater", 3))
+    relays.append( Relay(27, LED[4], "Other", 4))
     return relays
