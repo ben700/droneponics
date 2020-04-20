4edfb7137e78ef19fa6877108b4726bcd3208c33
ben700 <ben700@hotmail.com>
2020-04-20 05:48:30 +0100

    Update soilAddressChange.py

--- atlas/soilAddressChange.py
+++ atlas/soilAddressChange.py
@@ -6,9 +6,9 @@ from chirp import Chirp
 import sys
 
 if len(sys.argv) < 3:
-	print "Usage: %s old_addr new_addr" % (sys.argv[0])
-	print "Standard Chirp address is 0x20 (32)."
-	print "Use ''i2cdetect -y 1'' to see your current Chirp addresses."
+	print ("Usage: %s old_addr new_addr" % (sys.argv[0]))
+	print ("Standard Chirp address is 0x20 (32).")
+	print ("Use ''i2cdetect -y 1'' to see your current Chirp addresses.")
 	sys.exit()
 
 # Get old address (may be given in decimal or hex)
@@ -27,7 +27,7 @@ else:
 try:
 	chirp1 = Chirp(1, new_addr)
 	chirp1.reset()
-	print "The given new address %d is already in use. %s" % (new_addr, chirp1)
+	print ("The given new address %d is already in use. %s" % (new_addr, chirp1))
 	sys.exit()
 except IOError:
 	pass
@@ -36,9 +36,9 @@ try:
 	# If the following line throws an error, there is no Chirp on old_addr
 	chirp = Chirp(1, old_addr)
 except IOError:
-	print "Cannot find a Chirp on address", old_addr
-	print "Use ''i2cdetect -y 1'' to see your current Chirp addresses."
+	print ("Cannot find a Chirp on address", old_addr)
+	print ("Use ''i2cdetect -y 1'' to see your current Chirp addresses.")
 
-print "Renaming %s to address %d" % (chirp, new_addr)
+print ("Renaming %s to address %d" % (chirp, new_addr))
 chirp.set_addr(new_addr)
-print "Now your Chirp answers:", chirp
+print ("Now your Chirp answers:", chirp)
