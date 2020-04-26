274057040610260692c9218a5b36b0e5b7698912
ben700 <ben700@hotmail.com>
2020-04-26 14:44:02 +0100

    Update updatePi.sh

--- updatePi.sh
+++ updatePi.sh
@@ -3,13 +3,13 @@ python3 /home/pi/droneponics/replace.py /home/pi/droneponics/requirements.txt
 pip3 install -r /home/pi/droneponics/requirements.txt --upgrade
 
 
-#echo "Update PI"
-#sudo apt full-upgrade -y
-#sudo apt update
-#sudo apt full-upgrade -y
-#sudo apt upgrade -y
-#sudo apt dist-upgrade -y
-#sudo apt autoremove -y
-#sudo apt autoclean -y
-#/home/pi/updateDroneponics.sh
+echo "Update PI"
+sudo apt full-upgrade -y
+sudo apt update
+sudo apt full-upgrade -y
+sudo apt upgrade -y
+sudo apt dist-upgrade -y
+sudo apt autoremove -y
+sudo apt autoclean -y
+/home/pi/updateDroneponics.sh
 #sudo reboot
