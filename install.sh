f08ee3dd59572c0f5e9d9440be74f38b53c8a5d1
ben700 <ben700@hotmail.com>
2020-05-06 20:14:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
-cp /home/pi/droneponics/updateDroneponics.sh /home/pi/updateDroneponics.sh
+cd /tmp
+wget https://project-downloads.drogon.net/wiringpi-latest.deb
+sudo dpkg -i wiringpi-latest.deb
+cd
+sudo apt-get install wiringpi
+sudo apt-get install python-pip
+sudo apt-get install xterm -y
+sudo apt-get install lxde
+
 
 sudo apt full-upgrade -y
 sudo apt update
@@ -8,14 +16,5 @@ sudo apt dist-upgrade -y
 sudo apt autoremove -y
 sudo apt autoclean -y
 
-cd /tmp
-wget https://project-downloads.drogon.net/wiringpi-latest.deb
-sudo dpkg -i wiringpi-latest.deb
-cd
-sudo apt-get install wiringpi
-sudo apt-get install python-pip
-sudo apt-get install xterm -y
-sudo apt-get install lxde
+
 sh /home/pi/droneponics/installUsr.sh
-echo "Going to Reboot now"
-sudo reboot
