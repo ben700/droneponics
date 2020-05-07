addb2501029aa7e44d05a64783e3553ac421d437
ben700 <ben700@hotmail.com>
2020-05-07 07:27:36 +0100

    Update droneAir.py

--- droneAir.py
+++ droneAir.py
@@ -63,8 +63,13 @@ _log.setLevel(logging.DEBUG)
  
 openWeather = requests.get("https://api.openweathermap.org/data/2.5/onecall?lat=53.801277&lon=-1.548567&exclude=hourly,daily&units=metric&appid=7ab0c16c9b00854f26df8a57435ad6ce")   
 print(openWeather.text)
-print(openWeather.text['current'])
-print(openWeather.text['current']['pressure'])
+
+
+for key, value in openWeather.text.items():
+    print(key, value)
+  
+print(openWeather.text[0])
+print(openWeather.text["current"]["pressure"])
 print(openWeather.text['current']['weather']['icon'])
 
 try:
