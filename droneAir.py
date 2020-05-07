1f6be925cb60abef9df308e4139c461501d46a62
ben700 <ben700@hotmail.com>
2020-05-07 03:04:28 +0100

    Update droneAir.py

--- droneAir.py
+++ droneAir.py
@@ -181,15 +181,20 @@ try:
         try:
            blynk.run()
            if bootup :
+              
+              _log.info("I2C 1 devices")
               p = subprocess.Popen(['i2cdetect', '-y','0'],stdout=subprocess.PIPE,)
               #cmdout = str(p.communicate())
               for i in range(0,9):
                    blynk.virtual_write(98, str(p.stdout.readline()) + '\n')
+              
+              _log.info("I2C 1 devices")
               q = subprocess.Popen(['i2cdetect', '-y','1'],stdout=subprocess.PIPE,)
               #cmdout = str(p.communicate())
               for i in range(0,9):
                    blynk.virtual_write(98, str(q.stdout.readline()) + '\n')
               bootup = False
+             _log.info("Just about to complete Booting")
               now = datetime.now()
               blynk.virtual_write(99, now.strftime("%d/%m/%Y %H:%M:%S"))
               blynk.virtual_write(98, "clr")
