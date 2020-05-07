6552b2128d8e77459fead6b095077c112cf02dfa
ben700 <ben700@hotmail.com>
2020-05-07 03:44:14 +0100

    Update droneAir.py

--- droneAir.py
+++ droneAir.py
@@ -45,7 +45,7 @@ def setFormOffline(blynkObj=None, Msg=None):
    if Msg is not None:
        blynkObj.virtual_write(98, Msg + " " + '\n')
        _log.info(Msg)
-    _log.info("Going to set from colour Offline from setFormOffline")
+   _log.info("Going to set from colour Offline from setFormOffline")
    for i in range(255): 
       blynkObj.set_property(i, 'color', colours['OFFLINE'])
    _log.info("Completed setFormOffline")
