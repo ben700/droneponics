cfb89e76bc51504b157ff9e0cb2fa23c5cdf5679
ben700 <ben700@hotmail.com>
2020-05-07 02:23:52 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,20 @@
+import datetime
+import time
+import shlex, requests
+import board
+import busio
+import smbus 
+import mh_z19
+import blynklib
+import blynktimer
+import logging
+from datetime import datetime
+import adafruit_tsl2591
+import adafruit_bme680
+from meteocalc import Temp, dew_point
+import sys
+import os
+from configparser import ConfigParser
+
+i2c0 = busio.I2C(board.SCL, board.SDA)
+print(i2c0)
