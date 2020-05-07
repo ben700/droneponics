f4c0a5a88043ef27092d42c200e68b387042aae4
ben700 <ben700@hotmail.com>
2020-05-07 07:19:38 +0100

    Update droneAir.py

--- droneAir.py
+++ droneAir.py
@@ -61,8 +61,8 @@ _log.setLevel(logging.DEBUG)
  
  
  
-#openWeather = requests.get("https://api.openweathermap.org/data/2.5/onecall?lat=53.801277&lon=-1.548567&exclude=hourly,daily&units=metric&appid=7ab0c16c9b00854f26df8a57435ad6ce")   
-#print(openWeather)
+openWeather = requests.get("https://api.openweathermap.org/data/2.5/onecall?lat=53.801277&lon=-1.548567&exclude=hourly,daily&units=metric&appid=7ab0c16c9b00854f26df8a57435ad6ce")   
+print(openWeather.text)
 
 try:
     
