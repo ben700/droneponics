51b4100ecd8668db9316b0ebbd77b62bb8cbd865
ben700 <ben700@hotmail.com>
2020-05-07 03:55:12 +0100

    Update droneAir.py

--- droneAir.py
+++ droneAir.py
@@ -39,19 +39,6 @@ consoleHandler.setFormatter(logFormatter)
 _log.addHandler(consoleHandler)
 _log.setLevel(logging.DEBUG)
 
-def setFormOffline(blynkObj=None, Msg=None):
-   print("in setFormOffline")
-   if blynkObj is None:
-      blynkObj = blynklib.Blynk(parser.get('droneAir', 'BLYNK_AUTH'))
-   blynkObj.run()
-   _log.info("Going to set Msg from setFormOffline")
-   if Msg is not None:
-       blynkObj.virtual_write(98, Msg + " " + '\n')
-       _log.info(Msg)
-   _log.info("Going to set from colour Offline from setFormOffline")
-   for i in range(255): 
-      blynkObj.set_property(i, 'color', colours['OFFLINE'])
-   _log.info("Completed setFormOffline")
    
 try:
     
