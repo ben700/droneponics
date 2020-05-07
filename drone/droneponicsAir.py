5d2fc9b13516225681f7d4f4ef5693e59cf3d769
ben700 <ben700@hotmail.com>
2020-05-07 04:44:22 +0100

    Update droneponicsAir.py

--- drone/droneponicsAir.py
+++ drone/droneponicsAir.py
@@ -2,19 +2,47 @@ import logging
 import blynklib
 from drone.droneObj import colours
 
-def setFormOffline(blynkObj=None, Msg=None):
-   logging.debug("in setFormOffline")
-   if blynkObj is None:
-      blynkObj = blynklib.Blynk(parser.get('droneAir', 'BLYNK_AUTH'))
-   blynkObj.run()
-   logging.debug("Going to set Msg from setFormOffline")
-   if Msg is not None:
-       blynkObj.virtual_write(98, Msg + " " + '\n')
-       _log.info(Msg)
-   logging.debug("Going to set from colour Offline from setFormOffline")
+def setFormOnline(*args, **kwargs):
+   blynk = kwargs.get('blynkObj', None)
+   _log = kwargs.get('loggerObj', None)
+   msg =  kwargs.get('Msg', None)
+   if _log is None:
+      _log = logging.getLogger('BlynkLog')
+      logFormatter = logging.Formatter("%(asctime)s [%(levelname)s]  %(message)s")
+      consoleHandler = logging.StreamHandler()
+      consoleHandler.setFormatter(logFormatter)
+      _log.addHandler(consoleHandler)
+      _log.setLevel(logging.DEBUG)
+   _log.debug("in setFormOnline")
+   if blynk is None:
+      blynk = blynklib.Blynk(parser.get('droneAir', 'BLYNK_AUTH'))
+   blynk.run()
+   _log.debug("Going to set Msg from setFormOnline")
+   if msg is not None:
+       blynk.virtual_write(98, msg + " " + '\n')
+       _log.info(msg)
+   _log.debug("Going to set from colour Offline from setFormOnline")
    for i in range(255): 
-      blynkObj.set_property(i, 'color', colours['OFFLINE'])
-   logging.debug("Completed setFormOffline")
+      blynk.set_property(i, 'color', colours['ONLINE'])
+   _log.debug("Completed setFormOnline")
+   
+   
+def setFormOffline(*args, **kwargs):
+   blynk = kwargs.get('blynkObj', None)
+   _log = kwargs.get('loggerObj', None)
+   msg =  kwargs.get('Msg', None)
+   _log.debug("in setFormOffline")
+   if blynk is None:
+      blynk = blynklib.Blynk(parser.get('droneAir', 'BLYNK_AUTH'))
+   blynk.run()
+   _log.debug("Going to set Msg from setFormOffline")
+   if Msg is not None:
+       blynk.virtual_write(98, Msg + " " + '\n')
+       _log.info(Msg)
+   _log.debug("Going to set from colour Offline from setFormOffline")
+   for i in range(255): 
+      blynk.set_property(i, 'color', colours['OFFLINE'])
+   _log.debug("Completed setFormOffline")
 
 
 
