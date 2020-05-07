59f169057b2578a6ae83b1f75c64c8703f1c6e64
ben700 <ben700@hotmail.com>
2020-05-07 04:15:17 +0100

    Update droneponicsAir.py

--- drone/droneponicsAir.py
+++ drone/droneponicsAir.py
@@ -2,23 +2,16 @@ import logging
 from drone.droneObj import colours
 
 def setFormOffline(blynkObj=None, Msg=None):
-   
-   _log = logging.getLogger('BlynkLog')
-   logFormatter = logging.Formatter("%(asctime)s [%(levelname)s]  %(message)s")
-   consoleHandler = logging.StreamHandler()
-   consoleHandler.setFormatter(logFormatter)
-   _log.addHandler(consoleHandler)
-   _log.setLevel(logging.DEBUG)
-   
-   _log.debug("in setFormOffline")
+     
+   logging.debug("in setFormOffline")
    if blynkObj is None:
       blynkObj = blynklib.Blynk(parser.get('droneAir', 'BLYNK_AUTH'))
    blynkObj.run()
-   _log.debug("Going to set Msg from setFormOffline")
+   logging.debug("Going to set Msg from setFormOffline")
    if Msg is not None:
        blynkObj.virtual_write(98, Msg + " " + '\n')
        _log.info(Msg)
-   _log.debug("Going to set from colour Offline from setFormOffline")
+   logging.debug("Going to set from colour Offline from setFormOffline")
    for i in range(255): 
       blynkObj.set_property(i, 'color', colours['OFFLINE'])
-   _log.debug("Completed setFormOffline")
+   logging.debug("Completed setFormOffline")
