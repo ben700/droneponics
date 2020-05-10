fdec5d744c54159cb3fea7681f524da0d89358f1
ben700 <ben700@hotmail.com>
2020-05-10 23:12:12 +0100

    Update alarm.py

--- drone/alarm.py
+++ drone/alarm.py
@@ -1,25 +1,34 @@
+
+Alarm('temperature', "low", "low",15.0, Notify=False,  Message = 'Low TEMP!!!', Colour = '#c0392b')
+Alarm('temperature', "High", "high", 35.0, Notify=False, Message = 'High TEMP!!!', Colour = '#c0392b')
+Alarm('temperature', "low", "lowlow", 20.0, Notify=True,  Message = 'Low Low TEMP!!!', Colour = '#c0392b')
+Alarm('temperature', "High", "highhigh", 40.0,Notify=True, Message = 'High High TEMP!!!', Colour = '#c0392b')
+   
 class Alarm:
    
-   def __init__(self, Name, Type="Low", notify=False *args, **kwargs):
-      self.severity = kwargs.get('Severity', None)
-      self.name = Name==
+   def __init__(self, Metric, Type, Name, Value, notify=False *args, **kwargs):
+      self.metric = Metric
       self.type = Type
-      self.notify = notify
-      self.crititalValue = kwargs.get('VALUE', None)
-      self.crititalMessage = kwargs.get('MSG', None)
-      self.crititalColour = kwargs.get('COLOR', None)
+      self.name = Name
+      self.value = Value
+      self.notify = kwargs.get('Notify', None)
+      self.message = kwargs.get('Message', None)
+      self.colour = kwargs.get('Colour', None)
 
       
-    def addTest(temperature, state, "low", True, VALUE, 20.0, MSG = 'Low TEMP!!!', COLOR = '#c0392b')
+   
+   def test(blynk, VP, type, VALUE)   
        if (type == 'Low'):
-         if (VALUE > state):
+         if (VALUE > self.crititalValue):
             if(self.notify):
                blynk.notify(self.crititalMessage)
-               self.notify=Falue
-    
+               self.notify=False
+            if(self.crititalColour is not None):
+               blynk.set_property(VP, "color", self.crititalColour)
+               
        blynk.virtual_write(VP, self.temperature)
        blynk.set_property(VP, "label", self.name)
        blynk.set_property(VP, "color", self.colour)
 
          
-def testAlarm('temperature', VALUE);
+     def testAlarm('temperature', VALUE);
