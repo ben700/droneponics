1a0aa0082f45658c45a53759a9afecb410beea31
ben700 <ben700@hotmail.com>
2020-05-10 22:53:34 +0100

    Update alarm.py

--- drone/alarm.py
+++ drone/alarm.py
@@ -1,5 +1,25 @@
 class Alarm:
-   def __init__(self, name, notify=False *args, **kwargs):
-       self.severity = kwargs.get('Severity', None)
-       self.name = None
-       self.notify = notify
+   
+   def __init__(self, Name, Type="Low", notify=False *args, **kwargs):
+      self.severity = kwargs.get('Severity', None)
+      self.name = Name==
+      self.type = Type
+      self.notify = notify
+      self.crititalValue = kwargs.get('VALUE', None)
+      self.crititalMessage = kwargs.get('MSG', None)
+      self.crititalColour = kwargs.get('COLOR', None)
+
+      
+    def addTest(temperature, state, "low", True, VALUE, 20.0, MSG = 'Low TEMP!!!', COLOR = '#c0392b')
+       if (type == 'Low'):
+         if (VALUE > state):
+            if(self.notify):
+               blynk.notify(self.crititalMessage)
+               self.notify=Falue
+    
+       blynk.virtual_write(VP, self.temperature)
+       blynk.set_property(VP, "label", self.name)
+       blynk.set_property(VP, "color", self.colour)
+
+         
+def testAlarm('temperature', VALUE);
