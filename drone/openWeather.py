deddf068c0aab3b74db403df759c916989aea84e
ben700 <ben700@hotmail.com>
2020-05-11 01:37:00 +0100

    Update openWeather.py

--- drone/openWeather.py
+++ drone/openWeather.py
@@ -22,7 +22,10 @@ class OpenWeather:
       self.openWeatherData = requests.get("https://api.openweathermap.org/data/2.5/onecall?lat=53.801277&lon=-1.548567&exclude=hourly,daily&units=metric&appid=7ab0c16c9b00854f26df8a57435ad6ce")   
       print(self.openWeatherData)
       print("OpenWeather:-  going to json")
-      self.openWeather = openWeatherData.json()
+      print(self.openWeatherData.json())
+      
+      self.openWeather = self.openWeatherData.json()
+      
       print("OpenWeather:-  completed refresh")
       return
          
