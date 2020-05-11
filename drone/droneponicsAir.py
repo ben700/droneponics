598a1044f913a3696cd8fdc99d168cddd23bed58
ben700 <ben700@hotmail.com>
2020-05-11 03:45:25 +0100

    Update droneponicsAir.py

--- drone/droneponicsAir.py
+++ drone/droneponicsAir.py
@@ -1,6 +1,19 @@
 import logging
+import socket
 import blynklib
 from drone.droneObj import colours
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
 
 def setFormOnline(*args, **kwargs):
    blynk = kwargs.get('blynkObj', None)
