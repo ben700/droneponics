bebd730a593b980d6de2db3e0c54aefa375dd6d0
ben700 <ben700@hotmail.com>
2020-05-11 12:00:48 +0100

    Update nutrientSchedule.py

--- drone/nutrientSchedule.py
+++ drone/nutrientSchedule.py
@@ -4,21 +4,50 @@ def lightPin():
     return 26
             
             
-def buildNutrientMix(nutrientMix, _log):
+def buildNutrientMix(nutrientMix, _log, scheduleWeek='Grow'):
  #weekGrow
+ if (scheduleWeek=='Grow'):
     nutrientMix.append( Dose(119, 1.00, LED[0], "pH", VolumePin[0], 1000))
     nutrientMix.append( Dose(111, 6.00, LED[1], "Hydro Grow A", VolumePin[1], 3000))
     nutrientMix.append( Dose(114, 10.00, LED[3], "Root Stimulant", VolumePin[3], 3000))
     nutrientMix.append( Dose(115, 4.00, LED[4], "Enzyme", VolumePin[4], 3000))
     nutrientMix.append( Dose(116, 1.00, LED[5], "Hydro-Silicon", VolumePin[5], 500)) 
     nutrientMix.append( Dose(113, 6.00, LED[2], "Hydro Grow B", VolumePin[2], 3000)) 
- #week6-8
- #   nutrientMix.append( Dose(119, 1.00, LED[0], "pH", VolumePin[0], 1000))
- #   nutrientMix.append( Dose(111, 6.00, LED[1], "Hydro Bloom A", VolumePin[1], 5000))
- #   nutrientMix.append( Dose(114, 10.00, LED[3], "Ignition", VolumePin[3], 1000))
- #   nutrientMix.append( Dose(115, 4.00, LED[4], "Enzyme", VolumePin[4], 5000))
- #   nutrientMix.append( Dose(116, 1.00, LED[5], "Magne-Cal", VolumePin[5], 5000)) 
- #   nutrientMix.append( Dose(113, 6.00, LED[2], "Hydro Bloom B", VolumePin[2], 5000)) 
+ elif (scheduleWeek==1):
+    nutrientMix.append( Dose(119, 1.00, LED[0], "pH", VolumePin[0], 1000))
+    nutrientMix.append( Dose(111, 6.00, LED[1], "Hydro Grow A", VolumePin[1], 5000))
+    nutrientMix.append( Dose(114, 10.00, LED[3], "Ignition", VolumePin[3], 1000))
+    nutrientMix.append( Dose(115, 4.00, LED[4], "Enzyme", VolumePin[4], 5000))
+    nutrientMix.append( Dose(116, 1.00, LED[5], "Magne-Cal", VolumePin[5], 5000)) 
+    nutrientMix.append( Dose(113, 6.00, LED[2], "Hydro Grow B", VolumePin[2], 5000)) 
+ elif (scheduleWeek==2):
+    nutrientMix.append( Dose(119, 1.00, LED[0], "pH", VolumePin[0], 1000))
+    nutrientMix.append( Dose(111, 6.00, LED[1], "Hydro Bloom A", VolumePin[1], 5000))
+    nutrientMix.append( Dose(114, 10.00, LED[3], "Ignition", VolumePin[3], 1000))
+    nutrientMix.append( Dose(115, 4.00, LED[4], "Enzyme", VolumePin[4], 5000))
+    nutrientMix.append( Dose(116, 1.00, LED[5], "Magne-Cal", VolumePin[5], 5000)) 
+    nutrientMix.append( Dose(113, 6.00, LED[2], "Hydro Bloom B", VolumePin[2], 5000)) 
+ elif (scheduleWeek==3):
+    nutrientMix.append( Dose(119, 1.00, LED[0], "pH", VolumePin[0], 1000))
+    nutrientMix.append( Dose(111, 6.00, LED[1], "Hydro Bloom A", VolumePin[1], 5000))
+    nutrientMix.append( Dose(114, 10.00, LED[3], "Ignition", VolumePin[3], 1000))
+    nutrientMix.append( Dose(115, 4.00, LED[4], "Enzyme", VolumePin[4], 5000))
+    nutrientMix.append( Dose(116, 1.00, LED[5], "Magne-Cal", VolumePin[5], 5000)) 
+    nutrientMix.append( Dose(113, 6.00, LED[2], "Hydro Bloom B", VolumePin[2], 5000)) 
+ elif (scheduleWeek==8):
+    nutrientMix.append( Dose(119, 1.00, LED[0], "pH", VolumePin[0], 1000))
+    nutrientMix.append( Dose(111, 6.00, LED[1], "Hydro Bloom A", VolumePin[1], 5000))
+    nutrientMix.append( Dose(114, 10.00, LED[3], "Ignition", VolumePin[3], 1000))
+    nutrientMix.append( Dose(115, 4.00, LED[4], "Enzyme", VolumePin[4], 5000))
+    nutrientMix.append( Dose(116, 1.00, LED[5], "Magne-Cal", VolumePin[5], 5000)) 
+    nutrientMix.append( Dose(113, 6.00, LED[2], "Hydro Bloom B", VolumePin[2], 5000)) 
+ else (scheduleWeek==8):
+    nutrientMix.append( Dose(119, 1.00, LED[0], "pH", VolumePin[0], 1000))
+    nutrientMix.append( Dose(111, 6.00, LED[1], "Hydro Bloom A", VolumePin[1], 5000))
+    nutrientMix.append( Dose(114, 10.00, LED[3], "Ignition", VolumePin[3], 1000))
+    nutrientMix.append( Dose(115, 4.00, LED[4], "Enzyme", VolumePin[4], 5000))
+    nutrientMix.append( Dose(116, 1.00, LED[5], "Magne-Cal", VolumePin[5], 5000)) 
+    nutrientMix.append( Dose(113, 6.00, LED[2], "Hydro Bloom B", VolumePin[2], 5000)) 
     return nutrientMix
 
             
@@ -27,7 +56,7 @@ def buildOxyMix(nutrientMix, _log):
     return nutrientMix
 
    
-def buildSensors(sensors, _log):
+def buildSensors(sensors, _log, scheduleWeek='Grow'):
     sensors.append( Sensor(102, "Temprature", 30, Target=20, LowAlarm=10, HighAlarm=25))
     sensors.append( Sensor(100, "EC", 31 , Target=100, LowAlarm=500, HighAlarm=1500))
     sensors.append( Sensor(100, "EC", 31 , Target=1000, LowAlarm=500, HighAlarm=1500))
