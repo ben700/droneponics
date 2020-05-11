dc5d256474cb05d50ab224c9a8dc12cb4a8a5c63
ben700 <ben700@hotmail.com>
2020-05-11 01:26:43 +0100

    Update openWeather.py

--- drone/openWeather.py
+++ drone/openWeather.py
@@ -15,6 +15,7 @@ class OpenWeather:
       self.timestamp = datetime.now()
       print("in OpenWeather refresh got time")
       self.useByTime = datetime.now() +  datetime.timedelta(minutes=30)
+      print("in OpenWeather refresh got time delta")
       print(self.useByTime)
       print("OpenWeather:-  going to get data")
       self.openWeatherAPI = requests.get("https://api.openweathermap.org/data/2.5/onecall?lat=53.801277&lon=-1.548567&exclude=hourly,daily&units=metric&appid=7ab0c16c9b00854f26df8a57435ad6ce")   
