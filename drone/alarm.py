a6415bf26e010b00e6312f61b47a23586e1b6a90
ben700 <ben700@hotmail.com>
2020-05-11 17:45:21 +0100

    Update alarm.py

--- drone/alarm.py
+++ drone/alarm.py
@@ -1,21 +1,5 @@
  
-from colour import Color
 import blynklib
-
-def displaySensor(blynk, VP, VALUE, NAME , LOW, HIGH):
- print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
- red = Color("red")
- print("loaded clor class")
- colors = list(red.range_to(Color("green"),10))
- print("done colour list")
- print(colors)
- print("Going to update blynk")
- blynk.virtual_write(VP,VALUE)
- blynk.set_property(VP, "label", NAME)
- print("Going to update blynk colors")
- blynk.set_property(VP, "color", colors[round((HIGH-LOW)/10,0)])
- print("####################################################")
- return
 
   
 class Alarm:
