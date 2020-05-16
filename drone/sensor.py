ed97fa0f2fe369b294a107c3baf172825042fcde
ben700 <ben700@hotmail.com>
2020-05-16 05:28:57 +0100

    Update sensor.py

--- drone/sensor.py
+++ drone/sensor.py
@@ -21,29 +21,49 @@ class Sensor:
        self.sensor = AtlasI2C(SensorId)
        self.sensorId = SensorId
        self.name = Name
+       self.high =26
+       self.low =15
        self.displayPin = kwargs.get('DisplayPin', None)
        self.target = kwargs.get('Target', None)
        self.value = None
        self.lowAlarm = kwargs.get('LowAlarm', None)
        self.highAlarm = kwargs.get('HighAlarm', None)
-       
-      self.name = None
-      self.object = AtlasI2C(SensorId)
+
   def read():
   return self.sensor.query("R").split(":")[1].strip().rstrip('\x00')
+  def display(blynk, VALUE):
+    red = Color("red")
+    colors = list(red.range_to(Color("green"),10))
+    self.value =VALUE
+    blynk.set_property(displayPin, "label", self.name)
+    blynk.set_property(displayPin, 'color', colors[round((self.high-self.low)/10,0)])
+    return blynk.virtual_write(displayPin, self.value)
     
 class PH(Sensor):  
    def __init__(self, *args, **kwargs):
-    Sensor.__init__(self, 99, "pH", 32, Target=5.5, LowAlarm=5.3, HighAlarm=6.5, *args, **kwargs)
+    Sensor.__init__(self, 99, "pH", 32, *args, **kwargs)
+    self.high=6.3
+    self.low=5.4
+    self.target=5.5
  def read(cTemp):
   return self.sensor.query("RT,"+cTemp).split(":")[1].strip().rstrip('\x00')
  
 class EC(Sensor):
+   def __init__(self,  Target=600, *args, **kwargs):
+      Sensor.__init__(self, 100, "EC",  31 , Target=600,  *args, **kwargs) 
+      self.target=kwargs.get('Target')
+      self.high=self.target+200
+      self.low=self.target-200  
+   def read(cTemp):
+      return self.sensor.query("RT,"+cTemp).split(":")[1].strip().rstrip('\x00')
+  
+class DO(Sensor):
    def __init__(self, *args, **kwargs):
-      Sensor.__init__(self, 100, "EC",  31 , Target=600, LowAlarm=500, HighAlarm=1500, *args, **kwargs) 
+      Sensor.__init__(self, 97, "Dissolved Oxygen", 30, Target=10, *args, **kwargs) 
    def read(cTemp):
       return self.sensor.query("RT,"+cTemp).split(":")[1].strip().rstrip('\x00')
   
 class TEMP(Sensor):  
    def __init__(self, *args, **kwargs):
       Sensor.__init__(self, 102, "Temprature", 30, Target=20, LowAlarm=10, HighAlarm=25, *args, **kwargs) 
+    
