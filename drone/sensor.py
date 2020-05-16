16177994bd86bb77f367d13629981d18b26f2cb2
ben700 <ben700@hotmail.com>
2020-05-16 04:39:58 +0100

    Update sensor.py

--- drone/sensor.py
+++ drone/sensor.py
@@ -4,7 +4,7 @@ import blynklib
 def displaySensor(blynk, VP, VALUE, NAME , LOW, HIGH):
  print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
  red = Color("red")
- print("loaded clor class")
+ print("loaded color class")
  colors = list(red.range_to(Color("green"),10))
  print("done colour list")
  print(colors)
@@ -15,3 +15,4 @@ def displaySensor(blynk, VP, VALUE, NAME , LOW, HIGH):
  blynk.set_property(VP, "color", colors[round((HIGH-LOW)/10,0)])
  print("####################################################")
  return
+
