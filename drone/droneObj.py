ccb75c41e61150a0e203d1e10b05e8a19de09a5f
ben700 <ben700@hotmail.com>
2020-05-19 07:42:49 +0100

    Update droneObj.py

--- drone/droneObj.py
+++ drone/droneObj.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 LED = [10,11,12,13,14,15]
 VolumePin = [20,21,22,23,24,25] 
-colours = {1: '#FF0000', 0: '#00FF00', '0': '#00FF00', '1': '#FF0000', 2: '#00FF00', 3: '#80FF00',4: '#00FF80', 5: '#80FF80','OFFLINE': '#0000FF', 'ONLINE': '#00FF00', 'UNAVILABLE': '#002700'}
+colours = {1: '#FF0000', 0: '#00FF00', '0': '#00FF00', '1': '#FF0000', 2: '#00FF00', '3': '#80FF00',4: '#00FF80', 5: '#80FF80','OFFLINE': '#0000FF', 'ONLINE': '#00FF00', 'UNAVILABLE': '#002700'}
 systemLED=101
 
 class Relays:
