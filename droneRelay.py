f27d247a659ff512dbaad097284b1586be174cfc
ben700 <ben700@hotmail.com>
2020-05-19 14:20:39 +0100

    Update droneRelay.py

--- droneRelay.py
+++ droneRelay.py
@@ -74,9 +74,9 @@ try:
     
     
     # Initialize Blynk
-    #blynk = blynklib.Blynk(parser.get('blynk', 'BLYNK_AUTH'))
+    blynk = blynklib.Blynk(parser.get('blynk', 'BLYNK_AUTH'))
     _log.info("BLYNK_AUTH = " + parser.get('blynk', 'BLYNK_AUTH'))
-    blynk = blynklib.Blynk(sovoOQHytL_YkDc2WKMFCwzG6cOgQ_2H)
+    #blynk = blynklib.Blynk(sovoOQHytL_YkDc2WKMFCwzG6cOgQ_2H)
     
     timer = blynktimer.Timer()
         
