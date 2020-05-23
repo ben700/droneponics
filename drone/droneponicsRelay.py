a615444857414fd9a733893952b087d91a122a45
ben700 <ben700@hotmail.com>
2020-05-23 12:38:45 +0100

    Update droneponicsRelay.py

--- drone/droneponicsRelay.py
+++ drone/droneponicsRelay.py
@@ -19,13 +19,11 @@ def droneRelayWriteHandler(pin, button_state, blynk, relays):
         
         if (button_state == 1):
                 print("button_state = 1")
-        if (button_state == '1'):
-                print("button_state = !1")
         if (button_state == "1"):
                 print("button_state = str 1")
         
         
-        if (button_state in (1,"1",'1')):
+        if (button_state in (1,"1")):
            blynk.virtual_write(98, "Change state of button "+ str(pin) + " now on" + '\n')
            blynk.set_property(10+pin, 'color', colours[1])
            GPIO.output(relays[pin],1)
