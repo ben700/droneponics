8d675218e8250a1553efc74235232342fa7acdcb
ben700 <ben700@hotmail.com>
2020-05-23 12:03:48 +0100

    Update droneponicsRelay.py

--- drone/droneponicsRelay.py
+++ drone/droneponicsRelay.py
@@ -15,10 +15,12 @@ def droneRelayWriteHandler(pin, button_state, blynk, relays):
         blynk.set_property(systemLED, 'color', colours[1])
         blynk.virtual_write(250, "Updating")
         if (button_state == '1'):
+           print("set button on")     
            blynk.set_property(10+pin, 'color', colours[1])
            blynk.set_property(pin, 'onBackColor', colours[1])
            GPIO.output(relays[pin],1)
         else:
+           print("set button on")     
            blynk.set_property(10+pin, 'color', colours[0])
            blynk.set_property(pin, 'onBackColor', colours[0])
            GPIO.output(relays[pin],0)
