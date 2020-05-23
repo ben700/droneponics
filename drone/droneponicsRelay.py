43cfe5b3dbffe1040d00fe82e80b1986eceb23cf
ben700 <ben700@hotmail.com>
2020-05-23 11:42:56 +0100

    Update droneponicsRelay.py

--- drone/droneponicsRelay.py
+++ drone/droneponicsRelay.py
@@ -9,7 +9,7 @@ import time
 
         
 def droneRelayWriteHandler(pin, button_state, blynk, relays):
-        if button_state not in (0,1,"0","1"):
+        if button_state not in ('0','1'):
             print ("button_state was " + str(button_state) + " so updated to 0")
             button_state = 0
         now = datetime.now()
