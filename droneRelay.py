e7d597afdc0959f1d41d5238404cc9ea71ed50a3
ben700 <ben700@hotmail.com>
2020-05-24 05:02:14 +0100

    Update droneRelay.py

--- droneRelay.py
+++ droneRelay.py
@@ -198,15 +198,15 @@ try:
         
         if( startTime < seconds_since_midnight and stopTime > seconds_since_midnight):
             if all([CO2, CO2Target]):
-                blynk.virtual_write(98,"CO2Target : "+ str(CO2Target) + " and co2 level : " +str(CO2))    
+                blynk.virtual_write(98,"CO2Target : "+ str(CO2Target) + " and co2 level : " +str(CO2)+ '\n')    
                 if (CO2 <CO2Target):
                     _log.info("CO2 is less than target")
-                    blynk.virtual_write(98,"CO2 Relay is on")    
+                    blynk.virtual_write(98,"CO2 Relay is on"+ '\n')    
                     iValue = "1"
                 else:
                     _log.info("CO2 is above target")
                     iValue = "0"
-                    blynk.virtual_write(98,"Co2 Relay is off due to level")
+                    blynk.virtual_write(98,"Co2 Relay is off due to level"+ '\n')
             else:
                 _log.info("Co2 Relay is on due to time and not being overwritten by current co2 reading " +str(CO2) + " with target " + str(CO2Target))
                 iValue = "1"
@@ -214,7 +214,7 @@ try:
                     
         else:
             iValue = "0"
-            blynk.virtual_write(98,"Co2 Relay is off due to time")
+            blynk.virtual_write(98,"Co2 Relay is off due to time"+ '\n')
         now = datetime.now()
         blynk.virtual_write(99, now.strftime("%d/%m/%Y %H:%M:%S"))
         drone.droneRelayWriteHandler(pin, iValue, blynk, relays)
