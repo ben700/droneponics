24e882045ab1d5bb877fbf0770328115e0617d06
ben700 <ben700@hotmail.com>
2020-05-24 06:51:25 +0100

    Update droneObj.py

--- drone/droneObj.py
+++ drone/droneObj.py
@@ -5,6 +5,8 @@ LED = [10,11,12,13,14,15]
 VolumePin = [20,21,22,23,24,25] 
 colours = {1: '#FF0000', 0: '#00FF00', '0': '#00FF00', '1': '#FF0000', '2': '#00FF00', '3': '#80FF00','4': '#00FF80', '5': '#80FF80','OFFLINE': '#0000FF', 'ONLINE': '#00FF00', 'UNAVILABLE': '#002700'}
 systemLED=101
+TARGET_EMAIL = 'benslittlebitsandbobs@gmail.com'
+
 
 class Relays:
    def __init__(self):
