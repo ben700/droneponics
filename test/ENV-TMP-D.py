4c80b3ffd106539d77d80d747684f7af8ab58395
ben700 <ben700@hotmail.com>
2020-05-26 16:40:13 +0100

    Update ENV-TMP-D.py

--- test/ENV-TMP-D.py
+++ test/ENV-TMP-D.py
@@ -1,8 +1,31 @@
 import time
 import serial
+import subprocess
+import traceback
+import getrpimodel
+import struct
+import platform
+import argparse
+import sys
+import json
+import os.path
+
+# setting
+pimodel        = getrpimodel.model
+pimodel_strict = getrpimodel.model_strict()
+
+if os.path.exists('/dev/serial0'):
+  partial_serial_dev = 'serial0'
+elif pimodel == "3 Model B" or pimodel_strict == "Zero W":
+  partial_serial_dev = 'ttyS0'
+else:
+  partial_serial_dev = 'ttyAMA0'
+
+serial_dev = '/dev/%s' % partial_serial_dev
+
 
 ser = serial.Serial(
-        port='/dev/ttyS0', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
+        port=serial_dev, #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
         baudrate = 38400,
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE,
