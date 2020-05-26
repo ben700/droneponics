f799ad3d57a898135a9cfcedf74ff5a49eb541ba
ben700 <ben700@hotmail.com>
2020-05-26 17:43:26 +0100

    Update ENV-TMP-D.py

--- test/ENV-TMP-D.py
+++ test/ENV-TMP-D.py
@@ -23,12 +23,13 @@ else:
 
 serial_dev = '/dev/%s' % partial_serial_dev
 
+print("serial_dev = " + serial_dev)
 
 ser = serial.Serial(
         port=serial_dev, #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
         baudrate = 38400,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
+#        parity=serial.PARITY_NONE,
+#        stopbits=serial.STOPBITS_ONE,
         bytesize=serial.EIGHTBITS,
         timeout=1
 )
