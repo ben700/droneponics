ba8a611d3b24788d1787a4efd842c893baad4b98
ben700 <ben700@hotmail.com>
2020-05-28 02:33:40 +0100

    Update atlasFlowUART.py

--- test/atlasFlowUART.py
+++ test/atlasFlowUART.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python
 import serial
-self.ser = serial.Serial(
+ser = serial.Serial(
            port='/dev/ttyAMA0', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
            baudrate = 38400
       )
 #set the default units on probe 
-self.ser.write(str.encode("I2C,104"+'\r'))
-      
+ser.write(str.encode("I2C,104"+'\r'))
+
+ser.write(str.encode("R"+'\r'))
+rawOutput = self.ser.read(5)
+o = rawOutput.decode()
+print(o)        
