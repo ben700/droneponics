64ed0e2f7acb9b5a76c2a490b0df9c2faf37dee0
ben700 <ben700@hotmail.com>
2020-06-18 00:48:00 +0100

    Update droneRelayV2.py

--- droneRelayV2.py
+++ droneRelayV2.py
@@ -48,8 +48,6 @@ _log.setLevel(parser.get('logging', 'logLevel', fallback=logging.DEBUG))
 
 _log.info("/home/pi/droneponics/config/configRelay_"+drone.gethostname()+".ini")
 droneCounter = drone.DroneCounter()
-print(droneCounter.isAutomatic(_log))
-
 
 try:
     GPIO.setmode(GPIO.BCM)
