093cc1bca1119e10ce01c9fb774d5f9b26acae7d
ben700 <ben700@hotmail.com>
2020-06-17 06:54:40 +0100

    Update DroneCounter.py

--- drone/DroneCounter.py
+++ drone/DroneCounter.py
@@ -73,10 +73,10 @@ class DroneCounter:
          elif(self.overwrite is  "Off"):
              return "In minute " + str(self.cycle) + "  pump is set manually OFF"
          else:   
-              if(self.cycle < self.onCycle)
-                   return "In minute " + str(self.cycle) + " pump is on till minute " + str(self.onCycle)
-              else:
-                   return "In minute " + str(self.cycle) + " pump is on till minute " + str(self.onCycle+self.offCycle)
+             if(self.cycle < self.onCycle)
+                  return "In minute " + str(self.cycle) + " pump is on till minute " + str(self.onCycle)
+             else:
+                  return "In minute " + str(self.cycle) + " pump is on till minute " + str(self.onCycle+self.offCycle)
                
         
    def  incCycle(self, _log):
