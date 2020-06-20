aacff15ec06ac70e7dccb81281b051a6067c616b
ben700 <ben700@hotmail.com>
2020-06-20 16:56:39 +0100

    Update relay.py

--- drone/relay.py
+++ drone/relay.py
@@ -1,16 +1,16 @@
 from drone import * 
 class Relay:
-   def __init__(self, PinId, Led, Name, Button, *args, **kwargs):
+   GPIO.setmode(GPIO.BCM)
+   GPIO.setwarnings(False)
+
+   def __init__(self, gpioPin, Name, *args, **kwargs):
        print("Building object")
-       self.pinId = PinId
-       self.LED = Led
+       self.gpioPin = gpioPin
        self.name = Name
-       self.button = Button
-       self.noisy = kwargs.get('Noisy', False)
+       self.gpio = GPIO.setup(gpioPin)
        
-def buildRelay(relays, _log): 
-    relays.append( Relay(26, LED[1], "Air", 1, Noisy=True))
-    relays.append( Relay(13, LED[2], "Mixer", 2, Noisy=True))
-    relays.append( Relay(21, LED[3], "Heater", 3, Noisy=True))
-    relays.append( Relay(27, LED[4], "Other", 4))
-    return relays
+def turnOn(relays, _log): 
+    self.gpio.output(self.gpioPin,GPIO.HIGH) 
+      
+def turnOff(relays, _log): 
+    self.gpio.output(self.gpioPin,GPIO.LOW) 
