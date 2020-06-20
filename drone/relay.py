4e935414009f1820d5e8f0555870aa5525f70033
ben700 <ben700@hotmail.com>
2020-06-20 21:18:34 +0100

    Update relay.py

--- drone/relay.py
+++ drone/relay.py
@@ -8,6 +8,9 @@ class Relay:
        self.gpioPin = gpioPin
        self.name = Name
        self.gpio = GPIO.setup(gpioPin, GPIO.OUT)
+       self.automatic = False
+       self.cycle=0
+       self.cycleReset=0
        
 def turnOn(relays, _log): 
     try:
@@ -23,4 +26,22 @@ def turnOff(relays, _log):
          self.gpio.output(self.gpioPin,GPIO.LOW) 
     except:
          _log.error("Except: Turning off relay " + self.name)
+     
+   def setManual(self):
+         self.automatic = False
+   
+   def setAutomatic(self):
+         self.automatic = True
+     
+   def isAutomatic(self, _log):
+        return self.automatic
       
+   def cycleResetSet(self, _log, cycleReset):
+      self.cycleReset = int(cycleReset) 
+   
+   def cycleReset(self, _log):
+      self.cycle = 0 
+      
+   def incCycle(self):
+      self.cycle = self.cycle + 1
+      return self.cycle
