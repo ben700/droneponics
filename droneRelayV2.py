5d7e0a3a2ad6aab469476d895c37995e1bd06b7d
ben700 <ben700@hotmail.com>
2020-06-20 16:45:46 +0100

    Update droneRelayV2.py

--- droneRelayV2.py
+++ droneRelayV2.py
@@ -115,7 +115,7 @@ if (True):
         _log.debug("Waste relay is "+ str(relays[7]))
         if (staus is "1" ):
             try:
-                 GPIO.output(relays[7],GPIO.HIGH)
+               #  GPIO.output(relays[7],GPIO.HIGH)
                  droneCounter.wasteAutomatic = False
                  droneCounter.wasteCycleState = "Off"
                  blynk.virtual_write(28, "Waste is Off")
@@ -124,7 +124,7 @@ if (True):
                  blynk.virtual_write(28, "Except handle_event V7 Turning Off waste")           
         elif (staus is "2" ):
             try:
-                 GPIO.output(relays[7],GPIO.LOW)
+                # GPIO.output(relays[7],GPIO.LOW)
                  blynk.virtual_write(28, "Waste is On")
                  droneCounter.wasteAutomatic = False
                  droneCounter.wasteCycleState = "On"
