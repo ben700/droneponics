1aef574c5bdc9e39cbbebcdbe99beec0b76c4dd2
ben700 <ben700@hotmail.com>
2020-06-20 12:29:27 +0100

    Update counterClassTest.py

--- test/counterClassTest.py
+++ test/counterClassTest.py
@@ -4,8 +4,8 @@ import sys
 import os
 sys.path.append('/home/pi/droneponics')
 import drone
-
-
+import RPi.GPIO as GPIO  
+   
 
 _log = logging.getLogger('BlynkLog')
 logFormatter = logging.Formatter("%(asctime)s [%(levelname)s]  %(message)s")
@@ -14,6 +14,31 @@ consoleHandler.setFormatter(logFormatter)
 _log.addHandler(consoleHandler)
 _log.setLevel(logging.DEBUG)
 
+
+try:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+
+    relays=[0,18,23,24,25,12,16,20,21]
+    Relay1 = relays[1] #feed
+    Relay2 = relays[2] #fan
+    Relay3 = relays[3] #Air
+    Relay4 = relays[4] #heater
+    Relay5 = relays[5] #Feed
+    Relay6 = relays[6] #Air
+    Relay7 = relays[7] #Mixer - turned off with low water 
+    Relay8 = relays[8]  #Mixer - turned off with low water 
+
+    GPIO.setup(Relay1,GPIO.OUT, initial=1)
+    GPIO.setup(Relay2,GPIO.OUT, initial=1)
+    GPIO.setup(Relay3,GPIO.OUT, initial=1)
+    GPIO.setup(Relay4,GPIO.OUT, initial=1)
+    GPIO.setup(Relay5,GPIO.OUT, initial=1)
+    GPIO.setup(Relay6,GPIO.OUT, initial=1)
+    GPIO.setup(Relay7,GPIO.OUT, initial=1)
+    GPIO.setup(Relay8,GPIO.OUT, initial=1)
+except:
+   _log.info("error creating GPIO")
 
 droneCounter = drone.DroneCounter()
 droneCounter.setAutomatic()
