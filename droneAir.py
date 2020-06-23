d3029093ef64a4a11ecc2741d695c82ce037e1ad
ben700 <ben700@hotmail.com>
2020-06-23 06:38:01 +0100

    Update droneAir.py

--- droneAir.py
+++ droneAir.py
@@ -148,10 +148,10 @@ except:
     bme280 = None
 
 @blynk.handle_event('write V29')
-    def v29write_handler(pin, value):
-        _log.debug("v29write_handler rowIndex =" + str(value[0]))
-        global rowIndex
-        rowIndex = int(value[0])
+def v29write_handler(pin, value):
+    _log.debug("v29write_handler rowIndex =" + str(value[0]))
+    global rowIndex
+    rowIndex = int(value[0])
                  
     
 @blynk.handle_event('write V255')
