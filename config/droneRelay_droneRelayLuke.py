293860c5f4a80dd214ea25e7a63be22648b4f95e
ben700 <ben700@hotmail.com>
2020-07-02 05:10:59 +0100

    Create droneRelay_droneRelayLuke.py

---
+++ config/droneRelay_droneRelayLuke.py
@@ -0,0 +1,17 @@
+[droneRelay]
+Display=False
+RelaySize=10
+Relay1=Feed
+Relay2=Fan
+Relay3=Mixer
+Relay4=UV
+Relay5=Aerator
+Relay6=Chiller
+Relay7=Waste
+Relay8=CO2
+Relay9=Light 1
+Relay10=Light 2
+[blynk]
+BLYNK_AUTH=edRNg-iDx9FvTnXhmp1alXBb_X-Y4Hd8
+[logging]
+logLevel=DEBUG
