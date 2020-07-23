2648806eba97ad65771dc8a12e8cb9e2fa10be27
ben700 <ben700@hotmail.com>
2020-07-23 01:30:49 +0100

    Create changePumpIdOldDoser.py

---
+++ atlas/changePumpIdOldDoser.py
@@ -0,0 +1,55 @@
+##!/usr/bin/env python3 
+
+if True:
+    import datetime
+    import time
+    import shlex, requests
+    import logging
+    from datetime import datetime
+    import sys
+    import os
+    import RPi.GPIO as GPIO
+    sys.path.append('/home/pi/droneponics')
+    from AtlasI2C import (
+	    AtlasI2C
+    )
+    import blynklib
+    import blynktimer
+    
+    import subprocess
+    import re
+    import drone
+
+
+
+    # tune console logging
+    _log = logging.getLogger('BlynkLog')
+    logFormatter = logging.Formatter("%(asctime)s [%(levelname)s]  %(message)s")
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(logFormatter)
+    _log.addHandler(consoleHandler)
+    _log.setLevel(logging.DEBUG)
+    
+    # Initialize the sensor.
+    try:
+       # Create the I2C bus
+       pump = AtlasI2C(111)
+       _log.info(pump.query("I2C,11"))
+       
+       pump = AtlasI2C(112)
+       _log.info(pump.query("I2C,12"))
+       
+        pump = AtlasI2C(114)
+       _log.info(pump.query("I2C,13"))
+       
+       pump = AtlasI2C(115)
+       _log.info(pump.query("I2C,14"))
+       
+       pump = AtlasI2C(116)
+       _log.info(pump.query("I2C,15"))
+       
+       pump = AtlasI2C(119)
+       _log.info(pump.query("I2C,16"))
+    except:
+        _log.info("Unexpected error: Atlas")
+    
