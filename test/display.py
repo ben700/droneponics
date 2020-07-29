0d66721c21b97edccd87ee9f3cd8695c8d0d9d39
ben700 <ben700@hotmail.com>
2020-07-29 06:02:44 +0100

    Update display.py

--- test/display.py
+++ test/display.py
@@ -38,7 +38,8 @@ try:
 
     
     print ("***draw line")
-    draw.line([(20,20),(300,20)], fill = "BLUE",width = 15)
+    draw.line([(20,20),(300,20)], fill = "BLUE",width = 5)
+    draw.line([(20,40),(300,40)], fill = "BLUE",width = 5)
 
     
     print ("***draw text")
