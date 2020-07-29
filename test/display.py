8c811674a0e1bf989e99d294472dad15ad20bb0c
ben700 <ben700@hotmail.com>
2020-07-29 05:43:47 +0100

    Update display.py

--- test/display.py
+++ test/display.py
@@ -33,12 +33,13 @@ try:
     font30 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 30)
     font15 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 15)
     print ("***draw line")
-    draw.line([(40,20),(200,20)], fill = "BLUE",width = 25)
-    draw.line([(40,20),(40,200)], fill = "BLUE",width = 25)
-    draw.line([(40,200),(200,200)], fill = "BLUE",width = 25)
-    draw.line([(200,20),(200,200)], fill = "BLUE",width = 25)
+    draw.line([(40,20),(200,20)], fill = "BLUE",width = 5)
+    draw.line([(40,20),(40,200)], fill = "BLUE",width = 5)
+    draw.line([(40,200),(200,200)], fill = "BLUE",width = 5)
+    draw.line([(200,20),(200,200)], fill = "BLUE",width = 5)
+    
     print ("***draw rectangle")
-    draw.rectangle([(50,30),(190,70)],fill = "BLUE")
+    draw.rectangle([(0,0),(10,10)],fill = "RED")
     
     print ("***draw text")
     draw.text((60,30), 'annabella', font = font30, fill = "WHITE")
