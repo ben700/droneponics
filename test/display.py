41b0f4f49c7fcdef978c443ee8c0ebde2a0e8f7a
ben700 <ben700@hotmail.com>
2020-08-01 13:11:30 +0100

    Update display.py

--- test/display.py
+++ test/display.py
@@ -16,7 +16,7 @@ from PIL import Image,ImageDraw,ImageFont
 logging.basicConfig(level=logging.INFO)
 print(picdir)
 
-try:
+def updateLCD (bR1, bR2, bR3, bR4, iPH, iEC, iTemp):
     print ("2inch LCD Module")
     disp = ST7789.ST7789()
     # Initialize library.
@@ -36,10 +36,22 @@ try:
     
     draw.rectangle([(0,0),(320,240)],fill = "WHITE")
     
-    draw.rectangle([(20,10),(90,70)], fill = "GREEN", outline="BLACK")
-    draw.rectangle([(90,10),(160,70)], fill = "RED", outline="BLACK")
-    draw.rectangle([(160,10),(230,70)], fill = "GREEN", outline="BLACK")
-    draw.rectangle([(230,10),(300,70)], fill = "RED", outline="BLACK")
+    if(bR1):
+        draw.rectangle([(20,10),(90,70)], fill = "GREEN", outline="BLACK")
+    else:
+        draw.rectangle([(20,10),(90,70)], fill = "RED", outline="BLACK")
+    if(bR2):
+        draw.rectangle([(90,10),(160,70)], fill = "GREEN", outline="BLACK")
+    else:
+        draw.rectangle([(90,10),(160,70)], fill = "RED", outline="BLACK")
+    if(bR3):
+        draw.rectangle([(160,10),(230,70)], fill = "GREEN", outline="BLACK")
+    else:
+        draw.rectangle([(160,10),(230,70)], fill = "RED", outline="BLACK")
+    if(bR4):
+        draw.rectangle([(230,10),(300,70)], fill = "GREEN", outline="BLACK")        
+    else:
+        draw.rectangle([(230,10),(300,70)], fill = "RED", outline="BLACK")
 
     draw.rectangle([(20,70),(300,130)], fill = "WHITE", outline="BLACK")
     draw.rectangle([(20,130),(300,190)], fill = "WHITE", outline="BLACK")
@@ -59,9 +71,9 @@ try:
     draw.text((190, 40), 'R3', font = font15, fill = "BLACK")
     draw.text((260, 40), 'R4', font = font15, fill = "BLACK")
 
-    draw.text((50, 90), 'ph', font = font30, fill = "BLACK")
-    draw.text((50, 150), 'EC', font = font30, fill = "BLACK")
-    draw.text((50, 190), 'Temp', font = font30, fill = "BLACK")
+    draw.text((50, 90), 'pH = ' + str(iPH), font = font30, fill = "BLACK")
+    draw.text((50, 150), 'EC = ' + str(iEC), font = font30, fill = "BLACK")
+    draw.text((50, 190), 'Temp = ' + str(iTemp), font = font30, fill = "BLACK")
 
     image=image.rotate(180) 
     disp.ShowImage(image)
@@ -73,7 +85,6 @@ try:
     image=image.rotate(180)
     disp.ShowImage(image)
 
-except KeyboardInterrupt:    
-    logging.info("ctrl + c:")
-    exit()
+try:
+    updateLCD (True, True, False, False, 7.0, 400, 25):
 
