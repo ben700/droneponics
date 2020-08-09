c2b151ce3b2c7c1eb092c7e192cfba25b71ba875
ben700 <ben700@hotmail.com>
2020-08-09 14:37:31 +0100

    Update droneDoser.py

--- droneDoser.py
+++ droneDoser.py
@@ -670,7 +670,8 @@ try:
               _log.critical("pHCal  = ")
               _log.critical(pHCal)
 	
-              pHCal = " " + pHCal + " "	    
+              pHCal = " " + pHCal + " "	
+ 
               print("[" + pHCal + "]")
               print("[" + len(pHCal) + "]")
               print("[" + pHCal[0] + "]")
