ebc1ea149c4f68509d1cea2a51b57b5651be532f
ben700 <ben700@hotmail.com>
2020-09-06 10:30:46 +0100

    Update gpioPump.py

--- test/gpioPump.py
+++ test/gpioPump.py
@@ -1,13 +1,14 @@
 import RPi.GPIO as GPIO            # import RPi.GPIO module  
 from time import sleep             # lets us have a delay  
 GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD  
-GPIO.setup(4, GPIO.OUT)           # set GPIO24 as an output   
+GPIO.setwarnings(False)
+GPIO.setup(17, GPIO.OUT)           # set GPIO24 as an output   
   
 try:  
-    while True:  
-        GPIO.output(4, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
+    for x in range(10):  
+        GPIO.output(17, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
         sleep(0.5)                 # wait half a second  
-        GPIO.output(4, 0)         # set GPIO24 to 0/GPIO.LOW/False  
+        GPIO.output(17, 0)         # set GPIO24 to 0/GPIO.LOW/False  
         sleep(0.5)                 # wait half a second  
   
 except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt  
