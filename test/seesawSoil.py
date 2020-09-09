0abcc2c8aa2029d19f0350a3da4e6ef9645573cf
ben700 <ben700@hotmail.com>
2020-09-09 15:02:35 +0100

    Update seesawSoil.py

--- test/seesawSoil.py
+++ test/seesawSoil.py
@@ -8,7 +8,7 @@ from adafruit_seesaw.seesaw import Seesaw
 #i2c_bus = busio.I2C(SCL, SDA)
 i2c_bus = busio.I2C(board.D1, board.D0) 
 
-ss = Seesaw(i2c_bus, addr=0x39)
+ss = Seesaw(i2c_bus, addr=0x37)
 
 while True:
     # read moisture level through capacitive touch pad
