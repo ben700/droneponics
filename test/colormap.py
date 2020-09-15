6c6900eda39b4a35a9edd0a3c7674ac8cf07ed82
ben700 <ben700@hotmail.com>
2020-09-15 19:28:57 +0100

    Update colormap.py

--- test/colormap.py
+++ test/colormap.py
@@ -50,6 +50,7 @@ step_size = 1
 for value in np.arange(vmin, vmax+step_size, step_size):
     count += 1
     print("%d/%d %f%%" % (count, vmax*(1./step_size), 100.*count/(vmax*(1./step_size))))
+    print("value = " + str(value))
     rgba = mycmap(norm(value), bytes=True)
     color = (rgba[0], rgba[1], rgba[2])
     if color not in colors.values():
@@ -58,6 +59,6 @@ for value in np.arange(vmin, vmax+step_size, step_size):
 print ("value, red, green, blue")
 for value in range(1, 100):
     print("value = " + str(value))
-    rgb = colors[value/1000]
+    rgb = colors[value]
     print("%s, %s, %s, %s" % (round(value,1), rgb[0], rgb[1], rgb[2]))
-    value = value +10
+  
