3951ece4ca44c7b9a43dc8fc93e3f82fa3cf5ac7
ben700 <ben700@hotmail.com>
2020-09-15 19:42:41 +0100

    Update colormap.py

--- test/colormap.py
+++ test/colormap.py
@@ -2,8 +2,8 @@
 import matplotlib
 import numpy as np 
 
-vmin = 0
-vmax = 1000
+vmin = 1
+vmax = 1001
 
 norm = matplotlib.colors.Normalize(np.log10(vmin), np.log10(vmax))
 lognum = norm(np.log10([.5, 2., 10, 40, 150,1000]))
