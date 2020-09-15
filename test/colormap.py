48f2f75131d6016116c57398a4cd80018ecbdcf1
ben700 <ben700@hotmail.com>
2020-09-15 20:08:48 +0100

    Create colormap.py

---
+++ test/colormap.py
@@ -0,0 +1,15 @@
+#import colormap
+from colour import Color
+from matplotlib import cm
+
+#normalize item number values to colormap
+norm = matplotlib.colors.Normalize(vmin=0, vmax=400)
+
+#colormap possible values = viridis, jet, spectral
+rgba_color = cm.coolwarm(norm(100),bytes=True) 
+print("rgba_color =" + str(rgba_color))
+c = Colour(rgba_color[0],rgba_color[1],rgba_color[2])
+print("color =" + str(c))
+
+
+#400 is one of value between 0 and 1000
