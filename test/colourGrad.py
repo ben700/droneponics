9cce1052d07b12ea8bb6319d4430c4c79a287566
ben700 <ben700@hotmail.com>
2020-09-15 18:58:18 +0100

    Create colourGrad.py

---
+++ test/colourGrad.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import math
+# A map of rgb points in your distribution
+# [distance, (r, g, b)]
+# distance is percentage from left edge
+heatmap = [
+    [0.0, (0, 0, 0)],
+    [0.20, (0, 0, .5)],
+    [0.40, (0, .5, 0)],
+    [0.60, (.5, 0, 0)],
+    [0.80, (.75, .75, 0)],
+    [0.90, (1.0, .75, 0)],
+    [1.00, (1.0, 1.0, 1.0)],
+]
+
+def gaussian(x, a, b, c, d=0):
+    return a * math.exp(-(x - b)**2 / (2 * c**2)) + d
+
+def pixel(x, width=100, map=[], spread=1):
+    width = float(width)
+    r = sum([gaussian(x, p[1][0], p[0] * width, width/(spread*len(map))) for p in map])
+    g = sum([gaussian(x, p[1][1], p[0] * width, width/(spread*len(map))) for p in map])
+    b = sum([gaussian(x, p[1][2], p[0] * width, width/(spread*len(map))) for p in map])
+    return min(1.0, r), min(1.0, g), min(1.0, b)
+
+x = 10
+r, g, b = pixel(x, width=100, map=heatmap)
+
+print("r = " + str(r))
+print("g = " + str(g))
+print("b = " + str(b))
