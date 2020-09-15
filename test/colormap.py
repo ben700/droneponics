4b65e7909ecd9a8d050b2b66f13e6e4989e91051
ben700 <ben700@hotmail.com>
2020-09-15 20:17:07 +0100

    Update colormap.py

--- test/colormap.py
+++ test/colormap.py
@@ -9,6 +9,8 @@ norm = matplotlib.colors.Normalize(vmin=0, vmax=400)
 #colormap possible values = viridis, jet, spectral
 rgba_color = cm.coolwarm(norm(100),bytes=True) 
 print("rgba_color =" + str(rgba_color))
+rgba_color = rgba_color(0.5)
+print("rgba_color =" + str(rgba_color))
 c = Color(rgba_color[0],rgba_color[1],rgba_color[2])
 print("color =" + str(c))
 
