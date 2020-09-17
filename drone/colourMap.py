75e761acaf3f87cba27a76888d78d02b30def468
ben700 <ben700@hotmail.com>
2020-09-17 16:06:22 +0100

    Update colourMap.py

--- drone/colourMap.py
+++ drone/colourMap.py
@@ -4,17 +4,20 @@ from colour import Color
 from matplotlib import cm
 
 def getTempColour(Temp):
-     #normalize item number values to colormap
-     norm = matplotlib.colors.Normalize(vmin=0, vmax=400)
+     try:
+          #normalize item number values to colormap
+          norm = matplotlib.colors.Normalize(vmin=0, vmax=400)
 
-     #colormap possible values = viridis, jet, spectral
-     #rgba_color = cm.coolwarm(norm(Temp),bytes=True) 
-     rgba_color = cm.RdBu(norm(MoistPer),bytes=True) 
+          #colormap possible values = viridis, jet, spectral
+          #rgba_color = cm.coolwarm(norm(Temp),bytes=True) 
+          rgba_color = cm.RdBu(norm(MoistPer),bytes=True) 
      
-     print("rgba_color =" + str(rgba_color))
+          #print("rgba_color =" + str(rgba_color))
 
-     c = Color(rgb=(rgba_color[0]/255,rgba_color[1]/255,rgba_color[2]/255))
-     print("color =" + str(c))
+          #c = Color(rgb=(rgba_color[0]/255,rgba_color[1]/255,rgba_color[2]/255))
+          #print("color =" + str(c))
+       except:
+          c = '#0000FF'
      return c
 
 
