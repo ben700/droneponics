958cc58f67b6209be82b16e3605571f412dda92e
ben700 <ben700@hotmail.com>
2020-09-18 01:21:39 +0100

    Update droneDoser.py

--- droneDoser.py
+++ droneDoser.py
@@ -610,7 +610,9 @@ try:
         _log.info("Update Timer Run")
         blynk.virtual_write(250, "Running")
         now = datetime.now()
+	weekDay = now.weekday()
         blynk.virtual_write(0, now.strftime("%d/%m/%Y %H:%M:%S"))
+        blynk.virtual_write(98, "The weekday is " + weekDay)
 
         cTemp = sensors[0].sensor.query("R").split(":")[1].strip().rstrip('\x00')
         if (float(cTemp) < 0) :
