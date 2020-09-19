aca3154ecf6e715021324d7c3756b379ff0951c8
ben700 <ben700@hotmail.com>
2020-09-19 16:14:57 +0100

    Update droneDoser.py

--- droneDoser.py
+++ droneDoser.py
@@ -743,7 +743,7 @@ try:
               else:
                     cTemp= "TEMP CAL to " + tempCalPoints +" points."
               
-		
+              _log.info("----------------------")
               _log.info(sensors[1].sensor.query("R"))
               sensors[1].value = sensors[1].sensor.query("RT,"+cTemp).split(":")[1].strip().rstrip('\x00') #EC
               sensors[2].value = sensors[2].sensor.query("RT,"+sensors[0].value).split(":")[1].strip().rstrip('\x00')  #pH
