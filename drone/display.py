cd8e434b8bd28a7c3bc1145e00c02b70aaac9060
ben700 <ben700@hotmail.com>
2020-09-24 23:45:37 +0100

    Update display.py

--- drone/display.py
+++ drone/display.py
@@ -125,9 +125,8 @@ class LCD:
 
     def updateLCD (self, r1, r2, r3, r4):
         self._log.debug  ("updateLCD")
-        self.lcd.clear()  
-        self.lcd.printline(0, "Pump 1 status = " + str(r1))
-        self.lcd.printline(1, "Pump 2 status = " + str(r2))
-        self.lcd.printline(2, "Pump 3 status = " + str(r3))
-        self.lcd.printline(3, "Pump 4 status = " + str(r4))
+        self.lcd.printline(0, "Pump 1 status = " + str(r1) + " ")
+        self.lcd.printline(1, "Pump 2 status = " + str(r2) + " ")
+        self.lcd.printline(2, "Pump 3 status = " + str(r3) + " ")
+        self.lcd.printline(3, "Pump 4 status = " + str(r4) + " ")
   
