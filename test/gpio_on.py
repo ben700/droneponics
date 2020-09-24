a474e3d99119fc2d4854cdea57700f762cfef417
ben700 <ben700@hotmail.com>
2020-09-24 21:25:56 +0100

    Update gpio_on.py

--- test/gpio_on.py
+++ test/gpio_on.py
@@ -1,11 +1,16 @@
 import RPi.GPIO as GPIO            # import RPi.GPIO module  
 from time import sleep             # lets us have a delay  
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD  
 
+GPIO.setup(12, GPIO.OUT)           # set GPIO24 as an output   
+GPIO.setup(16, GPIO.OUT)           # set GPIO24 as an output   
 GPIO.setup(20, GPIO.OUT)           # set GPIO24 as an output   
 GPIO.setup(21, GPIO.OUT)           # set GPIO24 as an output   
   
+GPIO.output(12, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
+GPIO.output(16, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
 GPIO.output(20, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
 GPIO.output(21, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
 
-print ("20 and 21 are on")
+print ("12, 16, 20 and 21 are on")
