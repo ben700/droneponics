465b211f71d2ba6b708ead7cda27f2f8e20e4975
ben700 <ben700@hotmail.com>
2020-09-26 00:38:03 +0100

    Update droneDoser.py

--- droneDoser.py
+++ droneDoser.py
@@ -692,7 +692,11 @@ try:
                    blynk.virtual_write(y-10, dose.dose)			
                    y = y + 1		       
               
-              _log.info('Check cal for sensors')			
+              _log.info('Check cal for sensors')
+              _log.info(sensors[0].sensor)
+	      _log.info(sensors[0].sensor.query("Cal,?"))
+			
+		
               blynk.virtual_write(98,"Temp Cal " + sensors[0].sensor.query("Cal,?")+ '\n')
               blynk.virtual_write(98,"EC Cal " + sensors[1].sensor.query("Cal,?")+ '\n')
               blynk.virtual_write(98,"Temp Cal " + sensors[2].sensor.query("Cal,?")+ '\n')
