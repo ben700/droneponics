acd5e5c7106ecb823e8f32b744ea3f76040031d6
ben700 <ben700@hotmail.com>
2020-09-26 01:27:30 +0100

    Update nutrientSchedule.py

--- drone/nutrientSchedule.py
+++ drone/nutrientSchedule.py
@@ -1,7 +1,7 @@
 from drone import *         
  
-LED =[10,11,12,13,14,15]
-VolumePin =[20,21,22,23,24,25]
+LED =[10,11,12,13,14,15,16]
+VolumePin =[20,21,22,23,24,25,26]
 
 def lightPin(): 
     return 26
@@ -16,7 +16,7 @@ def buildNutrientMix(nutrientMix, _log, scheduleWeek='Grow'):
     nutrientMix.append( Dose(14, 10.00, LED[3], "Rhizotonic", VolumePin[3], 1000))
     nutrientMix.append( Dose(15, 4.00, LED[4], "Cannazym", VolumePin[4], 1000))
     nutrientMix.append( Dose(16, 1.00, LED[5], "PK 13-14", VolumePin[5], 500)) 
-    nutrientMix.append( Dose(17, 1.00, LED[5], "Boost", VolumePin[5], 1000)) 
+    nutrientMix.append( Dose(17, 1.00, LED[6], "Boost", VolumePin[56], 1000)) 
  
     
     return nutrientMix
