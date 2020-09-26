738f7d7e3029adf84c7bd1babb5ac7ebd4734a01
ben700 <ben700@hotmail.com>
2020-09-26 02:11:01 +0100

    Update droneDoser.py

--- droneDoser.py
+++ droneDoser.py
@@ -450,7 +450,11 @@ try:
         if(value[0] == '1'):
             _log.info("Pump for " +nutrientMix[x].name +" = " + nutrientMix[x].pump.query("X") + '\n')
             dosed = nutrientMix[x].pump.query("R").split(":")[1].strip().rstrip('\x00')
-            nutrientMix[x].volume = nutrientMix[x].volume + float(nutrientMix[x].pump.query("TV,?").split("TV,")[1])
+            volumeThisTime = nutrientMix[x].pump.query("TV,?").split("TV,")[1]
+            _log.info("Just dosed " + str(dosed) + "so total now " + str(volumeThisTime))
+            nutrientMix[x].volume = float(nutrientMix[x].volume) + float(volumeThisTime)
+	    _log.info("overall total now " + str(nutrientMix[x].volume))
+        
             blynk.virtual_write(nutrientMix[x].volumePin, nutrientMix[x].volume )
             blynk.virtual_write(98, now.strftime("%d/%m/%Y %H:%M:%S") + " :- Had used " + lVolume + " ml| Now Dosed :"+ str(nutrientMix[x].volume) + "ml" + '\n') 
             blynk.virtual_write(98, now.strftime("%d/%m/%Y %H:%M:%S") + " :- Pump for " + nutrientMix[x].name + ":- STOPPED"  + " Dosed :"+ str(dosed) + "ml" + '\n') 
