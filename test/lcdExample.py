4daee1c863751470015f75968f72ac8d86b4a24e
ben700 <ben700@hotmail.com>
2020-10-05 04:44:45 +0100

    Update lcdExample.py

--- test/lcdExample.py
+++ test/lcdExample.py
@@ -1,3 +1,4 @@
+import drone
 import liquidcrystal_i2c
 
 cols = 20
@@ -5,17 +6,17 @@ rows = 4
 try:
     lcd = liquidcrystal_i2c.LiquidCrystal_I2C(0x27, 1, numlines=rows)
     lcd.printline(0, 'LCM2004 IIC V2'.center(cols))
-    lcd.printline(1, 'and'.center(cols))
-    lcd.printline(2, 'python-')
-    lcd.printline(3, 'liquidcrystal_i2c'.rjust(cols))
+    lcd.printline(1, 'host-' + drone.gethostname())
+    lcd.printline(2, 'ip-' + drone.get_ip())
+    lcd.printline(3, 'liquidcrystal_i2c on bus 0')
 except:
     print("no lcd on bus 1")
     
 try:
     lcd = liquidcrystal_i2c.LiquidCrystal_I2C(0x27, 0, numlines=rows)
     lcd.printline(0, 'LCM2004 IIC V2'.center(cols))
-    lcd.printline(1, 'and'.center(cols))
-    lcd.printline(2, 'python-')
-    lcd.printline(3, 'liquidcrystal_i2c'.rjust(cols))
+    lcd.printline(1, 'host-' + drone.gethostname())
+    lcd.printline(2, 'ip-' + drone.get_ip())
+    lcd.printline(3, 'liquidcrystal_i2c on bus 0')
 except:
     print("no lcd on bus 0")
