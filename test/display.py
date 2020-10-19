a7a1ff47170afe43cbbdc4b4521f95bcf33aa226
ben700 <ben700@hotmail.com>
2020-10-19 21:17:57 +0100

    Create display.py

---
+++ test/display.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import time
+import sys
+import os
+picdir = '/home/pi/droneponics/pic/'
+libdir = '/home/pi/droneponics/lib/'
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+import logging
+import time
+from waveshare_2inch_LCD import ST7789
+from PIL import Image,ImageDraw,ImageFont
+
+logging.basicConfig(level=logging.INFO)
+print(picdir)
+
+def updateLCD ():
+    print ("2inch LCD Module")
+    disp = ST7789.ST7789()
+    # Initialize library.
+    disp.Init()
+    # Clear display.
+    disp.clear()
+
+  
+    font30 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 30)
+    font15 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 15)
+
+    
+
+    # read bmp file 
+    bmp = Image.open(os.path.join(picdir, 'dronePonics_Logo.bmp'))	
+    image.paste(bmp, (0,0))  
+    image=image.rotate(180)
+    disp.ShowImage(image)
+
+updateLCD ()
+
