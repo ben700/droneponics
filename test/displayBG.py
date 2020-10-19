ae8b4dad322e9fcc2cf7b3672d9b390200f0a3ac
ben700 <ben700@hotmail.com>
2020-10-19 21:57:45 +0100

    Update displayBG.py

--- test/displayBG.py
+++ test/displayBG.py
@@ -33,10 +33,10 @@ def updateLCD ():
     image.paste(bmp, (0,0))  
   
     draw = ImageDraw.Draw(image)
-    draw.text((50, 40), 'Pump 1 = On', font = font30, fill = "BLACK")
-    draw.text((50, 90), 'Pump 2 = On', font = font30, fill = "BLACK")
+    draw.text((50, 50), 'Pump 1 = On', font = font30, fill = "BLACK")
+    draw.text((50, 95), 'Pump 2 = On', font = font30, fill = "BLACK")
     draw.text((50, 140), 'Pump 3 = On', font = font30, fill = "BLACK")
-    draw.text((50, 190), 'Pump 4 = On', font = font30, fill = "BLACK")
+    draw.text((50, 185), 'Pump 4 = On', font = font30, fill = "BLACK")
     
     
     disp.ShowImage(image)
