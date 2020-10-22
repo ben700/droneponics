13707f97ab32bc538435ef0f2d5ce81379282262
ben700 <ben700@hotmail.com>
2020-10-22 02:39:24 +0100

    Update display.py

--- test/display.py
+++ test/display.py
@@ -13,7 +13,7 @@ import time
 from waveshare_2inch_LCD import ST7789
 from PIL import Image,ImageDraw,ImageFont
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 
 def updateLCD ():
@@ -25,16 +25,9 @@ def updateLCD ():
     disp.clear()
     
     image = Image.new('RGB', (disp.height,disp.width), (255,255,255)) 
-
-    font30 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 30)
-    font15 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 15)
-
-    
-
     # read bmp file 
     bmp = Image.open(os.path.join(picdir, 'droneponics_logo.bmp'))	
     image.paste(bmp, (10,10))  
-    image=image.rotate(180)
     disp.ShowImage(image)
 
 updateLCD ()
