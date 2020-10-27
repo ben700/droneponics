b26a76e395932f367699f74f4fcc7656824030bb
ben700 <ben700@hotmail.com>
2020-10-27 20:32:15 +0000

    Update installLCD.sh

--- installLCD.sh
+++ installLCD.sh
@@ -1,3 +1,7 @@
 pip3 install https://github.com/pl31/python-liquidcrystal_i2c/archive/master.zip
 sudo pip3 install https://github.com/pl31/python-liquidcrystal_i2c/archive/master.zip
-
+cd
+git clone -b master --single-branch https://github.com/oweidner/i2crelay.git
+cd i2crelay
+pip3 install --upgrade .
+cd
