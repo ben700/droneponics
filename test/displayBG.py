4a33c4970d7b8a327c1852811bc8deedee31ea50
ben700 <ben700@hotmail.com>
2020-10-27 23:09:04 +0000

    Update displayBG.py

--- test/displayBG.py
+++ test/displayBG.py
@@ -8,6 +8,9 @@ libdir = '/home/pi/droneponics/lib/'
 if os.path.exists(libdir):
     sys.path.append(libdir)
 
+if os.path.exists(picdir):
+    sys.path.append(picdir)
+    
 import logging
 import time
 from waveshare_2inch_LCD import ST7789
@@ -29,7 +32,7 @@ def updateLCD ():
     font30 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 30)
     font15 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 15)
     # read bmp file 
-    bmp = Image.open(os.path.join(picdir, 'background.bmp'))	
+    bmp = Image.open(os.path.join(picdir, 'droneponics_logo.bmp'))	
     image.paste(bmp, (0,0))  
   
     draw = ImageDraw.Draw(image)
