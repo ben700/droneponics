abd6eb06e9a7946c2df0caba3133fa80013b4c9d
ben700 <ben700@hotmail.com>
2020-11-07 17:51:39 +0000

    Create app.py

---
+++ GUI/app.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+from tkinter import * 			# imports the Tkinter lib
+root = Tk()				# create the root object
+root.wm_title("Hello World")		# sets title of the window 
+root.mainloop()				# starts the GUI loop
