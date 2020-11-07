cbdda440c6ec5da84fb3f3f7ef514252c05de708
ben700 <ben700@hotmail.com>
2020-11-07 20:56:18 +0000

    Update config.py

--- GUI/config.py
+++ GUI/config.py
@@ -27,17 +27,19 @@ def tempCalClicked():
     label_1["text"]="Temp Calibration"
     probe = AtlasI2C(102)
     label_info["text"]= probe.query("i").strip().rstrip('\x00')
-        
+    label_cal["text"]= probe.query("cal,?").strip().rstrip('\x00')
 
 def phCalClicked():
     label_1["text"]="pH Calibration"
     probe = AtlasI2C(99)
     label_info["text"]= probe.query("i").strip().rstrip('\x00')	
+    label_cal["text"]= probe.query("cal,?").strip().rstrip('\x00')
 
 def ecCalClicked():
     label_1["text"]="EC Calibration"
     probe = AtlasI2C(100)
     label_info["text"]= probe.query("i").strip().rstrip('\x00')	
+    label_cal["text"]= probe.query("cal,?").strip().rstrip('\x00')
 	
 def btnExit():
   	root.destroy()
@@ -54,6 +56,11 @@ label_1 = Label(root, text="Raspberry Pi Graphical User Interface", font="Verdan
 			padx = 1)
 
 label_info = Label(root, text="", font="Verdana 26 bold",
+			fg="#000",
+			bg="#99B898",
+			pady = 1,
+			padx = 1)
+label_cal = Label(root, text="", font="Verdana 26 bold",
 			fg="#000",
 			bg="#99B898",
 			pady = 1,
@@ -80,6 +87,7 @@ ecCalButton = Button(root, text="Cal EC",background = "#C06C84",
 
 label_1.grid(row=0, column=0, columnspan=3)
 label_info.grid(row=1, column=1, columnspan=2)
+label_cal.grid(row=1, column=1, columnspan=2)
 exitButton.grid(row = 1 ,column = 0)
 tempCalButton.grid(row = 2 ,column = 0)
 phCalButton.grid(row = 2 ,column = 1)
