1217b10dd671ac26a9ff13ff4c251e5c94f9e878
ben700 <ben700@hotmail.com>
2020-11-07 22:40:57 +0000

    Update config.py

--- GUI/config.py
+++ GUI/config.py
@@ -7,7 +7,8 @@ import drone
 import logging
 
 from tkinter import * 			# imports the Tkinter lib
-
+# pip install pillow
+from PIL import Image, ImageTk
 
     # tune console logging
 _log = logging.getLogger('BlynkLog')
@@ -23,14 +24,11 @@ _log.setLevel(logging.DEBUG)
 root = Tk()				# create the root object
 root.wm_title("GUI")			# sets title of the window
 
-#filename = PhotoImage(file = "/home/pi/droneponics/pic/droneponics_logo.bmp")
-image = Image.open("/home/pi/droneponics/pic/droneponics_logo.bmp")
-tkpi = ImageTk.PhotoImage(image)
-label_image = Tkinter.Label(root, image=tkpi)
-label_image.place(x=0,y=0)
-
-#background_label = Label(root, image=filename)
-#background_label.place(x=0, y=0, relwidth=1, relheight=1)
+load = Image.open("/home/pi/droneponics/pic/droneponics_logo.bmp")
+render = ImageTk.PhotoImage(load)
+img = Label(self, image=render)
+img.image = render
+img.place(x=0, y=0)
 
 root.configure(bg="#99B898")		# change the background color 
 root.attributes("-fullscreen", True) 	# set to fullscreen
