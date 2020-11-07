7144d689093a3c8d2234b8b6951c5c775ac6a574
ben700 <ben700@hotmail.com>
2020-11-07 20:41:34 +0000

    Update config.py

--- GUI/config.py
+++ GUI/config.py
@@ -8,6 +8,7 @@ ledB= False
 
 
 def btnClicked():
+  global ledB    
   if(ledB):
     ledButton["text"]="LED OFF"
     ledB= False
@@ -35,11 +36,23 @@ exitButton = Button(root, text="Exit", background = "#C06C84",
 ledButton = Button(root, text="LED OFF",background = "#C06C84", 
       command=btnClicked, height=10, width=20, font = "Arial 16 bold")
 
+tempCalButton = Button(root, text="Cal Temp",background = "#C06C84", 
+      command=btnClicked, height=10, width=20, font = "Arial 16 bold")
+
+
+phCalButton = Button(root, text="Cal pH",background = "#C06C84", 
+      command=btnClicked, height=10, width=20, font = "Arial 16 bold")
+
+
+ecCalButton = Button(root, text="Cal EC",background = "#C06C84", 
+      command=btnClicked, height=10, width=20, font = "Arial 16 bold")
+
 
 label_1.grid(row=0, column=0)
 exitButton.grid(row = 1 ,column = 0)
-ledButton.grid(row = 2 ,column = 0)
-
+tempCalButton.grid(row = 2 ,column = 0)
+phCalButton.grid(row = 2 ,column = 1)
+ecCalButton.grid(row = 2 ,column = 2)
 
 root.bind("<Escape>", end_fullscreen)
 root.mainloop()				# starts the GUI loop
