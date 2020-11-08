90444662b71ba09a2a57fab07d7b8235ddd40e18
ben700 <ben700@hotmail.com>
2020-11-08 04:04:49 +0000

    Update config.py

--- GUI/config.py
+++ GUI/config.py
@@ -54,11 +54,11 @@ def tempCalClicked():
 def phCalClicked():
     probe = AtlasI2C(99)
     deviceInfo = probe.query("i")
-    label_info["text"]= "Device " +deviceInfo.split(":")[1].split(",")[1].strip().rstrip('\x00') + ' Firmware ' + deviceInfo.split(":")[1].split(",")[2].strip().rstrip('\x00')
+    label_info["text"]= "Device " +deviceInfo.split(":")[1].split(",")[1].strip().rstrip('\x00') + '\nFirmware ' + deviceInfo.split(":")[1].split(",")[2].strip().rstrip('\x00')
     calInfo = probe.query("cal,?")
     label_cal["text"]= "Device Calibrated to " + calInfo.split(":")[1].split(",")[1].strip().rstrip('\x00') + " point(s)"
     statusInfo=probe.query("Status")
-    label_status["text"]= "Reason for restart " + statusInfo.split(":")[1].split(",")[1].strip().rstrip('\x00') + " Voltage " +  statusInfo.split(":")[1].split(",")[2].strip().rstrip('\x00')
+    label_status["text"]= "Reason for restart " + statusInfo.split(":")[1].split(",")[1].strip().rstrip('\x00') + "\nVoltage " +  statusInfo.split(":")[1].split(",")[2].strip().rstrip('\x00')
     readProbeButton["text"]= "Read pH"
     readProbeButton["command"]= readPHClicked
     calProbeButton["text"]= "Cal PH"
