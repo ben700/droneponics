505f2bbb5f0d78107a4514241ad501a579cb1b85
ben700 <ben700@hotmail.com>
2020-11-16 21:34:28 +0000

    Update page.py

--- GUI/page.py
+++ GUI/page.py
@@ -1,21 +1,30 @@
-import tkinter
+
+import tkinter as tk
+root=tk.Tk()
+root.geometry("360x360")
+
+frame=tk.Frame(root,bg='lightblue')
+frame.place(relx=0.2,rely=0.2,relheight=0.6,relwidth=0.6)
 
 def page1():
-    page2text.pack_forget()
-    page1text.pack()
+    label=tk.Label(frame,text='this is the page1')
+    label.place(relx=0.3,rely=0.4)
 
 def page2():
-    page1text.pack_forget()
-    page2text.pack()
+    label=tk.Label(frame,text='this is the page2')
+    label.place(relx=0.3,rely=0.4)
 
-window = tkinter.Tk()
+def page3():
+    label=tk.Label(frame,text='this is the page3')
+    label.place(relx=0.3,rely=0.4)
 
-page1btn = tkinter.Button(window, text="Page 1", command=page1)
-page2btn = tkinter.Button(window, text="Page 2", command=page2)
+bt=tk.Button(root,text='page1',command=page1)
+bt.grid(column=0,row=0)
 
-page1text = tkinter.Label(window, text="This is page 1")
-page2text = tkinter.Label(window, text="This is page 2")
+bt1=tk.Button(root,text='page2',command=page2)
+bt1.grid(row=0,column=1)
 
-page1btn.pack()
-page2btn.pack()
-page1text.pack()
+bt2=tk.Button(root,text='page3',command=page3)
+bt2.grid(row=0,column=2)
+
+root.mainloop()`
