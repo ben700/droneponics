73cc6d13fbc643badd5fe04d1e6ebab964e0c723
ben700 <ben700@hotmail.com>
2020-11-17 01:43:13 +0000

    Update page.py

--- GUI/page.py
+++ GUI/page.py
@@ -34,25 +34,25 @@ def pumpPage7():
     label=tk.Label(frame,text='this is the pump 7')
     label.place(relx=0.3,rely=0.4)
     
-b1=tk.Button(root,text='Pump 1',command=page1)
+b1=tk.Button(root,text='Pump 1',command=pumpPage1)
 bt1.grid(column=1,row=0)
 
-bt2=tk.Button(root,text='Pump 2',command=page2)
+bt2=tk.Button(root,text='Pump 2',command=pumpPage2)
 bt2.grid(row=1,column=1)
 
-bt3=tk.Button(root,text='Pump 3',command=page3)
+bt3=tk.Button(root,text='Pump 3',command=pumpPage3)
 bt3.grid(row=1,column=2)
 
-bt4=tk.Button(root,text='Pump 4',command=page4)
+bt4=tk.Button(root,text='Pump 4',command=pumpPage4)
 bt4.grid(row=1,column=3)
 
-bt5=tk.Button(root,text='Pump 5',command=page5)
+bt5=tk.Button(root,text='Pump 5',command=pumpPage5)
 bt5.grid(row=2,column=0)
 
-bt6=tk.Button(root,text='Pump 6',command=page6)
+bt6=tk.Button(root,text='Pump 6',command=pumpPage6)
 bt6.grid(row=2,column=1)
 
-bt7=tk.Button(root,text='Pump 7',command=page7)
+bt7=tk.Button(root,text='Pump 7',command=pumpPage7)
 bt7.grid(row=2,column=2)
 
 root.mainloop()
