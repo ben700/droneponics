5dd89746ba87cd4e3bb9fec6c71396093be490e7
ben700 <ben700@hotmail.com>
2020-11-26 01:31:36 +0000

    Update colourMap.py

--- drone/colourMap.py
+++ drone/colourMap.py
@@ -28,9 +28,9 @@ def getCO2Colour(_log, CO2):
           _log.info("getCO2Colour CO2 =" + str(CO2))
           #normalize item number values to colormap
           norm = matplotlib.colors.Normalize(vmin=0, vmax=2000)
-          _log.info("done norm")
+          _log.info("done norm CO2 = " + str(CO2))
           #colormap possible values = viridis, jet, spectral
-          rgba_color = cm.winter(norm(CO2),bytes=True) 
+          rgba_color = cm.winter(norm(int(CO2)),bytes=True) 
           _log.info("rgba_color =" + str(rgba_color))
           c = Color(rgb=(rgba_color[0]/255,rgba_color[1]/255,rgba_color[2]/255))
           _log.info("getCO2Colour =" + str(c))
