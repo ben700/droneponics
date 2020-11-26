161990df0ffda00d4bf547538ccc5981e7389590
ben700 <ben700@hotmail.com>
2020-11-26 04:42:30 +0000

    Create fill.py

---
+++ test/Display/fill.py
@@ -0,0 +1,27 @@
+import sys
+import os
+sys.path.append('/home/pi/droneponics')
+import drone
+import liquidcrystal_i2c
+
+cols = 20
+rows = 4
+lcd=None
+try:
+    lcd = liquidcrystal_i2c.LiquidCrystal_I2C(0x27, 1, numlines=rows)
+except:
+    print("no lcd to create on bus 1")
+    
+try:
+    self.lcd.printline(0, "asdfghjklqwertyuiopzxcvbnm1234567890ASDFGHJKLZXCVBNM<QWERTYUIOP")
+    self.lcd.printline(1, "asdfghjklqwertyuiopzxcvbnm1234567890ASDFGHJKLZXCVBNM<QWERTYUIOP")
+    self.lcd.printline(2, "asdfghjklqwertyuiopzxcvbnm1234567890ASDFGHJKLZXCVBNM<QWERTYUIOP")
+    self.lcd.printline(3, "asdfghjklqwertyuiopzxcvbnm1234567890ASDFGHJKLZXCVBNM<QWERTYUIOP")
+except:
+    print("no lcd on to fill on bus 1")
+    
+try:
+    lcd.autoscroll()
+except:
+    print("no lcd to get autoscroll from on bus 1")
+    
