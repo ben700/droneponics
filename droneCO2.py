6ac16191a70a252771123b9609ea796d35c03a65
ben700 <ben700@hotmail.com>
2020-11-26 01:04:57 +0000

    Update droneCO2.py

--- droneCO2.py
+++ droneCO2.py
@@ -71,7 +71,7 @@ try:
         for sensor in sensors:
            if sensor is not None:
               sensor.display(blynk)
-              blynk.set_property(sensors[0].displayPin, 'color',sensors[0].color)	
+              blynk.set_property(sensors[0].displayPin, 'color', colours['OFFLINE'])	
       		
     @blynk.handle_event('write V255')
     def rebooter(pin, value):
