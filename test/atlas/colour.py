35b8b79e4a4859f359ae653306eab22187a93874
ben700 <ben700@hotmail.com>
2020-11-26 06:14:56 +0000

    Update colour.py

--- test/atlas/colour.py
+++ test/atlas/colour.py
@@ -10,6 +10,6 @@ from AtlasI2C import (AtlasI2C)
 colour = AtlasI2C(112)
 
 try:
-    print(colour.query("R").rstrip('\x00') + '\n'))        
+    print(colour.query("R").rstrip('\x00') + '\n')       
 except:
     print("except")
