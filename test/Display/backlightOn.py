70441e0b0f08f16285dc69f82786e0a5d5208fb3
ben700 <ben700@hotmail.com>
2020-11-26 04:43:20 +0000

    Update backlightOn.py

--- test/Display/backlightOn.py
+++ test/Display/backlightOn.py
@@ -6,16 +6,20 @@ import liquidcrystal_i2c
 
 cols = 20
 rows = 4
+lcd=None
 try:
     lcd = liquidcrystal_i2c.LiquidCrystal_I2C(0x27, 1, numlines=rows)
-    lcd.Backlight()
-    print("Device found at address 27 on bus 1")
 except:
-    print("no lcd on bus 1")
+    print("no lcd to create on bus 1")
     
 try:
-    lcd = liquidcrystal_i2c.LiquidCrystal_I2C(0x27, 0, numlines=rows)
     lcd.Backlight()
-    print("Device found at address 27 on bus 0")
 except:
-    print("no lcd on bus 0")
+    print("no lcd on turn on backloight on bus 1")
+    
+try:
+    print("State of the backlight is now ")
+    print(lcd.getBacklight())
+except:
+    print("no lcd to get state from on bus 1")
+    
