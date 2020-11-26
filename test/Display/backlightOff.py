605e8da4135341d72236dcee90e111e53509156c
ben700 <ben700@hotmail.com>
2020-11-26 04:33:03 +0000

    Update and rename backlightOffpy to backlightOff.py

--- test/Display/backlightOff.py
+++ test/Display/backlightOff.py
@@ -9,13 +9,6 @@ rows = 4
 try:
     lcd = liquidcrystal_i2c.LiquidCrystal_I2C(0x27, 1, numlines=rows)
     lcd.noBacklight()
-    print("Device found at address 27 on bus 1")
 except:
     print("no lcd on bus 1")
     
-try:
-    lcd = liquidcrystal_i2c.LiquidCrystal_I2C(0x27, 0, numlines=rows)
-    lcd.noBacklight()
-    print("Device found at address 27 on bus 0")
-except:
-    print("no lcd on bus 0")
