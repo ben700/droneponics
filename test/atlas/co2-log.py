2aa463e63209d728002347f8230dd786f8e7085c
ben700 <ben700@hotmail.com>
2020-11-26 06:10:50 +0000

    Update co2-log.py

--- test/atlas/co2-log.py
+++ test/atlas/co2-log.py
@@ -10,8 +10,14 @@ from AtlasI2C import (AtlasI2C)
 CO2 = AtlasI2C(105)
 
 while True:
-    f = open("/home/pi/co2Data.txt", "a")
-    now = datetime.now()
-    f.write( now.strftime("%d/%m/%Y %H:%M:%S") + "     "+ CO2.query("R").rstrip('\x00') + '\n')
-    f.close()
-    time.sleep(5)
+    try:
+        f = open("/home/pi/co2Data.txt", "a")
+        now = datetime.now()
+        f.write( now.strftime("%d/%m/%Y %H:%M:%S") + "     "+ CO2.query("R").rstrip('\x00') + '\n')
+        f.close()
+        time.sleep(5)
+    except:
+        f = open("/home/pi/co2Data.txt", "a")
+        now = datetime.now()
+        f.write( now.strftime("%d/%m/%Y %H:%M:%S") + "     except" + '\n')
+        f.close()
