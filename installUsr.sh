b29d8db0df9f504a12e30fd832c8bf1a0124144b
ben700 <ben700@hotmail.com>
2020-11-26 03:47:06 +0000

    Update installUsr.sh

--- installUsr.sh
+++ installUsr.sh
@@ -1,32 +1,49 @@
 echo "Installing User Mods"
 pip3 install blynklib
 
+#Cairo graphics library
+apt-get install libcairo2-dev -y 
+# TSL2591 high precision light sensor
 sudo pip3 install adafruit-circuitpython-tsl2591
+#think python one is unused
 sudo pip3 install python-tsl2591
+#needed for droneAir
 pip3 install adafruit-blinka
 sudo pip3 install adafruit-blinka
+sudo pip3 install --force-reinstall adafruit-blinka
+
 sudo pip3 install board
 sudo pip3 install digitalio
+#SSD1306 OLED Display Module
 sudo pip3 install Adafruit-SSD1306
 pip3 install adafruit-circuitpython-ssd1306
+#Python Imaging Library
 sudo apt-get install python3-pil
-sudo pip3 install --force-reinstall adafruit-blinka
+#BME sensors
 sudo pip3 install adafruit-circuitpython-bme280
 sudo pip3 install adafruit-circuitpython-bme680
+#meteocalc used to do colour
 sudo pip3 install meteocalc
+#Triple-Axis Accelerometer I think unused
 pip3 install adafruit-circuitpython-lis3dh
+#analog to digital converters
 pip3 install adafruit-circuitpython-ads1x15
+#CO2 Sensor
 sudo pip3 install mh-z19
+#used colours
 sudo pip3 install colour
 sudo pip3 install configparser
-sudo apt-get install lxde
-sudo apt-get install xterm -y
+# display used for crystal display
 sudo pip3 install RPLCD
+#used to access I2C for displays 
 sudo pip3 install smbus2
+#another  display used for crystal display
 pip3 install https://github.com/pl31/python-liquidcrystal_i2c/archive/master.zip
 sudo pip3 install https://github.com/pl31/python-liquidcrystal_i2c/archive/master.zip
 
+#green soil moisture
 pip3 install adafruit-circuitpython-seesaw
+#This script updates all submodules to the latest tag  https://github.com/adafruit/Adafruit_CircuitPython_Bundle/blob/master/update-submodules.sh
 git submodule init
 git submodule update
 git submodule foreach git fetch --all
