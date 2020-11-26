bf88e73bf5ebdff6dd87e9cd59ff44a0f12d4570
ben700 <ben700@hotmail.com>
2020-11-26 05:00:43 +0000

    Update backlightOn.py

--- test/Display/backlightOn.py
+++ test/Display/backlightOn.py
@@ -10,17 +10,15 @@ lcd=None
 try:
     lcd = liquidcrystal_i2c.LiquidCrystal_I2C(0x27, 1, numlines=rows)
 except:
-    print("Except: no lcd to create on bus 1")
-    
+    print("Except: Creating lcd on bus 1")
 try:
     lcd.Backlight()
 except:
-    print("Except: Turning on backlight on bus 1")
-    
+    print("Except: Turning on backlight on bus 1") 
 try:
     print("State of the backlight is now " + str(lcd.getBacklight()))
 except NotImplementedError:
-    print("Except: NotImplementedError getBacklight()")
+    print("Except NotImplementedError: getBacklight()")
 except:
     print("Except: Getting state from on bus 1")
     
