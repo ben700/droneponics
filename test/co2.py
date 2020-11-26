9b3be43dc8cda7dfbd55e99c8a9b618c309c95b0
ben700 <ben700@hotmail.com>
2020-11-26 05:25:45 +0000

    Create co2.py

---
+++ test/co2.py
@@ -0,0 +1,14 @@
+import datetime
+import time
+from datetime import datetime
+import sys
+import os
+sys.path.append('/home/pi/droneponics')
+from AtlasI2C import (AtlasI2C)
+
+
+CO2 = AtlasI2C(105)
+
+f = open("/home/pi/co2Data.txt", "a")
+f.write(CO2.query("R"))
+f.close()
