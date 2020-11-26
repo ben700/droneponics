4c0b4234cf45a72ebed8a14e3922dcb738fe8cfc
ben700 <ben700@hotmail.com>
2020-11-26 01:06:58 +0000

    Update droneMonitorPro.py

--- droneMonitorPro.py
+++ droneMonitorPro.py
@@ -96,9 +96,9 @@ try:
     def rebooter(pin, value):
         _log.info( "User reboot")
         blynk.virtual_write(250, "Reboot")
-        blynk.set_property(250, 'color', colours['OFFLINE'])	
+        blynk.set_property(250, 'color', drone.colours['OFFLINE'])	
         blynk.virtual_write(98, "User Reboot " + '\n')
-        blynk.set_property(systemLED, 'color', colours['OFFLINE'])	
+        blynk.set_property(systemLED, 'color', drone.colours['OFFLINE'])	
         os.system('sh /home/pi/updateDroneponics.sh')
         blynk.virtual_write(98, "System updated and restarting " + '\n')
         os.system('sudo reboot')
