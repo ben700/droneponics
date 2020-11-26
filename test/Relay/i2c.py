ded899ba78c33b17fbdf95caddbe6adeb107a09f
ben700 <ben700@hotmail.com>
2020-11-26 16:34:25 +0000

    Update i2c.py

--- test/Relay/i2c.py
+++ test/Relay/i2c.py
@@ -10,17 +10,17 @@ I2C_BUS = 1
 # depends on the hardware pins A0 - A2
 I2C_ADDR = 0x26
 
-r1 = I2CRelayBoard(I2C_BUS, I2C_ADDR)
+i2CRelayBoard = I2CRelayBoard(I2C_BUS, I2C_ADDR)
 
-r1.switch_all_on()
+i2CRelayBoard.switch_all_on()
 time.sleep(1.0)
 
-r1.switch_all_off()
+i2CRelayBoard.switch_all_off()
 time.sleep(1.0)
 
 for relay in range(5, 9):
     print("Switching relay {}".format(relay))
-    r1.switch_on(relay)
+    i2CRelayBoard.switch_on(relay)
     time.sleep(0.5)
-    r1.switch_off(relay)
+    i2CRelayBoard.switch_off(relay)
     time.sleep(0.5)
