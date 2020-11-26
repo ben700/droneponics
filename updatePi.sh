efc85279c2827353b287c9421206dfb0225dcb8d
ben700 <ben700@hotmail.com>
2020-11-26 06:56:07 +0000

    Update updatePi.sh

--- updatePi.sh
+++ updatePi.sh
@@ -1,11 +1,5 @@
-echo "--------update pip3---------------------------------------------"
-apt-get install libcairo2-dev -y
-#sudo pip3 freeze > /home/pi/droneponics/requirements.txt
-#sudo python3 /home/pi/droneponics/replace.py /home/pi/droneponics/requirements.txt
-#sudo pip3 install -r /home/pi/droneponics/requirements.txt --upgrade
-
-echo "--------installUsr---------------------------------------------"
-/home/pi/droneponics/installUsr.sh
+echo "---------------------------------------------"Update Code---------------------------------------------""
+/home/pi/updateDroneponics.sh
 
 echo "---------------------------------------------"Update PI---------------------------------------------""
 sudo apt full-upgrade -y
@@ -15,10 +9,6 @@ sudo apt upgrade -y
 sudo apt dist-upgrade -y
 sudo apt autoremove -y
 sudo apt autoclean -y
-#echo "---------------------------------------------"Update droneponics---------------------------------------------""
-#/home/pi/updateDroneponics.sh
-#moved to non root
-echo "---------------------------------------------"Update Code---------------------------------------------""
-/home/pi/updateDroneponics.sh
+
 echo "---------------------------------------------"reboot---------------------------------------------""
 sudo reboot
