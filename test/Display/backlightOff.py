a27b83fd3e424175d91cd1d4bc975dea0dbdbcfd
ben700 <ben700@hotmail.com>
2020-11-26 04:35:51 +0000

    Update backlightOff.py

--- test/Display/backlightOff.py
+++ test/Display/backlightOff.py
@@ -8,7 +8,9 @@ cols = 20
 rows = 4
 try:
     lcd = liquidcrystal_i2c.LiquidCrystal_I2C(0x27, 1, numlines=rows)
+    print("Starting state of the backlight was " + lcd.getBacklight())
     lcd.noBacklight()
+    print("State of the backlight is now " + lcd.getBacklight())
 except:
     print("no lcd on bus 1")
     
