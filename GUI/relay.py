03797dd2ccc0e4a993ea4d26a406b7869849f0e9
ben700 <ben700@hotmail.com>
2020-12-01 16:30:57 +0000

    Update relay.py

--- GUI/relay.py
+++ GUI/relay.py
@@ -18,6 +18,25 @@ consoleHandler.setFormatter(logFormatter)
 _log.addHandler(consoleHandler)
 _log.setLevel(logging.DEBUG)
 
+# Initialize Blynk
+blynk = blynklib.Blynk(parser.get('blynk', 'BLYNK_AUTH'), log=_log.info) 
+timer = blynktimer.Timer()
+_log.debug("start blynk")
+blynk.run()
+_log.info("Blynk Created")  
+    
+
+relays=drone.RelaysI2C(_log, blynk)
+relays.addRelay(1, parser.get('droneFeed', 'Relay1'), 21, 85)
+relays.addRelay(2, parser.get('droneFeed', 'Relay2'), 22, 86)
+relays.addRelay(3, parser.get('droneFeed', 'Relay3'), 23, 87)
+relays.addRelay(4, parser.get('droneFeed', 'Relay4'), 24, 88)
+relays.addRelay(5, parser.get('droneFeed', 'Relay5'), 25, 89)
+relays.addRelay(6, parser.get('droneFeed', 'Relay6'), 26, 90)
+relays.addRelay(7, parser.get('droneFeed', 'Relay7'), 27, 91)
+relays.addRelay(8, parser.get('droneFeed', 'Relay8'), 28, 92)
+
+
 restartCode = {'P': "Powered Off", 'S':"Software Reset",'B':"Brown Out",'W':"Watchdog",'U':"Unknown"}
 
 import tkinter as tk
@@ -42,8 +61,8 @@ def pumpPage1():
     actionFrame.place(relx=0.2,rely=0.3,relheight=0.1,relwidth=0.6)
     label=tk.Label(actionFrame,text='Relay 1')
     label.grid(row=0,column=0, columnspan=4)
-    bbt1=tk.Button(actionFrame,text='Stop',command=pumpPage1)
-    bbt2=tk.Button(actionFrame,text='Start',command=pumpPage1)
+    bbt1=tk.Button(actionFrame,text='Stop',command=pump1Stop)
+    bbt2=tk.Button(actionFrame,text='Start',command=pump1Start)
     bbt1.grid(row=1,column=0)
     bbt2.grid(row=1,column=1)
 
@@ -52,8 +71,8 @@ def pumpPage2():
     actionFrame.place(relx=0.2,rely=0.3,relheight=0.1,relwidth=0.6)
     label=tk.Label(actionFrame,text='Relay 2')
     label.grid(row=0,column=0, columnspan=4)
-    bbt1=tk.Button(actionFrame,text='Stop',command=pumpPage1)
-    bbt2=tk.Button(actionFrame,text='Start',command=pumpPage1)
+    bbt1=tk.Button(actionFrame,text='Stop',command=pump2Stop)
+    bbt2=tk.Button(actionFrame,text='Start',command=pump2Start)
     bbt1.grid(row=1,column=0)
     bbt2.grid(row=1,column=1)
 
@@ -62,8 +81,8 @@ def pumpPage3():
     actionFrame.place(relx=0.2,rely=0.3,relheight=0.1,relwidth=0.6)
     label=tk.Label(actionFrame,text='Relay 3')
     label.grid(row=0,column=0, columnspan=4)
-    bbt1=tk.Button(actionFrame,text='Stop',command=pumpPage1)
-    bbt2=tk.Button(actionFrame,text='Start',command=pumpPage1)
+    bbt1=tk.Button(actionFrame,text='Stop',command=pump3Stop)
+    bbt2=tk.Button(actionFrame,text='Start',command=pump3Start)
     bbt1.grid(row=1,column=0)
     bbt2.grid(row=1,column=1)
 
@@ -72,8 +91,8 @@ def pumpPage4():
     actionFrame.place(relx=0.2,rely=0.3,relheight=0.1,relwidth=0.6)
     label=tk.Label(actionFrame,text='Relay 4')
     label.grid(row=0,column=0, columnspan=4)
-    bbt1=tk.Button(actionFrame,text='Stop',command=pumpPage1)
-    bbt2=tk.Button(actionFrame,text='Start',command=pumpPage1)
+    bbt1=tk.Button(actionFrame,text='Stop',command=pump4Stop)
+    bbt2=tk.Button(actionFrame,text='Start',command=pump4Start)
     bbt1.grid(row=1,column=0)
     bbt2.grid(row=1,column=1)
 
@@ -82,8 +101,8 @@ def pumpPage5():
     actionFrame.place(relx=0.2,rely=0.3,relheight=0.1,relwidth=0.6)
     label=tk.Label(actionFrame,text='Relay 5')
     label.grid(row=0,column=0, columnspan=4)
-    bbt1=tk.Button(actionFrame,text='Stop',command=pumpPage1)
-    bbt2=tk.Button(actionFrame,text='Start',command=pumpPage1)
+    bbt1=tk.Button(actionFrame,text='Stop',command=pump5Stop)
+    bbt2=tk.Button(actionFrame,text='Start',command=pump5Start)
     bbt1.grid(row=1,column=0)
     bbt2.grid(row=1,column=1)
 
@@ -92,8 +111,8 @@ def pumpPage6():
     actionFrame.place(relx=0.2,rely=0.3,relheight=0.1,relwidth=0.6)
     label=tk.Label(actionFrame,text='Relay 6')
     label.grid(row=0,column=0, columnspan=4)
-    bbt1=tk.Button(actionFrame,text='Stop',command=pumpPage1)
-    bbt2=tk.Button(actionFrame,text='Start',command=pumpPage1)
+    bbt1=tk.Button(actionFrame,text='Stop',command=pump6Stop)
+    bbt2=tk.Button(actionFrame,text='Start',command=pump6Start)
     bbt1.grid(row=1,column=0)
     bbt2.grid(row=1,column=1)
 
@@ -102,8 +121,8 @@ def pumpPage7():
     actionFrame.place(relx=0.2,rely=0.3,relheight=0.1,relwidth=0.6)
     label=tk.Label(actionFrame,text='Relay 7')
     label.grid(row=0,column=0, columnspan=4)
-    bbt1=tk.Button(actionFrame,text='Stop',command=pumpPage1)
-    bbt2=tk.Button(actionFrame,text='Start',command=pumpPage1)
+    bbt1=tk.Button(actionFrame,text='Stop',command=pump7Stop)
+    bbt2=tk.Button(actionFrame,text='Start',command=pump7Start)
     bbt1.grid(row=1,column=0)
     bbt2.grid(row=1,column=1)
 
@@ -113,12 +132,69 @@ def pumpPage8():
     actionFrame.place(relx=0.2,rely=0.3,relheight=0.1,relwidth=0.6)
     label=tk.Label(actionFrame,text='Relay 8')
     label.grid(row=0,column=0, columnspan=4)
-    bbt1=tk.Button(actionFrame,text='Stop',command=pumpPage1)
-    bbt2=tk.Button(actionFrame,text='Start',command=pumpPage1)
+    bbt1=tk.Button(actionFrame,text='Stop',command=pump8Stop)
+    bbt2=tk.Button(actionFrame,text='Start',command=pump8Start)
     bbt1.grid(row=1,column=0)
     bbt2.grid(row=1,column=1)
 
+def pumpStop(index):
+    global relays
+    index =  0 
+    relays.relays[index].turnOff(_log)	
 
+def pumpStart(index):
+    global relays
+    relays.relays[index].turnOn(_log)	
+
+
+def pump1Stop():
+    pumpStop(0)
+
+def pump1Start():
+    pumpStart(0)
+
+def pump2Stop():
+    pumpStop(1)
+
+def pump2Start():
+    pumpStart(1)
+
+def pump3Stop():
+    pumpStop(2)
+
+def pump3Start():
+    pumpStart(2)
+
+def pump4Stop():
+    pumpStop(3)
+
+def pump4Start():
+    pumpStart(3)
+
+def pump5Stop():
+    pumpStop(4)
+
+def pump5Start():
+    pumpStart(4)
+
+def pump6Stop():
+    pumpStop(5)
+
+def pump6Start():
+    pumpStart(5)
+
+def pump7Stop():
+    pumpStop(6)
+
+def pump7Start():
+    pumpStart(6)
+
+def pump8Stop():
+    pumpStop(7)
+
+def pump8Start():
+    pumpStart(7)
+		
 def btnExit():
   	root.destroy()
 
