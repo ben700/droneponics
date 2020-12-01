20ee3fd608b6a2d76b79fd34bd44fb55a9405143
ben700 <ben700@hotmail.com>
2020-12-01 17:13:30 +0000

    Update droneDoser.py

--- droneDoser.py
+++ droneDoser.py
@@ -18,15 +18,7 @@ from AtlasI2C import (
 import math  
 import subprocess
 import re
-import drone
-
-from drone import Alarm, OpenWeather
-alarmList=[]
-#load Temperature alarms
-alarmList.append(Alarm('temperature', "low", "low",18.0, Notify=False,  Message = 'Low TEMP!!!', Colour = '#c0392b'))
-alarmList.append(Alarm('temperature', "High", "high", 26.0, Notify=False, Message = 'High TEMP!!!', Colour = '#c0392b'))
-alarmList.append(Alarm('temperature', "low", "lowlow", 15.0, Notify=True,  Message = 'Low Low TEMP!!!', Colour = '#c0392b'))
-alarmList.append(Alarm('temperature', "High", "highhigh", 30.0,Notify=True, Message = 'High High TEMP!!!', Colour = '#c0392b'))
+import drone  
 
 #from drone import Alarm, OpenWeather
 #sensorList=[]
