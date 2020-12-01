743748aff4d381d9afdfc2ebf369bc63224a61cd
ben700 <ben700@hotmail.com>
2020-12-01 17:12:51 +0000

    Update __init__.py

--- drone/__init__.py
+++ drone/__init__.py
@@ -5,7 +5,7 @@ from drone.nutrientSchedule import *
 from drone.droneponicsAir import *
 from drone.droneponicsRelay import *
 from drone.openWeather import *
-from drone.alarm import Alarm
+#from drone.alarm import Alarm
 from drone.DroneCounter import DroneCounter
 from drone.sensor import *
 from drone.display import *
