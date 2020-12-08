9f2f936de4409fe0797f6f314b69f5e2fcafb857
ben700 <ben700@hotmail.com>
2020-12-08 10:56:55 +0000

    Update droneFeed.py

--- droneFeed.py
+++ droneFeed.py
@@ -143,7 +143,8 @@ try:
         global relayBus
         _log.debug("start v11write_handler")
         _log.debug("start v11write_handler value[0] =" + str(value[0]))
-        _log.debug("start v11write_handler relayBus.relays[0].name =" + str(relayBus.relays[0].name))
+	print(relayBus.relays)
+#        _log.debug("start v11write_handler relayBus.relays[0].name =" + str(relayBus.relays[0].name))
 	
         relayBus.relays[0].cycleResetSet(value[0])
         blynk.virtual_write(relayBus.relays[0].getInfoPin(), relayBus.relays[0].info())
