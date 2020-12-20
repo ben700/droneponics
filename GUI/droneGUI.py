38f69f17a08d4c78fc7343d630fe36ea58662366
ben700 <ben700@hotmail.com>
2020-12-20 02:40:21 +0000

    Update droneGUI.py

--- GUI/droneGUI.py
+++ GUI/droneGUI.py
@@ -1,7 +1,25 @@
 import tkinter as tk
 from tkinter import ttk
 
+class StartPage(tk.Frame):
 
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self,parent)
+        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button = ttk.Button(self, text="Calibration",
+                            command=lambda: controller.show_frame(CalibrationPage))
+        button.pack()
+
+        button2 = ttk.Button(self, text="Configuration",
+                            command=lambda: controller.show_frame(ConfigurationPage))
+        button2.pack()
+
+        button3 = ttk.Button(self, text="Graph Page",
+                            command=lambda: controller.show_frame(PageThree))
+        button3.pack()
+        
 class CalibrationPage(tk.Frame):
 
     def __init__(self, parent, controller):
@@ -49,4 +67,11 @@ class PMPCalPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Dose Pump Calibration Page!!!", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+        
+class ConfigurationPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Configuration Page!!!", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
