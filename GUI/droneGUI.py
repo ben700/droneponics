d220acd0d077f5f63ffd5a13aaf699ec3e3b10e8
ben700 <ben700@hotmail.com>
2020-12-20 02:27:23 +0000

    Create droneGUI.py

---
+++ GUI/droneGUI.py
@@ -0,0 +1,15 @@
+class PageOne(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Page One!!!", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button1 = ttk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        button2 = ttk.Button(self, text="Page Two",
+                            command=lambda: controller.show_frame(PageTwo))
+        button2.pack()
+
