8b010dafb227cc4b5e8b602f59410e826deff942
ben700 <ben700@hotmail.com>
2020-12-21 15:37:11 +0000

    Update graph.py

--- GUI/example/graph.py
+++ GUI/example/graph.py
@@ -129,6 +129,7 @@ class PageTwo(tk.Frame):
 class PageThree(tk.Frame):
 
     def __init__(self, parent, controller):
+        print("PageThree __init__")        
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Graph Page!", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
@@ -136,15 +137,12 @@ class PageThree(tk.Frame):
         button1 = ttk.Button(self, text="Back to Home",
                             command=lambda: controller.show_frame(StartPage))
         button1.pack()
-
-        
-
-        
-
+        print("pre FigureCanvasTk")
         canvas = FigureCanvasTk(f, self)
         canvas.draw()
         canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
-
+        print("post FigureCanvasTk")
+        
         toolbar = NavigationToolbar2Tk(canvas, self)
         toolbar.update()
         canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
