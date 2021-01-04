ca08d0d2eaca797f8b5ffd133d150a3ef1134068
ben700 <ben700@hotmail.com>
2021-01-04 21:52:14 +0000

    Update showMAC.py

--- test/showMAC.py
+++ test/showMAC.py
@@ -12,12 +12,15 @@ from AtlasI2C import (
 )
 import drone 
 
+
+print("{0}: {1}".format("wlan0", drone.get_mac("wlan0")))
+  
 # list all available network interfaces
-def list_all_netifaces():
-	return psutil.net_if_addrs().keys()
+#def list_all_netifaces():
+#	return psutil.net_if_addrs().keys()
 
 # print them all
-for ifaces in list_all_netifaces():
-	print("{0}: {1}".format(ifaces, drone.get_mac(ifaces)))
+#for ifaces in list_all_netifaces():
+#	print("{0}: {1}".format(ifaces, drone.get_mac(ifaces)))
   
  
