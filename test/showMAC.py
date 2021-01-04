b686f94356789062245ce3906ffd2415b5456c2b
ben700 <ben700@hotmail.com>
2021-01-04 21:44:56 +0000

    Create showMAC.py

---
+++ test/showMAC.py
@@ -0,0 +1,21 @@
+from configparser import ConfigParser
+from datetime import datetime
+import time
+import logging
+import sys
+import os
+sys.path.append('/home/pi/droneponics')
+from AtlasI2C import (
+   AtlasI2C
+)
+import drone 
+
+# list all available network interfaces
+def list_all_netifaces():
+	return psutil.net_if_addrs().keys()
+
+# print them all
+for ifaces in list_all_netifaces():
+	print("{0}: {1}".format(ifaces, drone.get_mac(ifaces)))
+  
+ 
