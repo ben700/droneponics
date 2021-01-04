3bda247a522e5bfda5ce0a078acb9293ea59a8d9
ben700 <ben700@hotmail.com>
2021-01-04 21:40:59 +0000

    Update droneponicsAir.py

--- drone/droneponicsAir.py
+++ drone/droneponicsAir.py
@@ -3,6 +3,25 @@ import socket
 import blynklib
 from configparser import ConfigParser
 import drone
+from binascii import hexlify
+
+# get mac address
+def get_mac(interface, p=0):
+
+    # create dummy socket
+    s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
+
+    # bind it with interface name
+    s.bind((interface,p))
+
+    # extract mac address
+    mac =  hexlify(s.getsockname()[4])
+
+    # close socket
+    s.close()
+
+    #return value
+    return mac
 
 def gethostname():
     return socket.gethostname()
