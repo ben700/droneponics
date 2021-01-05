cdf9413c891b1fff0643d1598bcd794e3b377010
ben700 <ben700@hotmail.com>
2021-01-05 02:05:46 +0000

    Update showMAC.py

--- test/showMAC.py
+++ test/showMAC.py
@@ -1,13 +1,3 @@
-from configparser import ConfigParser
-from datetime import datetime
-import time
-import logging
-import sys
-import os
-sys.path.append('/home/pi/droneponics')
-from AtlasI2C import (
-   AtlasI2C
-)
 import drone 
 
 
