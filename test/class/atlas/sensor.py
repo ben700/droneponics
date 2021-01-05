2b7ca2de5a91145e3161b61f4e5f65da7727b362
ben700 <ben700@hotmail.com>
2021-01-05 00:07:24 +0000

    Create sensor.py

---
+++ test/class/atlas/sensor.py
@@ -0,0 +1,37 @@
+import time
+import sys
+import os
+sys.path.append('/home/pi/droneponics')
+import drone
+from configparser import ConfigParser
+import logging
+
+
+parser = ConfigParser()
+parser.read("/home/pi/droneponics/config/configOxy/"+drone.gethostname()+".ini")
+
+
+
+# tune console logging
+_log = logging.getLogger('GoogleLog')
+logFormatter = logging.Formatter("%(asctime)s [%(levelname)s]  %(message)s")
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(logFormatter)
+_log.addHandler(consoleHandler)
+_log.setLevel(parser.get('logging', 'logLevel', fallback=logging.DEBUG))
+_log.critical("critical")
+_log.error("error")
+_log.warning("warning")
+_log.info("info")
+_log.debug("debug")
+
+
+sensors = []
+sensors = drone.buildMonitorSensors(sensors, _log)
+_log.info("All Monitor Sensors created")
+
+for sensor in sensors:
+    if sensor is not None:
+        _log.critical("Going to read the " + sensor.name)    
+        sensor.read()
+        _log.critical("The " + sensor.name + " is " + str(sensor.value))
