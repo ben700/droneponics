607b8b2a0920c8512f9e027a87ddc8fa4a69724c
ben700 <ben700@hotmail.com>
2021-01-07 21:02:49 +0000

    Update updateDroneponics.sh

--- updateDroneponics.sh
+++ updateDroneponics.sh
@@ -1,4 +1,5 @@
 sudo rm -rf /home/pi/droneponics
 git clone https://github.com/ben700/droneponics.git /home/pi/droneponics
 chmod +x /home/pi/droneponics/*.sh
+chmod +x /home/pi/droneponics/test/class/*.sh
 
