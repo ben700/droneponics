f7a1a5a3ee0fb8030da12482c1e93e69b9edcffb
ben700 <ben700@hotmail.com>
2021-01-07 20:19:58 +0000

    Update general.py

--- drone/general.py
+++ drone/general.py
@@ -1,3 +1,27 @@
+import logging
+import socket
+from getmac import get_mac_address
+# get mac address
+def get_mac(interface="wlan0", p=0):
+    #return value
+    return get_mac_address(interface)
+
+
+def gethostname():
+    return socket.gethostname()
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
 def rebooter(pin, value, blynk):
         _log.critical( "User reboot")
         blynk.virtual_write(250, "Reboot")
