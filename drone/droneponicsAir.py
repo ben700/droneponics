d065971f97ab8fc4ffbdd313c91c2d6c0a399173
ben700 <ben700@hotmail.com>
2021-01-07 20:19:51 +0000

    Update droneponicsAir.py

--- drone/droneponicsAir.py
+++ drone/droneponicsAir.py
@@ -1,29 +1,7 @@
 import logging
-import socket
 import blynklib
 from configparser import ConfigParser
 import drone
-from getmac import get_mac_address
-# get mac address
-def get_mac(interface="wlan0", p=0):
-    #return value
-    return get_mac_address(interface)
-
-
-def gethostname():
-    return socket.gethostname()
-
-def get_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        # doesn't even have to be reachable
-        s.connect(('10.255.255.255', 1))
-        IP = s.getsockname()[0]
-    except:
-        IP = '127.0.0.1'
-    finally:
-        s.close()
-    return IP
 
 
 def setFormBlynkLogObjects(*args, **kwargs):
