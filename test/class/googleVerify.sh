bc86d82061c768f3af69fa519e04e9e1dd8849cb
ben700 <ben700@hotmail.com>
2021-01-07 21:02:15 +0000

    Update googleVerify.sh

--- test/class/googleVerify.sh
+++ test/class/googleVerify.sh
@@ -1,2 +1,2 @@
-#./google-cloud-sdk/bin/gcloud beta pubsub subscriptions pull --auto-ack droneOxy
-./google-cloud-sdk/bin/gcloud beta pubsub subscriptions pull --auto-ack dronePH
+#/home/pi/google-cloud-sdk/bin/gcloud beta pubsub subscriptions pull --auto-ack droneOxy
+/home/pi/google-cloud-sdk/bin/gcloud beta pubsub subscriptions pull --auto-ack dronePH
