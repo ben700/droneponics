a27be2b53c1013da8b3bbd305fe53c5e9c4439fb
ben700 <ben700@hotmail.com>
2021-01-07 20:47:27 +0000

    Update __init__.py

--- drone/__init__.py
+++ drone/__init__.py
@@ -1,3 +1,4 @@
+from drone.general import *
 from drone.dose import *
 from drone.relay import Relay
 from drone.relayI2C import *
@@ -5,7 +6,6 @@ from drone.nutrientSchedule import *
 from drone.droneponicsAir import *
 from drone.droneponicsRelay import *
 from drone.openWeather import *
-#from drone.alarm import Alarm
 from drone.DroneCounter import DroneCounter
 from drone.sensor import *
 from drone.display import *
