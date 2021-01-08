de1a6ec0c05ab737e8bddb33ce073f4b1a6309de
ben700 <ben700@hotmail.com>
2021-01-08 21:27:20 +0000

    Update googleVerify.sh

--- test/class/googleVerify.sh
+++ test/class/googleVerify.sh
@@ -1,2 +1,7 @@
 #/home/pi/google-cloud-sdk/bin/gcloud beta pubsub subscriptions pull --auto-ack droneOxy
-/home/pi/google-cloud-sdk/bin/gcloud beta pubsub subscriptions pull --auto-ack dronePH
+#!/bin/bash
+for i in 1 2 3 4 5
+do
+   echo "Welcome $i times"
+   /home/pi/google-cloud-sdk/bin/gcloud beta pubsub subscriptions pull --auto-ack dronePH
+done
