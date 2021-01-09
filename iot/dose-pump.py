8735b6254065f1218cd97f69851864df43d8efec
ben700 <ben700@hotmail.com>
2021-01-09 12:03:28 +0000

    Update dose-pump.py

--- iot/dose-pump.py
+++ iot/dose-pump.py
@@ -16,15 +16,50 @@ from __future__ import print_function
 
 import socket
 import sys
-
-import RPi.GPIO as GPIO
+import drone
+from configparser import ConfigParser
 from colors import bcolors
 
 
-LED_IOPIN = 14
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(LED_IOPIN, GPIO.OUT)
+# Droneponics Start
+parser = ConfigParser()
+parser.read("/home/pi/droneponics/config/Google/"+drone.gethostname()+".ini")
+
+# tune console logging
+_log = logging.getLogger('GoogleLog')
+logFormatter = logging.Formatter("%(asctime)s [%(levelname)s]  %(message)s")
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(logFormatter)
+_log.addHandler(consoleHandler)
+_log.setLevel(parser.get('logging', 'logLevel', fallback=logging.DEBUG))
+_log.critical("critical")
+_log.error("error")
+_log.warning("warning")
+_log.info("info")
+_log.debug("debug")
+_log.info("ConfigParser path = /home/pi/droneponics/config/Google/"+drone.gethostname()+".ini")
+
+
+
+ssl_private_key_filepath = parser.get('Google', 'ssl_private_key_filepath')
+ssl_algorithm = parser.get('Google', 'ssl_algorithm')
+root_cert_filepath = parser.get('Google', 'root_cert_filepath')
+project_id = parser.get('Google', 'project_id')
+gcp_location = parser.get('Google', 'gcp_location')
+registry_id = parser.get('Google', 'sensor_registry_id')
+device_id = parser.get('Google', 'device_id')
+device_sensor_type = parser.get('Google', 'device_sensor_type')
+
+_log.info('-------------------- device_sensor_type = ' + str(device_sensor_type))
+_log.info("ssl_private_key_filepath = " + str(ssl_private_key_filepath))
+_log.info("ssl_algorithm = " + str(ssl_algorithm))
+_log.info("root_cert_filepath = " + str(root_cert_filepath))
+_log.info("project_id = " + str(project_id))
+_log.info("gcp_location = " + str(gcp_location))
+_log.info("registry_id = " + str(registry_id))
+_log.info("device_id = " + str(device_id))
+
+relay = drone.Relay(_log, 21, "Ozone")
 
 ADDR = ''
 PORT = 10000
@@ -74,21 +109,21 @@ def RunAction(action, data=''):
 try:
     RunAction('detach')
     RunAction('attach')
-    RunAction('event', 'LED is online')
+    RunAction('event', 'Doser is online')
     RunAction('subscribe')
 
     while True:
         response = client_sock.recv(4096).decode('utf8')
         print('Client received {}'.format(response))
         if response.upper() == 'ON' or response.upper() == b'ON':
-            GPIO.output(LED_IOPIN, GPIO.HIGH)
+            relay.turnOn(_log) 
             sys.stdout.write('\r>> ' + bcolors.OKGREEN + bcolors.CBLINK +
-                             " LED is ON " + bcolors.ENDC + ' <<')
+                             " Doser is ON " + bcolors.ENDC + ' <<')
             sys.stdout.flush()
         elif response.upper() == "OFF" or response.upper() == b'OFF':
-            GPIO.output(LED_IOPIN, GPIO.LOW)
+            relay.turnOff(_log)
             sys.stdout.write('\r >>' + bcolors.CRED + bcolors.BOLD +
-                             ' LED is OFF ' + bcolors.ENDC + ' <<')
+                             ' Doser is OFF ' + bcolors.ENDC + ' <<')
             sys.stdout.flush()
         else:
             print('Invalid message {}'.format(response))
