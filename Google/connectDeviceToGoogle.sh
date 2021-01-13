6ad524b324b190bcd0a0182d85d5fcc93f62948d
ben700 <ben700@hotmail.com>
2021-01-13 19:03:27 +0000

    Update connectDeviceToGoogle.sh

--- Google/connectDeviceToGoogle.sh
+++ Google/connectDeviceToGoogle.sh
@@ -1,7 +1,7 @@
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-321.0.0-linux-x86.tar.gz
 tar -xf google-cloud-sdk-321.0.0-linux-x86.tar.gz
 rm -f google-cloud-sdk-321.0.0-linux-x86.tar.gz
-./google-cloud-sdk/install.sh
+./google-cloud-sdk/install.sh --disable-prompts 
 ./google-cloud-sdk/bin/gcloud init
 
 #4/1AY0e-g67mG0kFmGRFUu0-dI3QHUg8HPawRIuMFGDCxpi_QenCIdCSRrUP9E
