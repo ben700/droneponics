2c8240c798d809a308d1e93617de8be66e9d72c2
ben700 <ben700@hotmail.com>
2021-01-13 19:46:04 +0000

    Update connectDeviceToGoogle.sh

--- Google/connectDeviceToGoogle.sh
+++ Google/connectDeviceToGoogle.sh
@@ -1,8 +1,16 @@
+sudo apt-get install build-essential
+sudo apt-get install libssl-dev
+sudo apt-get install python-dev
+sudo apt-get install libffi-dev
+sudo pip3 install paho-mqtt
+sudo pip3 install pyjwt
+sudo pip3 install cryptography
+
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-321.0.0-linux-x86.tar.gz
 tar -xf google-cloud-sdk-321.0.0-linux-x86.tar.gz
 rm -f google-cloud-sdk-321.0.0-linux-x86.tar.gz
 ./google-cloud-sdk/install.sh --disable-prompts 
-./google-cloud-sdk/bin/gcloud init
+./google-cloud-sdk/bin/gcloud init --no-launch-browser --account
 
 #4/1AY0e-g67mG0kFmGRFUu0-dI3QHUg8HPawRIuMFGDCxpi_QenCIdCSRrUP9E
 
