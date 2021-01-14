8410290d0870ad7c6459bef57f527dad3777daf4
ben700 <ben700@hotmail.com>
2021-01-14 18:54:56 +0000

    Update google.py

--- drone/google.py
+++ drone/google.py
@@ -155,14 +155,14 @@ def pubGasiousReadingsToGoolgeCloud(dronePayload):
     return pubGoolgeCloud(_MQTT_TOPIC, dronePayload.get())
    
 def pubSensorReadingsToGoolgeCloud(sensors, _log):
-
+    _log.critical("-------------------------------------------pubSensorReadingsToGoolgeCloud")
     # Droneponics Start
     parser = ConfigParser()
     parser.read("/home/pi/droneponics/config/Google/"+drone.gethostname()+".ini")
     device_id = parser.get('Google', 'device_id')
     _MQTT_TOPIC = '/devices/{}/events'.format(device_id)
-
-    return pubGoolgeCloud(_MQTT_TOPIC, drone.buildSensorPayload(sensors, _log, payload))
+    _log.critical("-------------------------------------------pubSensorReadingsToGoolgeCloud final line")
+    return pubGoolgeCloud(_MQTT_TOPIC, drone.buildSensorPayload(sensors, _log, ""))
 
    
 def pubDoseVolumeToGoolgeCloud(dose, _log):
