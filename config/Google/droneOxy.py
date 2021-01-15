8b1d96d6801183b99d1a02933d6f4855dab41c57
ben700 <ben700@hotmail.com>
2021-01-15 21:10:17 +0000

    Update droneOxy.py

--- config/Google/droneOxy.py
+++ config/Google/droneOxy.py
@@ -2,10 +2,10 @@
 import os 
   
 # Add a new environment variable  
-os.environ['GCLOUD_PROJECT'] = 'droneponics-301222'
+os.environ['GOOGLE_CLOUD_PROJECT'] = 'droneponics-301222'
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/home/pi/droneponics/config/Google/service_account.json'
 
 # Get the value of 
 # Added environment variable  
-print("GCLOUD_PROJECT:", os.environ['GCLOUD_PROJECT'])
+print("GOOGLE_CLOUD_PROJECT:", os.environ['GOOGLE_CLOUD_PROJECT'])
 print("GOOGLE_APPLICATION_CREDENTIALS:", os.environ['GOOGLE_APPLICATION_CREDENTIALS']) 
