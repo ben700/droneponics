66d5443c770d582ac86f3c72483c678a6ef860bc
ben700 <ben700@hotmail.com>
2021-01-16 08:03:28 +0000

    Update 02_basics.py

--- Google/02_basics.py
+++ Google/02_basics.py
@@ -22,7 +22,26 @@ import jwt
 import paho.mqtt.client as mqtt
 import re
 import devicePayload
+from configparser import ConfigParser
 
+parser = ConfigParser()
+parser.read("/home/pi/droneponics/config/Google/"+drone.gethostname()+".ini")
+
+service_account_json =parser.get('Google', 'service_account_json')
+project_id =parser.get('Google', 'project_id')
+cloud_region = parser.get('Google','gcp_location')
+registry_id =parser.get('Google', 'registry_id')
+device_id = parser.get('Google', 'device_id')
+gateway_id =parser.get('Google', 'gateway_id')
+num_messages = 1
+rsa_private_path = parser.get('Google', 'rsa_private_path')
+algorithm = parser.get('Google', 'ssl_algorithm')
+ca_certs = parser.get('Google', 'root_cert_filepath')
+log_path = parser.get('Google', 'log_path')
+mqtt_bridge_hostname = "mqtt.googleapis.com"
+mqtt_bridge_port = 8883
+jwt_expires_minutes=5
+listen_time=500
 
 # Define some project-based variables to be used below. This should be the only
 # block of variables that you need to edit in order to run this script
