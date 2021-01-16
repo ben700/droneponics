11dad50c6cd2bb7774162b8b93764cdaf063890d
ben700 <ben700@hotmail.com>
2021-01-16 02:10:35 +0000

    Update printDeviceStatus.py

--- Google/printDeviceStatus.py
+++ Google/printDeviceStatus.py
@@ -1,6 +1,6 @@
 from atlas_i2c import sensors
 from atlas_i2c import commands
-from commands import Command
+from atlas_i2c.commands import Command
 from atlas_i2c import atlas_i2c
 
 class Cal(Command):
