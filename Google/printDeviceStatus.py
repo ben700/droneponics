4111a349ee9b5c8e6c1c41c1994356992b392efe
ben700 <ben700@hotmail.com>
2021-01-16 01:08:43 +0000

    Create printDeviceStatus.py

---
+++ Google/printDeviceStatus.py
@@ -0,0 +1,18 @@
+from atlas_i2c import sensors
+from atlas_i2c import commands
+
+
+RTD = sensors.Sensor("Temperature", 102)
+DO =  sensors.Sensor("dissolvedOxygen", 97)
+ORP =  sensors.Sensor("oxidationReductionPotential", 102)
+
+device_list = [RTD,DO,ORP]
+
+
+for device in device_list:
+    device.connect()
+    device.query(commands.Status)
+    print(device.name + " returned status_code = " + str(device.status_code))
+    print(device.name + " data = " + str(device.data))
+    
+
