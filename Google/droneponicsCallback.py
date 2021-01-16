228936dad330c31ccc74db1dd32b9c485c6045e4
ben700 <ben700@hotmail.com>
2021-01-16 10:06:48 +0000

    Update droneponicsCallback.py

--- Google/droneponicsCallback.py
+++ Google/droneponicsCallback.py
@@ -14,6 +14,23 @@ import json
 import connect
 import csv
 
+
+
+parser = ConfigParser()
+parser.read("/home/pi/droneponics/config/Google/"+drone.gethostname()+".ini")
+
+service_account_json =parser.get('Google', 'service_account_json')
+project_id =parser.get('Google', 'project_id')
+cloud_region = parser.get('Google','gcp_location')
+registry_id =parser.get('Google', 'registry_id')
+device_id = parser.get('Google', 'device_id')
+gateway_id =parser.get('Google', 'gateway_id')
+num_messages = 1
+rsa_private_path = parser.get('Google', 'rsa_private_path')
+algorithm = parser.get('Google', 'ssl_algorithm')
+ca_certs = parser.get('Google', 'root_cert_filepath')
+log_path = parser.get('Google', 'log_path')
+
 def logDroneponicsCallback(client):
     def log_on_message(unused_client, unused_userdata, message):
         if not os.path.exists(log_path):
