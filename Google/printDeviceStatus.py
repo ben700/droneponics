b8b3c726e032afac3a24934c27331519b2ec13de
ben700 <ben700@hotmail.com>
2021-01-16 06:22:23 +0000

    Update printDeviceStatus.py

--- Google/printDeviceStatus.py
+++ Google/printDeviceStatus.py
@@ -138,20 +138,73 @@ print(serializedState)
 print("The length of the json is " + str(len(serializedState)))
 
 
+service_account_json =parser.get('Google', 'ssl_private_key_filepath')
+project_id =parser.get('Google', 'project_id')
+cloud_region = parser.get('Google','gcp_location')
+registry_id =parser.get('Google', 'registry_id')
+device_id = parser.get('Google', 'device_id')
+gateway_id =parser.get('Google', 'gateway_id')
+num_messages = 1
+private_key_file = parser.get('Google', 'ssl_private_key_filepath')
+algorithm = parser.get('Google', 'ssl_algorithm')
+ca_certs = parser.get('Google', 'root_cert_filepath')
+mqtt_bridge_hostname = "mqtt.googleapis.com"
+mqtt_bridge_port = 8883
+jwt_expires_minutes = 20
+
+
 connect.send_data_from_bound_device(
-    parser.get('Google', 'ssl_private_key_filepath'),
-    parser.get('Google', 'project_id'),
-    parser.get('Google','gcp_location'),
-    parser.get('Google', 'registry_id'),
-    parser.get('Google', 'device_id'),
-    parser.get('Google', 'gateway_id'),
-    1,
-    parser.get('Google', 'ssl_private_key_filepath'),
-    parser.get('Google', 'ssl_algorithm'),
-    parser.get('Google', 'root_cert_filepath'),
-    "mqtt.googleapis.com",
-    8883,
-    20,
+    service_account_json,
+    project_id,
+    cloud_region,
+    registry_id,
+    device_id,
+    gateway_id,
+    num_messages,
+    private_key_file,
+    algorithm,
+    ca_certs,
+    mqtt_bridge_hostname,
+    mqtt_bridge_port,
+    jwt_expires_minutes,
     serializedState,
 )
 
+
+def log_callback(client):
+    def log_on_message(unused_client, unused_userdata, message):
+        print(message.payload)
+        if not os.path.exists(log_path):
+            with open(log_path, "w") as csvfile:
+                logwriter = csv.writer(csvfile, dialect="excel")
+                logwriter.writerow(["time", "topic", "data"])
+
+        with open(log_path, "a") as csvfile:
+            logwriter = csv.writer(csvfile, dialect="excel")
+            logwriter.writerow(
+                [
+                    datetime.datetime.now().isoformat(),
+                    message.topic,
+                    message.payload,
+                ]
+            )
+
+    client.on_message = log_on_message
+
+connect.listen_for_messages(
+    service_account_json,
+    project_id,
+    cloud_region,
+    registry_id,
+    device_id,
+    gateway_id,
+    num_messages,
+    rsa_private_path,
+    algorithm,
+    ca_cert_path,
+    mqtt_bridge_hostname,
+    mqtt_bridge_port,
+    jwt_exp_time,
+    listen_time,
+    log_callback,
+)
