618de1e571635d5d56de710bf2224c0b8904cbea
ben700 <ben700@hotmail.com>
2021-01-16 15:43:10 +0000

    Update printDeviceStatus.py

--- Google/printDeviceStatus.py
+++ Google/printDeviceStatus.py
@@ -1,7 +1,10 @@
 import json
-from devicePayload import  getDeviceStatePayload
+from devicePayload import  getDeviceStatePayload, calibrationHelpPayload
 
 stateJson = json.loads(getDeviceStatePayload())
 serializedState= json.dumps(stateJson, sort_keys=False, indent=3)
 print(serializedState)
 print("The length of the json is " + str(len(serializedState)))
+
+
+print(calibrationHelpPayload("Error Text"))
