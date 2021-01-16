6975386aee9a3b1aa5bbfa89b379118e0b430666
ben700 <ben700@hotmail.com>
2021-01-16 02:19:18 +0000

    Update printDeviceStatus.py

--- Google/printDeviceStatus.py
+++ Google/printDeviceStatus.py
@@ -22,7 +22,7 @@ def list_i2c_devices():
     dev = atlas_i2c.AtlasI2C()
     for device in deviceName:
         try:
-            dev.set_i2c_address(device[1])
+            dev.set_i2c_address(device[0])
             dev.read("R")
             i2c_devices_attached.append(device)
         except IOError:
