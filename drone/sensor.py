7a7ff28811229ff6b8281cbc1d9b5830cc3dc216
ben700 <ben700@hotmail.com>
2021-01-17 20:50:44 +0000

    Update sensor.py

--- drone/sensor.py
+++ drone/sensor.py
@@ -72,9 +72,9 @@ def buildPayloadField(sString):
    
 def buildSensorPayload(sensors, _log):
     if(sensors[1].name == "EC"):
-        payload = '{{ "ts": "{}", "devicemac": "{}", "temperature": "{}", "conductivity": "{}", "totaldissolvedsolids": "{}", "salinity": "{}", "specificgravity": "{}", "pH": "{}"}}'.format(int(time.time()), drone.get_mac(), buildPayloadField(sensors[0].value), buildPayloadField(sensors[1].value), buildPayloadField(sensors[1].value2), buildPayloadField(sensors[1].value3), buildPayloadField(sensors[1].value4), buildPayloadField(sensors[2].value) )
+        payload = '{{ "deviceTime": "{}", "devicemac": "{}", "temperature": "{}", "conductivity": "{}", "totaldissolvedsolids": "{}", "salinity": "{}", "specificgravity": "{}", "pH": "{}"}}'.format(int(time.time()), drone.get_mac(), buildPayloadField(sensors[0].value), buildPayloadField(sensors[1].value), buildPayloadField(sensors[1].value2), buildPayloadField(sensors[1].value3), buildPayloadField(sensors[1].value4), buildPayloadField(sensors[2].value) )
     elif(sensors[1].name == "Dissolved Oxygen"):      
-        payload = '{{ "ts": "{}", "deviceMAC": "{}", "temperature": "{}", "dissolvedOxygen": "{}", "saturation": "{}", "oxidationReductionPotential": "{}"}}'.format(int(time.time()), drone.get_mac(), buildPayloadField(sensors[0].value), buildPayloadField(sensors[1].value), buildPayloadField(sensors[1].value2), buildPayloadField(sensors[2].value) )
+        payload = '{{ "deviceTime": "{}", "deviceMAC": "{}", "temperature": "{}", "dissolvedOxygen": "{}", "saturation": "{}", "oxidationReductionPotential": "{}"}}'.format(int(time.time()), drone.get_mac(), buildPayloadField(sensors[0].value), buildPayloadField(sensors[1].value), buildPayloadField(sensors[1].value2), buildPayloadField(sensors[2].value) )
     return payload    
    
 class Sensor:
