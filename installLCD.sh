d59a77a6163d696c8d9aba9dc440ec33792fde89
ben <ben700@hotmail.com>
2021-02-04 05:49:20 +0000

    Removed old code

--- installLCD.sh
+++
@@ -1,9 +0,0 @@
-pip3 install https://github.com/pl31/python-liquidcrystal_i2c/archive/master.zip
-sudo pip3 install https://github.com/pl31/python-liquidcrystal_i2c/archive/master.zip
-cd
-git clone -b master --single-branch https://github.com/oweidner/i2crelay.git
-cd i2crelay
-pip3 install --upgrade .
-sudo pip3 install --upgrade .
-cd
-sudo apt-get install libatlas-base-dev
