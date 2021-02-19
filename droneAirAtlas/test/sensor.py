52861e097a888934069eb73ed461a7db09546483
ben700 <ben700@hotmail.com>
2021-02-19 02:46:00 +0000

    Update sensor.py

--- droneAirAtlas/test/sensor.py
+++ droneAirAtlas/test/sensor.py
@@ -11,6 +11,6 @@ print("Found " + str(len(device_address_list)) + " devices" + '/n')
 for i in device_address_list:
    idevice = AtlasI2C(i) 
    print(idevice.get_device_info())
-   print(idevice.query("I"))
+ #  print(idevice.query("I"))
    time.sleep(1)
    print(idevice.query("R"))
