5102a5f27f2593c71a9035d39c0f1117617715ad
ben700 <ben700@hotmail.com>
2021-02-19 02:42:50 +0000

    Update sensor.py

--- droneAirAtlas/test/sensor.py
+++ droneAirAtlas/test/sensor.py
@@ -6,10 +6,9 @@ from AtlasI2C import (AtlasI2C)
     
 device = AtlasI2C()
 device_address_list = device.list_i2c_devices()
-print("Found " + str(len(device_address_list)) + " devices")
+print("Found " + str(len(device_address_list)) + " devices" + '/n')
 for i in device_address_list:
-   print("Success :- Found " + str(i))
    idevice = AtlasI2C(i) 
    print(idevice.get_device_info())
-   print(idevice.query("I"))
-   print(idevice.query("R"))
+//   print(idevice.query("I"))
+//   print(idevice.query("R"))
