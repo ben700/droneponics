4e498cb67bd42c2d21cad8c55f4eefefdb508bbc
ben700 <ben700@hotmail.com>
2021-02-19 04:33:29 +0000

    Update droneAirAtlas.py

--- droneAirAtlas/droneAirAtlas.py
+++ droneAirAtlas/droneAirAtlas.py
@@ -242,12 +242,16 @@ def main():
             except:
                 pass 
         
-        serializedPayload= json.dumps(payload, sort_keys=False, indent=2)
-        if (connected and len(serializedPayload) > 0):
-            print('publishing ' + str(serializedPayload) + ' on ' + mqtt_topic)
-            print(client.publish(mqtt_topic, serializedPayload, qos=0))
-            
-        time.sleep(60)
+            serializedPayload = json.dumps(payload, sort_keys=False, indent=2)
+        
+            if (connected and len(serializedPayload) > 0):
+                print('publishing ' + str(serializedPayload) + ' on ' + mqtt_topic)
+                print(client.publish(mqtt_topic, serializedPayload, qos=0))
+            else if (len(serializedPayload) > 0):
+                print("Error: no payload Data")
+                
+                
+            time.sleep(60)
         
         except KeyboardInterrupt:
                 print('\nCtrl-C Pressed! Exiting.\n')
