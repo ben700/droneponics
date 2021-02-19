261c5f2b6e9f4e33adc5a03671b9e6f23c91a17c
ben700 <ben700@hotmail.com>
2021-02-19 09:57:12 +0000

    Update droneAirParameter.py

--- droneAtlas/install/droneAirParameter.py
+++ droneAtlas/install/droneAirParameter.py
@@ -11,26 +11,35 @@ while (complete is not True):
     try:
         co2 = AtlasI2C(105)
         print("Processing CO2")
-        print(co2.query("O,t,1"))
-        print(co2.query("O,?")) 
-        print("Success CO2")
-        complete = True
+        print(co2.query("O,?"))     
+    except:
+        print("Failed : No CO2 sensor")
+    try:
+        if(co is not None):
+            print(co2.query("O,t,1"))
+            print(co2.query("O,?")) 
+            print("Success CO2")
+            complete = True
     except:
         print("Fail CO2")
-        AtlasI2C(105).query("Factory")
         complete = False
+    except:
+        print("Failed : No Humidity sensor")
 
     try:
         hum = AtlasI2C(111)
         print("Processing Humidity")
-        print(hum.query("O,HUM,1"))
-        print(hum.query("O,Dew,1"))
-        print(hum.query("O,T,1"))
         print(hum.query("O,?")) 
-        print("Success HUM")
+        
+    try:
+        if(hum is not None):
+            print(hum.query("O,HUM,1"))
+            print(hum.query("O,Dew,1"))
+            print(hum.query("O,T,1"))
+            print(hum.query("O,?")) 
+            print("Success HUM")
     except:
         print("Failed hum")
-        AtlasI2C(111).query("Factory")
         complete = False
     
     if(complete is not True):
