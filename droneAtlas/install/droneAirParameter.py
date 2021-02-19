b54404e628a0777720adba2a6f027f3cd1d41264
ben700 <ben700@hotmail.com>
2021-02-19 09:48:06 +0000

    Update droneAirParameter.py

--- droneAtlas/install/droneAirParameter.py
+++ droneAtlas/install/droneAirParameter.py
@@ -5,31 +5,31 @@ sys.path.append('/home/pi/droneponics/droneAtlas')
 from AtlasI2C import (AtlasI2C)
 
 complete = False
-while (!complete):
+while (complete is not True):
     complete = True
-  try:
-    co2 = AtlasI2C(105)
-    print("Processing CO2")
-    print(co2.query("O,t,1"))
-    print(co2.query("O,?")) 
-    print("Success CO2")
-    complete = True
-  except:
-    print("Fail CO2")
-    complete = False
+    try:
+        co2 = AtlasI2C(105)
+        print("Processing CO2")
+        print(co2.query("O,t,1"))
+        print(co2.query("O,?")) 
+        print("Success CO2")
+        complete = True
+    except:
+        print("Fail CO2")
+        complete = False
 
-  try:
-    hum = AtlasI2C(111)
-    print("Processing Humidity")
-    print(hum.query("O,HUM,1"))
-    print(hum.query("O,Dew,1"))
-    print(hum.query("O,T,1"))
-    print(hum.query("O,?")) 
-    print("Success HUM")
-  except:
-    print("Failed hum")
-    complete = False
+    try:
+        hum = AtlasI2C(111)
+        print("Processing Humidity")
+        print(hum.query("O,HUM,1"))
+        print(hum.query("O,Dew,1"))
+        print(hum.query("O,T,1"))
+        print(hum.query("O,?")) 
+        print("Success HUM")
+    except:
+        print("Failed hum")
+        complete = False
     
-  if(!complete):
-    print("Something Failed will retry")
-  
+    if(!complete):
+        print("Something Failed will retry")
+ 
