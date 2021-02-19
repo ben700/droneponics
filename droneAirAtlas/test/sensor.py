387235f5ffc60bd440e64883b4750d99a7fb0137
ben700 <ben700@hotmail.com>
2021-02-19 02:01:41 +0000

    Create sensor.py

---
+++ droneAirAtlas/test/sensor.py
@@ -0,0 +1,18 @@
+##!/usr/bin/env python3 
+import sys
+import os
+sys.path.append('/home/pi/droneponics')
+from AtlasI2C import (AtlasI2C)
+    
+try:
+  hum = AtlasI2C(111)
+except:
+  print("Unexpected error: Atlas")
+   
+   
+try:	
+  reading = hum.query("R").split(":")[1]
+  delay(1000)
+  print("Reading :" + str(hum) + '\n')
+except:
+  print("Expected error: Use Atlas CO2 Error")
