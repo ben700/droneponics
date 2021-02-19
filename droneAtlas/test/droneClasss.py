2d52022ba7f75a1e3fd3044b1f6f1db103ca03da
ben700 <ben700@hotmail.com>
2021-02-19 06:37:24 +0000

    Update droneClasss.py

--- droneAtlas/test/droneClasss.py
+++ droneAtlas/test/droneClasss.py
@@ -4,8 +4,14 @@ sys.path.append('/home/pi/droneponics/droneAtlas')
 import drone
 import json
 
+
+
+
 sensorList = drone.SensorList()
 payload = {}
 sensorList.payload(payload)
 serializedPayload= json.dumps(payload, sort_keys=False, indent=2)
-print('publishing ' + str(serializedPayload))
+if (len(serializedPayload) > 0):
+  print('publishing ' + str(serializedPayload))
+else
+  print("Did have a payload to publish")
