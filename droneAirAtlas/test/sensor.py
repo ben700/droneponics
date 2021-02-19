275808f13836d2efe0105e15bd28cf1ba5ad7fa3
ben700 <ben700@hotmail.com>
2021-02-19 02:13:32 +0000

    Update sensor.py

--- droneAirAtlas/test/sensor.py
+++ droneAirAtlas/test/sensor.py
@@ -4,6 +4,13 @@ import os
 sys.path.append('/home/pi/droneponics/droneAirAtlas')
 from AtlasI2C import (AtlasI2C)
     
+device = AtlasI2C()
+device_address_list = device.list_i2c_devices()
+print("Found " + str(sizeOf(device_address_list)) + " devices")
+for i in device_address_list:
+   print("Success :- Found " + str(i))
+    
+    
 try:
   hum = AtlasI2C(111)
 except:
