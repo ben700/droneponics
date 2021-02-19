996db975c5b8a36fe2c4e27f4e7eb398ed776648
ben700 <ben700@hotmail.com>
2021-02-19 05:14:22 +0000

    Update droneClasss.py

--- droneAtlas/test/droneClasss.py
+++ droneAtlas/test/droneClasss.py
@@ -1,10 +1,10 @@
 import sys
 import os
 sys.path.append('/home/pi/droneponics/droneAirAtlas/lib')
-import Sensor
+import drone
     
-hum = Sensor(111, "Humidity")
+hum = drone.Sensor(111, "Humidity")
 hum.read()
 
-co2 = Sensor(105, "CO2")
+co2 = drone.Sensor(105, "CO2")
 co2.read()
