167614b25733cc9c923dc2e6ac0acf770e29f4b8
ben700 <ben700@hotmail.com>
2021-02-19 12:17:30 +0000

    Create general.py

---
+++ droneSoil/drone/general.py
@@ -0,0 +1,37 @@
+
+import socket
+import time
+from getmac import get_mac_address
+import drone
+import os
+
+# get mac address
+def get_mac(interface="wlan0", p=0):
+    #return value
+    return get_mac_address(interface)
+
+def gethostname():
+    return socket.gethostname()
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+def payload(payload):
+    payload["bootTime"] = int(time.time())
+    payload["deviceMAC"] = drone.get_mac()
+    payload["deviceIP"] = drone.get_ip()
+    payload["gethostname"] = drone.gethostname()
+
+def fixMe():
+    os.system('sh /home/pi/updateDroneponics.sh')
+    os.system('sudo reboot')
+
