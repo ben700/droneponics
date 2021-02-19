27536a4972d152f7b034597c82dc9b0ea18e7360
ben700 <ben700@hotmail.com>
2021-02-19 10:11:12 +0000

    Update droneAirParameter.py

--- droneAtlas/install/droneAirParameter.py
+++ droneAtlas/install/droneAirParameter.py
@@ -11,7 +11,7 @@ while (complete is not True):
     try:
         co2 = AtlasI2C(105)
         time.sleep(1)
-        print("CO2 Read config :" + co2.query("O,?"))     
+        print("CO2 Read starting config :" + co2.query("O,?"))     
     except:
         complete = False
         co2 = None
@@ -19,17 +19,17 @@ while (complete is not True):
     try:
         if(co2 is not None):
             print("CO2 Turn on temp :" + co2.query("O,t,1"))
-            print("CO2 Read config :" + co2.query("O,?")) 
-            print("Success : CO2")
+            print("CO2 Read ending config :" + co2.query("O,?")) 
+            print("Success : Processing CO2 commands")
             complete = True
     except:
-        print("Failed : Config CO2 sensor")
+        print("Failed : Processing CO2 commands")
         complete = False
 
     try:
         hum = AtlasI2C(111)
         time.sleep(1)
-        print("Humidity Read config :" + hum.query("O,?")) 
+        print("Humidity Read starting config :" + hum.query("O,?")) 
     except:
         complete = False
         hum = None
@@ -41,10 +41,10 @@ while (complete is not True):
             print("Humidity Turn on Humidity :" + hum.query("O,HUM,1"))
             print("Humidity Turn on dew :" + hum.query("O,Dew,1"))
             print("Humidity Turn on temp :" + hum.query("O,T,1"))
-            print("Humidity Read config :" + hum.query("O,?")) 
-            print("Success : Humidity")
+            print("Humidity Read ending config :" + hum.query("O,?")) 
+            print("Success : Processing Humidity commands")
     except:
-        print("Failed : Config Humidity sensor")
+        print("Failed : Processing Humidity commands")
         complete = False
     
     if(complete is not True):
