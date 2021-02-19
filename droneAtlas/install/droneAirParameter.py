1ee7b9ec2640ea32c92bbdaa36d48120d150db28
ben700 <ben700@hotmail.com>
2021-02-19 09:44:53 +0000

    Create droneAirParameter.py

---
+++ droneAtlas/install/droneAirParameter.py
@@ -0,0 +1,35 @@
+##!/usr/bin/env python3 
+import sys
+import os
+sys.path.append('/home/pi/droneponics/droneAtlas')
+from AtlasI2C import (AtlasI2C)
+
+complete = False
+while (!complete):
+    complete = True
+  try:
+    co2 = AtlasI2C(105)
+    print("Processing CO2")
+    print(co2.query("O,t,1"))
+    print(co2.query("O,?")) 
+    print("Success CO2")
+    complete = True
+  except:
+    print("Fail CO2")
+    complete = False
+
+  try:
+    hum = AtlasI2C(111)
+    print("Processing Humidity")
+    print(hum.query("O,HUM,1"))
+    print(hum.query("O,Dew,1"))
+    print(hum.query("O,T,1"))
+    print(hum.query("O,?")) 
+    print("Success HUM")
+  except:
+    print("Failed hum")
+    complete = False
+    
+  if(!complete):
+    print("Something Failed will retry")
+  
