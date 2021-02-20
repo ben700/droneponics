5d0f1dcd1eddc8f9106db7dd06bc5a5fd55b486c
ben700 <ben700@hotmail.com>
2021-02-20 03:46:34 +0000

    Update licence.py

--- droneAtlas/drone/licence.py
+++ droneAtlas/drone/licence.py
@@ -5,7 +5,8 @@ def write_key():
     Generates a key and save it into a file
     """
     key = Fernet.generate_key()
-    with open("key.key", "wb") as key_file:
+#    with open("key.key", "wb") as key_file:
+     with open("key.key", "a") as key_file:
         key_file.write(key)
         print("Success : key = " + str(key))
 write_key()
