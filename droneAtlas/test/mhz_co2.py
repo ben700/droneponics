6ccb891aa4e8d5231a0ad15dec2e26d7d42377b3
ben700 <ben700@hotmail.com>
2021-02-20 01:21:09 +0000

    Update mhz_co2.py

--- droneAtlas/test/mhz_co2.py
+++ droneAtlas/test/mhz_co2.py
@@ -1,4 +1,9 @@
 ##!/usr/bin/env python3 
 import mh_z19
+import sys
+import os
+
 while True:
+    os.write("sudo chmod 777 /dev/serial0")
+    os.write("sudo chown pi:pi /dev/serial0")
     print(mh_z19.read()) 
