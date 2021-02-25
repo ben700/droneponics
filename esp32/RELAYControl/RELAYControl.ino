758b533c16f5746dcd0c6d7e05fb17fd89dae94b
ben700 <ben700@hotmail.com>
2021-02-25 02:09:18 +0000

    Update RELAYControl.ino

--- esp32/RELAYControl/RELAYControl.ino
+++ esp32/RELAYControl/RELAYControl.ino
@@ -7,37 +7,21 @@ WiFiServer server(80);
  
 void setup() 
 {
-  Serial.begin(115200); // must be same baudrate with the Serial Monitor
+  Serial.begin(9600); // must be same baudrate with the Serial Monitor
  
   pinMode(RELAY,OUTPUT);
   digitalWrite(RELAY, LOW);
- 
-  // Connect to WiFi network
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
  
   WiFi.begin(ssid, password);
  
   while (WiFi.status() != WL_CONNECTED) 
   {
     delay(500);
-    Serial.print(".");
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
- 
+  
   // Start the server
   server.begin();
-  Serial.println("Server started");
- 
-  // Print the IP address
-  Serial.print("Use this URL to connect: ");
-//  Serial.print("https://192.168.0.178/");
-  Serial.print(WiFi.localIP());
-  Serial.println("/");
- 
+  
 }
  
 void loop() 
@@ -50,28 +34,23 @@ void loop()
   }
  
   // Wait until the client sends some data
-  Serial.println("new client");
   while(!client.available())
   {
     delay(1);
   }
 
   // Read the first line of the request
-  String request = client.readStringUntil('\r');
-  Serial.println(request);
   client.flush();
  
   // Match the request
   int value = LOW;
   if (request.indexOf("/RELAY=ON") != -1)  
   {
-    Serial.println("RELAY=ON");
     digitalWrite(RELAY,LOW);
     value = LOW;
   }
   if (request.indexOf("/RELAY=OFF") != -1)  
   {
-    Serial.println("RELAY=OFF");
     digitalWrite(RELAY,HIGH);
     value = HIGH;
   }
@@ -82,7 +61,13 @@ void loop()
   client.println(""); //  this is a must
   client.println("<!DOCTYPE HTML>");
   client.println("<html>");
-  client.println("<head><title>ESP8266 RELAY Control</title></head>");
+ 
+  client.println("<head>");
+  client.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"html/style.css\">");
+  client.println("<title>Droneponics WiFi Configuration Page</title>");
+  client.println("<link rel=\"icon\" href=\"html/favicon.ico\">");
+  client.println("</head>");
+                 
   client.print("Relay is now: ");
  
   if(value == HIGH) 
@@ -96,9 +81,6 @@ void loop()
   client.println("<br><br>");
   client.println("Turn <a href=\"/RELAY=OFF\">OFF</a> RELAY<br>");
   client.println("Turn <a href=\"/RELAY=ON\">ON</a> RELAY<br>");
-    client.println("</html>");
+  client.println("</html>");
  
-  delay(1);
-  Serial.println("Client disonnected");
-  Serial.println("");
 }
