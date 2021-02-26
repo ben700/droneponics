c63e89d5477011312feb1f6e17f58e3dbbf67614
ben700 <ben700@hotmail.com>
2021-02-26 11:58:35 +0000

    Update gpio_on.py

--- test/gpio_on.py
+++ test/gpio_on.py
@@ -7,10 +7,12 @@ GPIO.setup(12, GPIO.OUT)           # set GPIO24 as an output
 GPIO.setup(16, GPIO.OUT)           # set GPIO24 as an output   
 GPIO.setup(20, GPIO.OUT)           # set GPIO24 as an output   
 GPIO.setup(21, GPIO.OUT)           # set GPIO24 as an output   
+GPIO.setup(18, GPIO.OUT)           # set GPIO24 as an output   
   
+GPIO.output(18, 1)         # set GPIO24 to 1/GPIO.HIGH/True    
 GPIO.output(12, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
 GPIO.output(16, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
 GPIO.output(20, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
 GPIO.output(21, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
 
-print ("12, 16, 20 and 21 are on")
+print ("12, 16, 18, 20 and 21 are on")
