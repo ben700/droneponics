01e6a3cf8aba2e305787410a144798f39a36a8ca
ben700 <ben700@hotmail.com>
2021-02-26 11:58:04 +0000

    Update gpio_off.py

--- test/gpio_off.py
+++ test/gpio_off.py
@@ -8,10 +8,12 @@ GPIO.setup(12, GPIO.OUT)           # set GPIO24 as an output
 GPIO.setup(16, GPIO.OUT)           # set GPIO24 as an output   
 GPIO.setup(20, GPIO.OUT)           # set GPIO24 as an output   
 GPIO.setup(21, GPIO.OUT)           # set GPIO24 as an output   
+GPIO.setup(18, GPIO.OUT)           # set GPIO24 as an output   
   
+GPIO.output(18, 0)         # set GPIO24 to 1/GPIO.HIGH/True    
 GPIO.output(12, 0)         # set GPIO24 to 1/GPIO.HIGH/True  
 GPIO.output(16, 0)         # set GPIO24 to 1/GPIO.HIGH/True  
 GPIO.output(20, 0)         # set GPIO24 to 1/GPIO.HIGH/True  
 GPIO.output(21, 0)         # set GPIO24 to 1/GPIO.HIGH/True  
 
-print ("12, 16, 20 and 21 are off")
+print ("12, 16, 18, 20 and 21 are off")
