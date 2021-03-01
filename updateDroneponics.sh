6ac071fc41db703aa02cffdb109010da3dba7d50
ben700 <ben700@hotmail.com>
2021-03-01 07:36:19 +0000

    Update updateDroneponics.sh

--- updateDroneponics.sh
+++ updateDroneponics.sh
@@ -3,4 +3,7 @@ git clone https://github.com/ben700/droneponics.git ~/droneponics
 chmod +x ~/droneponics/*.sh
 chmod +x ~/droneponics/test/class/*.sh
 chmod +x ~/droneponics/droneSoil/droneGoogleSoil.py
+chmod +x ~/droneponics/droneAtlas/droneAir.py
+chmod +x ~/droneponics/droneAtlas/droneAtlas.py
+chmod +x ~/droneponics/droneAtlas/droneRelayMicro.py
 
